5ab42c1790fb3dcefb820bb510b37de7adc4d6e8
Jason Plumb <jplumb@flightstats.com>
2013-02-07 10:31:07 -0800

    Init script that starts cassandra at startup time

---
+++ bin/cassandra-init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# This is a unix init script for cassandra.
+#
+
+CASSANDRA_DIR=/home/ubuntu/apache-cassandra-1.2.1
+PIDFILE=${CASSANDRA_DIR}/cassandra.pid
+USER=ubuntu
+
+case "$1" in
+	start)
+		touch ${PIDFILE}
+		chown ubuntu:ubuntu ${PIDFILE}
+
+		start-stop-daemon --start \
+			--chuid ${USER}:${USER} \
+			--chdir ${CASSANDRA_DIR} \
+			--pidfile ${PIDFILE} \
+			--exec /bin/bash \
+			-- --login -c "${CASSANDRA_DIR}/bin/cassandra -p ${PIDFILE}"
+		;;
+	stop)
+		start-stop-daemon --stop \
+			--pidfile ${PIDFILE} \
+			--user ${USER} \
+			--retry TERM/30/KILL/5
+		;;
+	*)
+		echo "Usage: $0 start|stop" >&2
+		exit 1
+		;;
+esac
