5f7d816540995ff7b83f36216d811f15bfeea31b
Jason Plumb <jplumb@flightstats.com>
2013-02-12 13:44:44 -0800

    Deploy script fixes.

--- bin/deploy-to-dev.sh
+++ bin/deploy-to-dev.sh
@@ -22,8 +22,7 @@ ssh ${USER}@${HOST} "sudo /etc/init.d/datahub stop"
 echo Deploying ${ZIPFILE} to ${HOST}
 rsync -avv --progress ${BUILD_DIR}/distributions/${ZIPFILE} ${USER}@${HOST}:/home/${USER}/
 echo Exploding zip file...
-echo ssh ${USER}@${HOST} "unzip -o -f /home/${USER}/${ZIPFILE}"
-ssh ${USER}@${HOST} "unzip -o /home/${USER}/${ZIPFILE}"
+ssh ${USER}@${HOST} "unzip -o -q /home/${USER}/${ZIPFILE}"
 echo Creating symlink
 ssh ${USER}@${HOST} "rm /home/${USER}/datahub; ln -s /home/${USER}/${DISTDIR} /home/${USER}/datahub"
 echo Installing init script
@@ -34,3 +33,17 @@ ssh ${USER}@${HOST} "sudo ln -s /etc/init.d/datahub /etc/rc2.d/S99datahub"
 
 echo Starting up datahub...
 ssh ${USER}@${HOST} "sudo /etc/init.d/datahub start"
+
+echo Waiting for service to be active...
+for i in `seq 1 60` ; do
+	curl --silent http://${HOST}:8080 > /dev/null
+	if [ "$?" == "0" ] ; then
+		echo Service is up.
+		exit 0
+	fi
+	echo -n .
+	sleep 1
+done
+echo Timeout waiting for service to start.
+exit 1
+
