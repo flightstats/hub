fbde4442a9652ca2c399b39da3d827c86915989b
Jason Plumb <jplumb@flightstats.com>
2013-02-18 09:57:31 -0800

    Provide the client with a creation-timestamp in the response from a data storage request.

--- src/main/java/com/flightstats/datahub/service/SingleChannelResource.java
+++ src/main/java/com/flightstats/datahub/service/SingleChannelResource.java
@@ -1,7 +1,7 @@
 package com.flightstats.datahub.service;
 
 import com.flightstats.datahub.dao.ChannelDao;
-import com.flightstats.datahub.model.ValueInsertedResponse;
+import com.flightstats.datahub.model.ValueInsertionResult;
 import com.flightstats.rest.Linked;
 import com.google.inject.Inject;
 
@@ -12,7 +12,6 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import static com.flightstats.rest.Linked.linked;
 
@@ -45,14 +44,14 @@ public class SingleChannelResource {
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Linked<ValueInsertedResponse> insertValue(@PathParam("channelName") String channelName, byte[] data) {
+    public Linked<ValueInsertionResult> insertValue(@PathParam("channelName") String channelName, byte[] data) {
         if (!channelDao.channelExists(channelName)) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
-        UUID uid = channelDao.insert(channelName, data);
+        ValueInsertionResult insertionResult = channelDao.insert(channelName, data);
         URI channelUri = uriInfo.getRequestUri();
-        URI payloadUri = URI.create(channelUri.toString() + "/" + uid.toString());
-        return linked(new ValueInsertedResponse(uid))
+        URI payloadUri = URI.create(channelUri.toString() + "/" + insertionResult.getId().toString());
+        return linked(insertionResult)
                 .withLink("channel", channelUri)
                 .withLink("self", payloadUri)
                 .build();
