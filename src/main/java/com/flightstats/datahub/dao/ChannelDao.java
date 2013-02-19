dfc21cf93e3b012a0c2b2b1d05a98f6f9730f444
Jason Plumb <jplumb@flightstats.com>
2013-02-18 16:29:17 -0800

    Provide a client with the content type when retrieving a value.

--- src/main/java/com/flightstats/datahub/dao/ChannelDao.java
+++ src/main/java/com/flightstats/datahub/dao/ChannelDao.java
@@ -1,6 +1,7 @@
 package com.flightstats.datahub.dao;
 
 import com.flightstats.datahub.model.ChannelConfiguration;
+import com.flightstats.datahub.model.DataHubCompositeValue;
 import com.flightstats.datahub.model.ValueInsertionResult;
 
 import java.util.UUID;
@@ -11,7 +12,7 @@ public interface ChannelDao {
 
     ChannelConfiguration createChannel(String name);
 
-    ValueInsertionResult insert(String channelName, byte[] data);
+    ValueInsertionResult insert(String channelName, String contentType, byte[] data);
 
-    byte[] getValue(String channelName, UUID id);
+    DataHubCompositeValue getValue(String channelName, UUID id);
 }
