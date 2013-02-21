ec93a65fe7924415ad9b0b8d02631b2d303f7e50
Jason Plumb <jplumb@flightstats.com>
2013-02-21 11:15:30 -0800

    Let's all pretend that I know how to write asynchronous javascript, mmmmm-k?

--- src/test/integration/insert_binary_spec.js
+++ src/test/integration/insert_binary_spec.js
@@ -4,34 +4,36 @@ var frisby = require('frisby');
 var fs = require('fs');
 var request = require('request');
 
-var channelName = "integrationtests";
+var channelName = utils.randomChannelName();
 var thisChannelResource = channelUrl + "/" + channelName;
 
-utils.ensureTestChannelCreated(channelName);
+utils.runInTestChannel(channelName, function () {
 
-catUrl = 'http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg';
-console.info("Fetching some binary content to insert....");
-utils.download(catUrl, function (imagedata) {
+    catUrl = 'http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg';
+    console.info("Fetching some binary content to insert....");
+    utils.download(catUrl, function (imagedata) {
 
-    console.info("Inserting an image (" + imagedata.length + " bytes).")
+        console.info("Inserting an image (" + imagedata.length + " bytes).");
 
-    buf = new Buffer(imagedata, 'binary');
-    //console.info("I made a buffer of length: " + buf.length);
-    //console.info("Original bytelength = " + Buffer.byteLength(imagedata));
-    //request.post({url: thisChannelResource, headers: {"Content-Type": "image/jpeg"}, body: buf}, function (error, response, body) {
-    request.post({url: thisChannelResource, body: buf}, function (error, response, body) {
-        expect(error).toBeNull();
-        resultObj = JSON.parse(body);
-        expect(resultObj['_links']['channel']['href']).toBe(thisChannelResource);
+        buf = new Buffer(imagedata, 'binary');
+        //console.info("I made a buffer of length: " + buf.length);
+        //console.info("Original bytelength = " + Buffer.byteLength(imagedata));
+        //request.post({url: thisChannelResource, headers: {"Content-Type": "image/jpeg"}, body: buf}, function (error, response, body) {
+        request.post({url: thisChannelResource, body: buf}, function (error, response, body) {
+            expect(error).toBeNull();
+            console.info(body);
+            resultObj = JSON.parse(body);
+            expect(resultObj['_links']['channel']['href']).toBe(thisChannelResource);
 
-        var valueUrl = resultObj['_links']['self']['href'];
-        console.info("Now to retrieve and compare cats: " + valueUrl);
-        utils.download(valueUrl, function (hubdata) {
-            console.info("Checking to see if the cats match...");
-            expect(hubdata.length).toEqual(imagedata.length);
-            expect(hubdata).toEqual(imagedata);
-            console.info("The cats match!");
+            var valueUrl = resultObj['_links']['self']['href'];
+            console.info("Now to retrieve and compare cats: " + valueUrl);
+            utils.download(valueUrl, function (hubdata) {
+                console.info("Checking to see if the cats match...");
+                expect(hubdata.length).toEqual(imagedata.length);
+                expect(hubdata).toEqual(imagedata);
+                console.info("The cats match!");
 
+            });
         });
     });
 });
--- src/test/integration/insertion_response_has_timestamp_spec.js
+++ src/test/integration/insertion_response_has_timestamp_spec.js
@@ -5,33 +5,33 @@ var channelName = "integrationtests";
 var thisChannelResource = channelUrl + "/" + channelName;
 var messageText = "MY SUPER TEST CASE: this & <that>. " + Math.random().toString();
 
-utils.ensureTestChannelCreated(channelName);
+utils.runInTestChannel(channelName, function () {
 
-console.info('Inserting a value...');
-frisby.create('Inserting a value into a channel.')
-    .post(thisChannelResource, null, { body: messageText})
-    .addHeader("Content-Type", "text/plain")
-    .expectStatus(200)
-    .expectHeader('content-type', 'application/json')
-    .expectJSON('_links', {
-        channel: {
-            href: thisChannelResource
-        }
-    })
-    .expectJSON('_links.self', {
-        href: function (value) {
-            var regex = new RegExp("^" + thisChannelResource.replace(/\//g, "\\/").replace(/\:/g, "\\:") + "\\/[a-f,0-9]{8}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{12}$");
-            expect(value).toMatch(regex);
-        }
-    })
-    .expectJSON({
-        id: function (value) {
-            expect(value).toMatch(/^[a-f,0-9]{8}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{12}$/);
-        },
-        timestamp: function (value) {
-            expect(value).toMatch(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d-\d\d:\d\d$/);
-        }
-    })
-    .inspectJSON()
-    .toss();
-
+    console.info('Inserting a value...');
+    frisby.create('Inserting a value into a channel.')
+        .post(thisChannelResource, null, { body: messageText})
+        .addHeader("Content-Type", "text/plain")
+        .expectStatus(200)
+        .expectHeader('content-type', 'application/json')
+        .expectJSON('_links', {
+            channel: {
+                href: thisChannelResource
+            }
+        })
+        .expectJSON('_links.self', {
+            href: function (value) {
+                var regex = new RegExp("^" + thisChannelResource.replace(/\//g, "\\/").replace(/\:/g, "\\:") + "\\/[a-f,0-9]{8}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{12}$");
+                expect(value).toMatch(regex);
+            }
+        })
+        .expectJSON({
+            id: function (value) {
+                expect(value).toMatch(/^[a-f,0-9]{8}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{4}-[a-f,0-9]{12}$/);
+            },
+            timestamp: function (value) {
+                expect(value).toMatch(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d-\d\d:\d\d$/);
+            }
+        })
+        .inspectJSON()
+        .toss();
+});
