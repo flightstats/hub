8f15acebaecc8072bb7343c7dc0d986e434be2c7
Jason Plumb <jplumb@flightstats.com>
2013-02-22 13:29:24 -0800

    Add tests around "latest" uri.

--- src/test/integration/channel_creation_basic_spec.js
+++ src/test/integration/channel_creation_basic_spec.js
@@ -1,39 +1,44 @@
 require('./integration_config.js');
 var frisby = require('frisby');
+var utils = require('./utils.js');
 
-var channelName = "testcase_" + Math.random().toString().replace(".", "_");
+var channelName = utils.randomChannelName();
 var jsonBody = JSON.stringify({ "name": channelName});
 var channelResource = channelUrl + "/" + channelName;
+
+console.info("Testing basic channel creation response.")
 
 console.info("Making sure channel resource does not yet exist.");
 frisby.create('Making sure channel resource does not yet exist.')
     .get(channelResource)
     .expectStatus(404)
-    .toss();
-
-console.info("Testing basic channel creation response.")
-
-frisby.create('Test basic channel creation')
-    .post(channelUrl, null, { body: jsonBody})
-    .addHeader("Content-Type", "application/json")
-    .expectStatus(200)
-    .expectHeader('content-type', 'application/json')
-    .expectJSON({
-        _links: {
-            self: {
-                href: channelResource
-            }
-        },
-        name: channelName
-        //TODO: Date validation
-    })
-    .afterJSON(function (result) {
-        console.info("Fetching channel resource to ensure that it exists...");
-        frisby.create('Making sure channel resource exists.')
-            .get(result['_links']['self']['href'])
+    .after(function () {
+        frisby.create('Test basic channel creation')
+            .post(channelUrl, null, { body: jsonBody})
+            .addHeader("Content-Type", "application/json")
             .expectStatus(200)
+            .expectHeader('content-type', 'application/json')
+            .expectJSON({
+                _links: {
+                    self: {
+                        href: channelResource
+                    }
+                },
+                name: channelName
+                //TODO: Date validation
+            })
+            .afterJSON(function (result) {
+                console.info("Fetching channel resource to ensure that it exists...");
+                frisby.create('Making sure channel resource exists.')
+                    .get(result['_links']['self']['href'])
+                    .expectStatus(200)
+                    .toss();
+            })
+            .inspectJSON()
             .toss();
+
     })
-    .inspectJSON()
     .toss();
+
+
 
---
+++ src/test/integration/latest_item_in_channel_is_404_when_channel_empty_spec.js
@@ -0,0 +1,16 @@
+require('./integration_config.js');
+var frisby = require('frisby');
+var utils = require('./utils.js');
+
+var channelName = utils.randomChannelName();
+
+utils.runInTestChannel(channelName, function (channelResponse) {
+    console.info(channelResponse);
+    var latestLink = channelResponse['_links']['latest']['href'];
+    frisby.create('Test latest item is 404 when channel is empty')
+        .get(latestLink)
+        .expectStatus(302)
+        .expectHeader('Location', 'application/json')
+        .toss();
+});
+
