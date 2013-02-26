8e52e83b74239a4be139b5b996eb5973b9087447
Jason Plumb <jplumb@flightstats.com>
2013-02-26 11:21:17 -0800

    Use UTC when generating row keys (consistency!)

--- src/test/java/com/flightstats/datahub/dao/YearMonthDayRowKeyStrategyTest.java
+++ src/test/java/com/flightstats/datahub/dao/YearMonthDayRowKeyStrategyTest.java
@@ -4,6 +4,7 @@ import com.flightstats.datahub.model.DataHubKey;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,5 +19,16 @@ public class YearMonthDayRowKeyStrategyTest {
         String result = testClass.buildKey(null, key);
 
         assertEquals("19700523", result);
+    }
+
+    @Test
+    public void testDateBoundard() throws Exception {
+        GregorianCalendar calendar = new GregorianCalendar(2013, 1, 26, 16, 5); //4pm west coast is past midnight for the green witch
+        DataHubKey key = new DataHubKey(calendar.getTime(), (short) 1);
+
+        YearMonthDayRowKeyStrategy testClass = new YearMonthDayRowKeyStrategy();
+        String result = testClass.buildKey("flib", key);
+
+        assertEquals("20130227", result);
     }
 }
