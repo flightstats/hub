7da2f78043e1e2df001d0b0fe9ca3bbdba7c86ed
Jason Plumb <jplumb@flightstats.com>
2013-02-27 15:15:34 -0800

    Clarify test name

--- src/test/integration/fetch_value_nonexistent_channel_spec.js
+++ src/test/integration/fetch_value_nonexistent_channel_spec.js
@@ -6,7 +6,7 @@ var channelName = utils.randomChannelName();
 var badValueUrl = channelUrl + "/" + channelName + "/685221b0-77c2-11e2-8a3e-20c9d08600a5";
 
 utils.runInTestChannel(channelName, function () {
-    frisby.create('Fetching a nonexistent value.')
+    frisby.create('Fetching a nonexistent channel.')
         .get(badValueUrl)
         .expectStatus(404)
         .toss();
