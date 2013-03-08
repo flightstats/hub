519e079b1b8a143b889e8098a2927c9a03efa7d2
Jason Plumb <jplumb@flightstats.com>
2013-03-08 15:32:41 -0800

    starting on sub stuff

--- src/main/java/com/flightstats/datahub/app/DataHubMain.java
+++ src/main/java/com/flightstats/datahub/app/DataHubMain.java
@@ -5,6 +5,7 @@ import com.flightstats.datahub.app.config.GuiceConfig;
 import com.google.inject.servlet.GuiceFilter;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
@@ -14,30 +15,35 @@ import java.util.EnumSet;
  */
 public class DataHubMain {
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
+	public static void main(String[] args) throws Exception {
+		Server server = new Server();
 
-        HttpConfiguration httpConfig = new HttpConfiguration();
+		HttpConfiguration httpConfig = new HttpConfiguration();
 
-        ConnectionFactory connectionFactory = new HttpConnectionFactory(httpConfig);
+		ConnectionFactory connectionFactory = new HttpConnectionFactory(httpConfig);
 
-        ServerConnector serverConnector = new ServerConnector(server, connectionFactory);
+		ServerConnector serverConnector = new ServerConnector(server, connectionFactory);
 
-        //TODO: Don't hard code these here.
-        serverConnector.setHost("0.0.0.0");
-        serverConnector.setPort(8080);
-        serverConnector.setIdleTimeout(30000);
+		//TODO: Don't hard code these here.
+		serverConnector.setHost("0.0.0.0");
+		serverConnector.setPort(8080);
+		serverConnector.setIdleTimeout(30000);
 
-        server.setConnectors(new Connector[]{serverConnector});
+		server.setConnectors(new Connector[]{serverConnector});
 
-        ServletContextHandler rootContextHandler = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
-        rootContextHandler.addEventListener(new GuiceConfig());
-        rootContextHandler.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-        rootContextHandler.addServlet(EmptyServlet.class, "/*");
-        //        rootContextHandler.setErrorHandler(xxx);
 
-        server.start();
-        server.join();
-    }
+		ServletContextHandler rootContextHandler = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+
+		JettyWebSocketServlet jettyWebSocketServlet = new JettyWebSocketServlet();
+		ServletHolder jettyWsHolder = new ServletHolder(jettyWebSocketServlet);
+		rootContextHandler.addServlet(jettyWsHolder, "/jettyws");
+
+		rootContextHandler.addEventListener(new GuiceConfig());
+		rootContextHandler.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+		rootContextHandler.addServlet(EmptyServlet.class, "/*");
+
+		server.start();
+		server.join();
+	}
 
 }
