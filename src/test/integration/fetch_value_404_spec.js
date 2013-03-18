58cf373a3df842d965e7636b54bb6afb13c3eb3a
Jason Plumb <jplumb@flightstats.com>
2013-03-18 14:43:37 -0700

    fix typo.

--- src/test/integration/fetch_value_404_spec.js
+++ src/test/integration/fetch_value_404_spec.js
@@ -5,7 +5,7 @@ var utils = require('./utils.js');
 var channelName = utils.randomChannelName();
 var badValueUrl = channelUrl + "/" + channelName + "/foooo" + Math.random().toString();
 
-utisl.configureFrisby();
+utils.configureFrisby();
 
 utils.runInTestChannel(channelName, function () {
     frisby.create('Fetching a nonexistent value.')
