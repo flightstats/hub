f5927bd95f8310c07577665e14228da7542168e2
gnewcomb <gnewcomb@conducivetech.com>
2013-03-20 21:40:14 -0700

    Checking in subscription tests

--- src/test/qatests/Tests/SubscribeToChannel.js
+++ src/test/qatests/Tests/SubscribeToChannel.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var chai = require('chai');
 var expect = chai.expect;
 var superagent = require('superagent');
@@ -10,18 +12,15 @@ var WebSocket = require('ws');
 
 var dhh = require('../DH_test_helpers/DHtesthelpers.js');
 var testRandom = require('../js_testing_utils/randomUtils.js');
-var WAIT_FOR_CHANNEL_RESPONSE_MS = 500;
-var WAIT_FOR_SOCKET_OPEN_MS = 3000;
+var WAIT_FOR_CHANNEL_RESPONSE_MS = 3000;
 
 var URL_ROOT = dhh.URL_ROOT;
 //var DOMAIN = '10.250.220.197:8080';
 var DOMAIN = 'datahub-01.cloud-east.dev:8080';
+var DEBUG = true;
 
 // Test variables that are regularly overwritten
-var agent
-    , payload
-    , req
-    , uri;
+var agent, payload, req, uri;
 
 var channelName;
 
@@ -29,145 +28,166 @@ var channelName;
 describe('Channel Subscription:', function() {
 
     before(function(){
-        console.log('\nURL_ROOT: '+ URL_ROOT);
-        console.log('DOMAIN (for websockets): '+ DOMAIN);
+        dhh.debugLog('\nURL_ROOT: '+ URL_ROOT);
+        dhh.debugLog('DOMAIN (for websockets): '+ DOMAIN);
+        dhh.debugLog('Debugging ENABLED', DEBUG);
     });
 
     beforeEach(function(myCallback){
         agent = superagent.agent();
         payload = uri = req = null;
 
-        channelName = 'gtest';
-
-        /*
         channelName = dhh.makeRandomChannelName();
+
         dhh.makeChannel(channelName, function(res){
-            if ((res.error) || (res.status != 200)) {
+            if ((res.error) || (res.status !== 200)) {
                 myCallback(res.error);
-            };
+            }
             myCallback();
         });
-        */
-        myCallback();
     });
 
     it('Acceptance: subscription works and updates are sent in order', function(done) {
-        var ws = dhh.createWebSocket(DOMAIN, channelName);
-        var responseQueue = [];
+        var responseQueue = [], ws;
         var uri1, uri2; // remember, the numbers do NOT necessarily reflect the order of creation
 
+        var mainTest = function() {
+            // post values to channel
+            async.parallel([
+                function(callback){
+                    dhh.postData(channelName, testRandom.randomString(50), function(res, uri) {
+                        expect(res.status).to.equal(dhh.DATA_POST_SUCCESS);
+                        dhh.debugLog('Posted first value ', DEBUG);
+                        uri1 = uri;
+                        callback(null, null);
+                    });
+                },
+                function(callback){
+                    dhh.postData(channelName, testRandom.randomString(50), function(res, uri) {
+                        expect(res.status).to.equal(dhh.DATA_POST_SUCCESS);
+                        dhh.debugLog('Posted second value ', DEBUG);
+                        uri2 = uri;
+                        callback(null, null);
+                    });
+                }
+            ],
+                function(e, r){
+                    dhh.debugLog('In final part of async ', DEBUG);
+                    dhh.getLatestUriFromChannel(channelName, function(latestUri) {
+                        var firstUri = (latestUri == uri1) ? uri2 : uri1;
+
+                        //console.log('First uri: '+ firstUri);
+                        //console.log('Second uri: '+ latestUri);
+
+                        // Wait for some period of time for both values
+                        var endWait = Date.now() + (2 * WAIT_FOR_CHANNEL_RESPONSE_MS);
+
+                        while((responseQueue.length < 2) && (Date.now() < endWait)) {
+                            setTimeout(function () {
+                                // pass
+                            }, 100)
+                        };
+
+                        expect(responseQueue.length).to.equal(2);
+                        expect(responseQueue[0]).to.equal(firstUri);
+                        expect(responseQueue[1]).to.equal(latestUri);
+
+                        console.log('final socket state is '+ ws.readyState);
+
+                        ws.close();
+                        done();
+                    });
+                });
+        };
+
+        var onOpen = function() {
+            console.log('OPEN EVENT at '+ Date.now());
+            console.log('readystate: '+ ws.readyState)   ;
+            mainTest();
+        };
+
+        ws = dhh.createWebSocket(DOMAIN, channelName, onOpen);
+
         ws.on('message', function(data, flags) {
+            console.log('MESSAGE EVENT at '+ Date.now());
+            dhh.debugLog('Readystate is '+ ws.readyState, DEBUG);
             responseQueue.push(data);
         });
+    });
 
-        // post values to channel
-        async.parallel([
-            function(callback){
+    // Note, this test also ensures that all updates are correctly saved in the DH *and* that their
+    //  relative links are correct.
+    it('Multiple nigh-simultaneous updates are sent with order preserved.', function(done) {
+        var actualResponseQueue = [], expectedResponseQueue = [], endWait, i;
+        var numUpdates = 10, doDebug = false;
+        this.timeout((numUpdates * WAIT_FOR_CHANNEL_RESPONSE_MS) + 45000);
+
+        var mainTest = function() {
+            async.times(numUpdates, function(n, next) {
                 dhh.postData(channelName, testRandom.randomString(50), function(res, uri) {
-                    expect(res.status).to.equal(dhh.DATA_POST_SUCCESS);
-                    uri1 = uri;
-                    callback(null, null);
+                    dhh.debugLog('Posted data #'+ n, DEBUG);
+                    next(null, uri);
                 });
-            },
-            function(callback){
-                dhh.postData(channelName, testRandom.randomString(50), function(res, uri) {
-                    expect(res.status).to.equal(dhh.DATA_POST_SUCCESS);
-                    uri2 = uri;
-                    callback(null, null);
-                });
-            }
-        ],
-            function(e, r){
-                dhh.getLatestUriFromChannel(channelName, function(latestUri) {
-                    var firstUri = (latestUri == uri1) ? uri2 : uri1;
+            }, function(err, uris) {
+                dhh.debugLog('Number of entries in actual response queue: '+ actualResponseQueue.length, DEBUG);
 
-                    //console.log('First uri: '+ firstUri);
-                    //console.log('Second uri: '+ latestUri);
+                // Repeatedly wait and check response queue from socket until we give up or reach expected number
+                endWait = Date.now() + (numUpdates * WAIT_FOR_CHANNEL_RESPONSE_MS);
+                var lastQueueLength = actualResponseQueue.length;   // for debugging
 
-                    // Wait for some period of time for both values
-                    var endWait = Date.now() + (2 * WAIT_FOR_CHANNEL_RESPONSE_MS);
+                var checkQueueLength = function () {
+                    if (actualResponseQueue > lastQueueLength) {
+                        dhh.debugLog('Response Queue at '+ actualResponseQueue.length, DEBUG);
+                        lastQueueLength = actualResponseQueue.length;
+                    }
+                };
 
-                    while((responseQueue.length < 2) && (Date.now() < endWait)) {
-                        setTimeout(function () {
-                            // pass
-                        }, 100)
-                    };
+                while((actualResponseQueue.length < numUpdates) && (Date.now() < endWait)) {
+                    setTimeout( checkQueueLength(), 100);
+                }
+                dhh.debugLog('Passed while loop with actual queue at '+ actualResponseQueue.length, DEBUG);
 
-                    expect(responseQueue.length).to.equal(2);
-                    expect(responseQueue[0]).to.equal(firstUri);
-                    expect(responseQueue[1]).to.equal(latestUri);
+                if (actualResponseQueue.length !== numUpdates) {
+                    dhh.debugLog('Timed out before response queue got all updates.', DEBUG);
+                    expect(actualResponseQueue.length).to.equal(numUpdates);
+                }
+
+                // confirm results
+                dhh.getListOfLatestUrisFromChannel(numUpdates, channelName, function(allUris) {
+                    expectedResponseQueue = allUris;
+                    dhh.debugLog('Expected response queue length: '+ expectedResponseQueue.length, DEBUG);
+
+                    expect(actualResponseQueue.length).to.equal(numUpdates);
+                    expect(expectedResponseQueue.length).to.equal(numUpdates);
+
+                    dhh.debugLog('Expected and Actual queues are full. Comparing queues...', DEBUG);
+
+                    for (i = 0; i < numUpdates; i += 1) {
+                        expect(actualResponseQueue[i]).to.equal(expectedResponseQueue[i]);
+                        dhh.debugLog('Matched queue number '+ i, DEBUG);
+                    }
 
                     ws.close();
                     done();
                 });
             });
-    });
+        };
 
-    it.skip('Multiple nigh-simultaneous updates are sent with order preserved.', function(done) {
-        var ws = dhh.createWebSocket(DOMAIN, channelName);
-        var actualResponseQueue = [], expectedResponseQueue = [], endWait;
-        var numUpdates = 10;
+        var onOpen = function() {
+            dhh.debugLog('Open event fired!', DEBUG);
+            dhh.debugLog('Readystate: '+ ws.readyState, DEBUG);
+            mainTest();
+        };
 
-        this.timeout((numUpdates * WAIT_FOR_CHANNEL_RESPONSE_MS) + 45000);
+        var ws = dhh.createWebSocket(DOMAIN, channelName, onOpen);
 
         ws.on('message', function(data, flags) {
             actualResponseQueue.push(data);
-            console.log('Received message: '+ data);
+            dhh.debugLog('Received message: '+ data, DEBUG);
+            dhh.debugLog('Response queue length: '+ actualResponseQueue.length, DEBUG);
         });
 
-        endWait = Date.now() + WAIT_FOR_SOCKET_OPEN_MS;
-
-        while((WebSocket.OPEN != ws.readyState) && (Date.now() < endWait)) {
-            setTimeout(function() {
-                // just chillin'
-            }, 100)
-        };
-
-        async.times(numUpdates, function(n, next) {
-            dhh.postDataAndWait(channelName, testRandom.randomString(50), 1000, function(res, uri) {
-                console.log('Posted data #'+ n);
-                next(null, uri);
-            });
-        }, function(err, uris) {
-            //console.log('Entering post-async function');
-            console.log('Number of entries in actual response queue: '+ actualResponseQueue.length);
-
-            // Repeatedly wait and check response queue from socket until we give up or reach expected number
-            endWait = Date.now() + (numUpdates * WAIT_FOR_CHANNEL_RESPONSE_MS);
-            var lastQueueLength = actualResponseQueue.length;   // for debugging
-
-            while((actualResponseQueue.length < numUpdates) && (Date.now() < endWait)) {
-                setTimeout(function () {
-                    if (actualResponseQueue > lastQueueLength) {
-                        console.log('Response Queue at '+ actualResponseQueue.length);
-                        lastQueueLength = actualResponseQueue.length;
-                    }
-                }, 100)
-            };
-
-            if (actualResponseQueue.length != numUpdates) {
-                console.log('Timed out before response queue got all updates.');
-                expect(actualResponseQueue.length).to.equal(numUpdates);
-            }
-
-            // confirm results
-            dhh.getListOfLatestUrisFromChannel(numUpdates, channelName, function(allUris) {
-                expectedResponseQueue = allUris;
-
-                expect(actualResponseQueue.length).to.equal(numUpdates);
-                expect(expectedResponseQueue.length).to.equal(numUpdates);
-
-                console.log('Expected and Actual queues are full. Comparing queues...');
-
-                for (var i = 0; i < numUpdates; i += 1) {
-                    expect(actualResponseQueue[i]).to.equal(expectedResponseQueue[i]);
-                    console.log('Matched queue number '+ i);
-                }
-
-                ws.close();
-                done();
-            });
-        });
+        //process.nextTick();
     });
 
     it.skip('Multiple agents on a channel can be supported.', function(done) {
