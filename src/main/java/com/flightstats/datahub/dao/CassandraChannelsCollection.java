93feacee916770d29d77abb6f255a8751b52e916
Jason Plumb <jplumb@flightstats.com>
2013-04-17 14:59:00 -0700

    Address channel creation performance.  This is primarily addressed by removing the "does the channel already exist" check and simply catching the exception from hector.

--- src/main/java/com/flightstats/datahub/dao/CassandraChannelsCollection.java
+++ src/main/java/com/flightstats/datahub/dao/CassandraChannelsCollection.java
@@ -17,73 +17,71 @@ import me.prettyprint.hector.api.query.QueryResult;
  */
 public class CassandraChannelsCollection {
 
-    static final String CHANNELS_ROW_KEY = "DATA_HUB_CHANNELS";
-    static final String CHANNELS_COLUMN_FAMILY_NAME = "channelMetadata";
+	static final String CHANNELS_ROW_KEY = "DATA_HUB_CHANNELS";
+	static final String CHANNELS_COLUMN_FAMILY_NAME = "channelMetadata";
 
-    private final CassandraConnector connector;
-    private final Serializer<ChannelConfiguration> channelConfigSerializer;
-    private final HectorFactoryWrapper hector;
-    private final TimeProvider timeProvider;
+	private final CassandraConnector connector;
+	private final Serializer<ChannelConfiguration> channelConfigSerializer;
+	private final HectorFactoryWrapper hector;
+	private final TimeProvider timeProvider;
 
-    @Inject
-    public CassandraChannelsCollection(CassandraConnector connector, Serializer<ChannelConfiguration> channelConfigSerializer, HectorFactoryWrapper hector, TimeProvider timeProvider) {
-        this.connector = connector;
-        this.channelConfigSerializer = channelConfigSerializer;
-        this.hector = hector;
-        this.timeProvider = timeProvider;
-    }
+	@Inject
+	public CassandraChannelsCollection(CassandraConnector connector, Serializer<ChannelConfiguration> channelConfigSerializer, HectorFactoryWrapper hector, TimeProvider timeProvider) {
+		this.connector = connector;
+		this.channelConfigSerializer = channelConfigSerializer;
+		this.hector = hector;
+		this.timeProvider = timeProvider;
+	}
 
-    public ChannelConfiguration createChannel(String name) {
-        ChannelConfiguration channelConfig = new ChannelConfiguration(name, timeProvider.getDate(), null);
-        createColumnFamilyForChannel(channelConfig);
-        insertChannelMetadata(channelConfig);
-        return channelConfig;
-    }
+	public ChannelConfiguration createChannel(String name) {
+		ChannelConfiguration channelConfig = new ChannelConfiguration(name, timeProvider.getDate(), null);
+		createColumnFamilyForChannel(channelConfig);
+		insertChannelMetadata(channelConfig);
+		return channelConfig;
+	}
 
-    public int countChannels() {
-        QueryResult<Integer> result = hector.createCountQuery(connector.getKeyspace(), StringSerializer.get(), StringSerializer.get())
-                                            .setKey(CHANNELS_ROW_KEY)
-                                            .setColumnFamily(CHANNELS_COLUMN_FAMILY_NAME)
-                                            .setRange(null, null, Integer.MAX_VALUE)
-                                            .execute();
-        return result.get();
-    }
+	public int countChannels() {
+		QueryResult<Integer> result = hector.createCountQuery(connector.getKeyspace(), StringSerializer.get(), StringSerializer.get())
+											.setKey(CHANNELS_ROW_KEY)
+											.setColumnFamily(CHANNELS_COLUMN_FAMILY_NAME)
+											.setRange(null, null, Integer.MAX_VALUE)
+											.execute();
+		return result.get();
+	}
 
-    private void insertChannelMetadata(ChannelConfiguration channelConfig) {
-        connector.createColumnFamilyIfNeeded(CHANNELS_COLUMN_FAMILY_NAME);
-        StringSerializer keySerializer = StringSerializer.get();
-        Mutator<String> mutator = connector.buildMutator(keySerializer);
-        HColumn<String, ChannelConfiguration> column = hector.createColumn(channelConfig.getName(), channelConfig, StringSerializer.get(),
-                channelConfigSerializer);
-        mutator.insert(CHANNELS_ROW_KEY, CHANNELS_COLUMN_FAMILY_NAME, column);
-    }
+	private void insertChannelMetadata(ChannelConfiguration channelConfig) {
+		StringSerializer keySerializer = StringSerializer.get();
+		Mutator<String> mutator = connector.buildMutator(keySerializer);
+		HColumn<String, ChannelConfiguration> column = hector.createColumn(channelConfig.getName(), channelConfig, StringSerializer.get(),
+				channelConfigSerializer);
+		mutator.insert(CHANNELS_ROW_KEY, CHANNELS_COLUMN_FAMILY_NAME, column);
+	}
 
-    private void createColumnFamilyForChannel(ChannelConfiguration channelConfig) {
-        String columnSpaceName = channelConfig.getName();
-        connector.createColumnFamilyIfNeeded(columnSpaceName);
-    }
+	private void createColumnFamilyForChannel(ChannelConfiguration channelConfig) {
+		String columnSpaceName = channelConfig.getName();
+		connector.createColumnFamily(columnSpaceName);
+	}
 
-    public boolean channelExists(String channelName) {
-        ChannelConfiguration channelConfiguration = getChannelConfiguration(channelName);
-        return channelConfiguration != null;
-    }
+	public boolean channelExists(String channelName) {
+		ChannelConfiguration channelConfiguration = getChannelConfiguration(channelName);
+		return channelConfiguration != null;
+	}
 
-    public ChannelConfiguration getChannelConfiguration(String channelName) {
-        connector.createColumnFamilyIfNeeded(CHANNELS_COLUMN_FAMILY_NAME);
-        Keyspace keyspace = connector.getKeyspace();
-        ColumnQuery<String, String, ChannelConfiguration> rawQuery = hector.createColumnQuery(keyspace, StringSerializer.get(),
-                StringSerializer.get(), channelConfigSerializer);
-        ColumnQuery<String, String, ChannelConfiguration> columnQuery = rawQuery.setName(channelName)
-                                                                                .setKey(CHANNELS_ROW_KEY)
-                                                                                .setColumnFamily(CHANNELS_COLUMN_FAMILY_NAME);
-        QueryResult<HColumn<String, ChannelConfiguration>> result = columnQuery.execute();
-        HColumn<String, ChannelConfiguration> column = result.get();
-        return column == null ? null : column.getValue();
-    }
+	public ChannelConfiguration getChannelConfiguration(String channelName) {
+		Keyspace keyspace = connector.getKeyspace();
+		ColumnQuery<String, String, ChannelConfiguration> rawQuery = hector.createColumnQuery(keyspace, StringSerializer.get(),
+				StringSerializer.get(), channelConfigSerializer);
+		ColumnQuery<String, String, ChannelConfiguration> columnQuery = rawQuery.setName(channelName)
+																				.setKey(CHANNELS_ROW_KEY)
+																				.setColumnFamily(CHANNELS_COLUMN_FAMILY_NAME);
+		QueryResult<HColumn<String, ChannelConfiguration>> result = columnQuery.execute();
+		HColumn<String, ChannelConfiguration> column = result.get();
+		return column == null ? null : column.getValue();
+	}
 
-    public void updateLastUpdatedKey(String channelName, DataHubKey key) {
-        ChannelConfiguration config = getChannelConfiguration(channelName);
-        ChannelConfiguration updatedConfig = config.updateLastUpdateKey(key);
-        insertChannelMetadata(updatedConfig);
-    }
+	public void updateLastUpdatedKey(String channelName, DataHubKey key) {
+		ChannelConfiguration config = getChannelConfiguration(channelName);
+		ChannelConfiguration updatedConfig = config.updateLastUpdateKey(key);
+		insertChannelMetadata(updatedConfig);
+	}
 }
