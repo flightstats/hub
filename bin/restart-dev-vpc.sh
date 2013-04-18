04224ec2840bfcbb8ad01b383211624e19d041c6
Jason Plumb <jplumb@flightstats.com>
2013-04-18 10:27:04 -0700

    Use port checks, because cassandra can take a while to start up -- and we want to make sure it's up before we start our tests.  Nudge the timeout back up.

--- bin/restart-dev-vpc.sh
+++ bin/restart-dev-vpc.sh
@@ -9,31 +9,40 @@ OWNER='jason.plumb@flightstats.com'
 OS=`uname`
 
 INSTANCES=( \
-	cassandra-01.i-9e3a5ded \
-    cassandra-02.i-c0395eb3 \
-    cassandra-03.i-9c395eef \
-	datahub-01.i-b4572ac4 \
+	cassandra-01.i-9e3a5ded:9160 \
+    cassandra-02.i-c0395eb3:9160 \
+    cassandra-03.i-9c395eef:9160 \
+	datahub-01.i-b4572ac4:22 \
 ) 
 
-function ping_host {
-    HOSTNAME=$1
-	if [ "$OS" == "Darwin" ] ; then
-	    ping -q -c 1 -t 2 ${HOSTNAME} > /dev/null
-	else
-	    ping -q -c 1 -W 2 ${HOSTNAME} > /dev/null
-	fi
+if [ -x /usr/local/bin/nc ] ; then
+    NC=/usr/local/bin/nc
+elif [ -x /usr/bin/nc ] ; then
+    NC=/usr/bin/nc
+elif [ -x /bin/nc ] ; then
+    NC=/bin/nc
+else
+    echo "Cannot find a usable netcat (nc)"
+    exit 1
+fi
+
+function port_check {
+    HOST=$1
+    PORT=$2
+    $NC -z -w 1 ${HOST} ${PORT}
 }
 
 for INSTANCE in ${INSTANCES[@]}; do
 	echo '-------------------------------------------------------------------'
 	HOSTNAME=`echo ${INSTANCE} | sed -e "s/\..*//"`.cloud-east.dev
+	PORT=`echo ${INSTANCE} | sed -e "s/^.*://"`
 
 	echo "Checking to see if ${HOSTNAME} is alive..."
-	ping_host ${HOSTNAME}
+	port_check ${HOSTNAME} ${PORT}
 	if [ "$?" == "0" ]; then
 		echo ${HOSTNAME} is already running!
 	else
-		INSTANCE_ID=`echo ${INSTANCE} | sed -e "s/^.*\.//"`
+		INSTANCE_ID=`echo ${INSTANCE} | sed -e "s/^.*\.//" | sed -e "s/:.*//"`
 		RESTART_URL="http://deploy.util.hq.prod:6543/vpc/${INSTANCE_ID}/restart"
 		echo Resurrecting "${HOSTNAME} (instance = ${INSTANCE_ID})"
 		curl -s ${RESTART_URL} &
@@ -45,9 +54,10 @@ echo '-------------------------------------------------------------------'
 echo Waiting for all instances to be alive...
 for INSTANCE in ${INSTANCES[@]}; do
 	HOSTNAME=`echo ${INSTANCE} | sed -e "s/\..*//"`.cloud-east.dev
+	PORT=`echo ${INSTANCE} | sed -e "s/^.*://"`
 	while :
 	do
-	    ping_host ${HOSTNAME}
+	    port_check ${HOSTNAME} ${PORT}
 		if [ "$?" == "0" ]; then
 			echo ${HOSTNAME} is alive.
 			break
