58ee8c6abede275eeefcab01df2181188d839646
John Watson <jwatson@flightstats.com>
2013-04-24 11:22:04 -0700

    first cut implementation

--- src/main/java/com/flightstats/datahub/app/config/GuiceConfig.java
+++ src/main/java/com/flightstats/datahub/app/config/GuiceConfig.java
@@ -1,6 +1,7 @@
 package com.flightstats.datahub.app.config;
 
 import com.flightstats.datahub.dao.*;
+import com.flightstats.datahub.dao.prototypes.InMemoryChannelDao;
 import com.flightstats.datahub.model.ChannelConfiguration;
 import com.flightstats.datahub.model.DataHubCompositeValue;
 import com.flightstats.datahub.model.DataHubKey;
@@ -31,6 +32,9 @@ import java.util.Properties;
 public class GuiceConfig extends GuiceServletContextListener {
 
 	public static final String DATAHUB_PROPERTIES_FILENAME = "datahub.properties";
+	public static final String CASSANDRA_BACKING_STORE_TAG = "cassandra";
+	public static final String BACKING_STORE_PROPERTY = "backing.store";
+	public static final String MEMORY_BACKING_STORY_TAG = "memory";
 
 	private final static Map<String, String> JERSEY_PROPERTIES = new HashMap<>();
 
@@ -42,46 +46,74 @@ public class GuiceConfig extends GuiceServletContextListener {
 
 	@Override
 	protected Injector getInjector() {
-		Module jerseyServletModule = new DataHubModule();
+		Properties properties = loadProperties();
+		String backingStoreName = properties.getProperty(BACKING_STORE_PROPERTY);
+		Module jerseyServletModule;
+		if (backingStoreName == null || CASSANDRA_BACKING_STORE_TAG.equals(backingStoreName)) {
+			jerseyServletModule = new CassandraBackedDataHubModule(properties);
+		} else if (MEMORY_BACKING_STORY_TAG.equals(backingStoreName)) {
+			jerseyServletModule = new MemoryBackedDataHubModule();
+		} else {
+			throw new IllegalStateException(String.format("Unknown backing store specified: %s", backingStoreName));
+		}
 		return Guice.createInjector(jerseyServletModule);
 	}
 
-	private static class DataHubModule extends JerseyServletModule {
+	private Properties loadProperties() {
+		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(DATAHUB_PROPERTIES_FILENAME);
+		Properties properties = new Properties();
+		try {
+			properties.load(in);
+		} catch (IOException e) {
+			throw new RuntimeException("Error loading properties.", e);
+		}
+		return properties;
+	}
 
-		private final ObjectMapper objectMapper = new DataHubObjectMapperFactory().build();
-		private final JacksonHectorSerializer<ChannelConfiguration> jacksonHectorSerializer = new JacksonHectorSerializer<>(objectMapper,
-				ChannelConfiguration.class);
+	private static class BaseDataHubModule extends JerseyServletModule {
 
-		@Override
-		protected void configureServlets() {
-			Properties properties = loadProperties();
-			Names.bindProperties(binder(), properties);
-			bind(CassandraChannelDao.class).asEagerSingleton();
+		protected void configureBaseServlets() {
 			bind(ChannelLockExecutor.class).in(Singleton.class);
 			bind(SubscriptionDispatcher.class).in(Singleton.class);
 			bind(SubscriptionRoster.class).in(Singleton.class);
 			bind(JettyWebSocketServlet.class).in(Singleton.class);
-			bind(CassandraConnectorFactory.class).in(Singleton.class);
 			bind(DataHubKeyRenderer.class).in(Singleton.class);
 			bind(DataHubKeyGenerator.class).in(Singleton.class);
-			bind(new TypeLiteral<Serializer<ChannelConfiguration>>() {
-			}).toInstance(jacksonHectorSerializer);
 			bind(new TypeLiteral<RowKeyStrategy<String, DataHubKey, DataHubCompositeValue>>() {
 			}).to(YearMonthDayRowKeyStrategy.class);
-			bind(ChannelDao.class).to(CassandraChannelDao.class).in(Singleton.class);
 			serveRegex("/channel/\\w+/ws").with(JettyWebSocketServlet.class);
 			serve("/*").with(GuiceContainer.class, JERSEY_PROPERTIES);
 		}
+	}
 
-		private Properties loadProperties() {
-			InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(DATAHUB_PROPERTIES_FILENAME);
-			Properties properties = new Properties();
-			try {
-				properties.load(in);
-			} catch (IOException e) {
-				throw new RuntimeException("Error loading properties.", e);
-			}
-			return properties;
+	private static class MemoryBackedDataHubModule extends BaseDataHubModule {
+		@Override
+		protected void configureServlets() {
+			bind(ChannelDao.class).to(InMemoryChannelDao.class).in(Singleton.class);
+			super.configureBaseServlets();
+		}
+
+	}
+
+	private static class CassandraBackedDataHubModule extends BaseDataHubModule {
+
+		private final ObjectMapper objectMapper = new DataHubObjectMapperFactory().build();
+		private final JacksonHectorSerializer<ChannelConfiguration> jacksonHectorSerializer = new JacksonHectorSerializer<>(objectMapper, ChannelConfiguration.class);
+		private final Properties properties;
+
+		public CassandraBackedDataHubModule(Properties properties) {
+			this.properties = properties;
+		}
+
+		@Override
+		protected void configureServlets() {
+			super.configureBaseServlets();
+			Names.bindProperties(binder(), properties);
+			bind(CassandraChannelDao.class).asEagerSingleton();
+			bind(CassandraConnectorFactory.class).in(Singleton.class);
+			bind(new TypeLiteral<Serializer<ChannelConfiguration>>() {
+			}).toInstance(jacksonHectorSerializer);
+			bind(ChannelDao.class).to(CassandraChannelDao.class).in(Singleton.class);
 		}
 
 		@Inject
