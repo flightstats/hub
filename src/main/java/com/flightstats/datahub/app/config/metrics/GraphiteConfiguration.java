89f46f7e830fe88f73a3b07c477031d47835c846
Jason Plumb <jplumb@flightstats.com>
2013-06-04 12:38:18 -0700

    Prefix the jvm metric names.  Repackage.

--- src/main/java/com/flightstats/datahub/app/config/metrics/GraphiteConfiguration.java
+++ src/main/java/com/flightstats/datahub/app/config/metrics/GraphiteConfiguration.java
@@ -1,11 +1,10 @@
-package com.flightstats.datahub.app.config;
+package com.flightstats.datahub.app.config.metrics;
 
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
-import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
@@ -16,28 +15,24 @@ import com.google.inject.name.Named;
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
-import static com.codahale.metrics.MetricRegistry.name;
-
-public class GraphiteConfiguration
-{
+public class GraphiteConfiguration {
 	@Inject
 	public GraphiteConfiguration(
-		MetricRegistry registry, SubscriptionRoster subscriptions,
-		@Named("graphite.host") String graphiteHost, @Named("graphite.port") int graphitePort ) {
+			MetricRegistry registry, SubscriptionRoster subscriptions,
+			@Named("graphite.host") String graphiteHost, @Named("graphite.port") int graphitePort) {
 
-		createJvmMetrics( registry );
-		createAggregateWebSocketGauge( subscriptions, registry );
-		createReporter( registry, graphiteHost, graphitePort );
+		createJvmMetrics(registry);
+		createAggregateWebSocketGauge(subscriptions, registry);
+		createReporter(registry, graphiteHost, graphitePort);
 	}
 
-	private void createJvmMetrics( MetricRegistry registry )
-	{
-		registry.registerAll( new GarbageCollectorMetricSet() );
-		registry.registerAll( new MemoryUsageGaugeSet() );
-		registry.registerAll( new ThreadStatesGaugeSet() );
+	private void createJvmMetrics(MetricRegistry registry) {
+		registry.registerAll(new KeyPrefixedMetricSet("jvm.gc", new GarbageCollectorMetricSet()));
+		registry.registerAll(new KeyPrefixedMetricSet("jvm.memory", new MemoryUsageGaugeSet()));
+		registry.registerAll(new KeyPrefixedMetricSet("jvm.threads", new ThreadStatesGaugeSet()));
 	}
 
-	private void createAggregateWebSocketGauge( final SubscriptionRoster subscriptions, MetricRegistry registry ) {
+	private void createAggregateWebSocketGauge(final SubscriptionRoster subscriptions, MetricRegistry registry) {
 		registry.register("websocket-clients.total", new Gauge<Integer>() {
 			@Override
 			public Integer getValue() {
@@ -46,14 +41,15 @@ public class GraphiteConfiguration
 		});
 	}
 
-	private void createReporter( MetricRegistry registry, String graphiteHost, int graphitePort ) {
+	private void createReporter(MetricRegistry registry, String graphiteHost, int graphitePort) {
 		final Graphite graphite = new Graphite(new InetSocketAddress(graphiteHost, graphitePort));
 		final GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
 														  .prefixedWith("datahub")
-														  .convertRatesTo( TimeUnit.SECONDS)
+														  .convertRatesTo(TimeUnit.SECONDS)
 														  .convertDurationsTo(TimeUnit.MILLISECONDS)
-														  .filter( MetricFilter.ALL)
+														  .filter(MetricFilter.ALL)
 														  .build(graphite);
 		reporter.start(5, TimeUnit.SECONDS);
 	}
+
 }
