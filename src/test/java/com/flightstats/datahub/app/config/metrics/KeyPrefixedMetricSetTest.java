551e84f79d3c9e3e1583f49e820c3265375679d2
Jason Plumb <jplumb@flightstats.com>
2013-06-04 12:38:18 -0700

    Prefix the jvm metric names.  Repackage.

---
+++ src/test/java/com/flightstats/datahub/app/config/metrics/KeyPrefixedMetricSetTest.java
@@ -0,0 +1,41 @@
+package com.flightstats.datahub.app.config.metrics;
+
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricSet;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class KeyPrefixedMetricSetTest {
+
+	@Test
+	public void testPrefixedKeys() throws Exception {
+		//GIVEN
+		MetricSet set = mock(MetricSet.class);
+		Map<String, Metric> metricsMap = new HashMap<>();
+		Metric mock1 = mock(Metric.class);
+		Metric mock2 = mock(Metric.class);
+		Metric mock3 = mock(Metric.class);
+		Metric mock4 = mock(Metric.class);
+		metricsMap.put("foo", mock1);
+		metricsMap.put("bar", mock2);
+		metricsMap.put("baz", mock3);
+		metricsMap.put("bongo", mock4);
+		when(set.getMetrics()).thenReturn(metricsMap);
+		KeyPrefixedMetricSet testClass = new KeyPrefixedMetricSet("my.custom", set);
+
+		//WHEN
+		Map<String, Metric> result = testClass.getMetrics();
+
+		//THEN
+		assertEquals(result.get("my.custom.foo"), mock1);
+		assertEquals(result.get("my.custom.bar"), mock2);
+		assertEquals(result.get("my.custom.baz"), mock3);
+		assertEquals(result.get("my.custom.bongo"), mock4);
+	}
+}
