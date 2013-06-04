1f7a14cd89161ecc5662f6b97113624aec616178
Jason Plumb <jplumb@flightstats.com>
2013-06-04 11:18:42 -0700

    Add per-channel websocket client count tracking with metrics.

--- src/main/java/com/flightstats/datahub/service/eventing/CustomWebSocketCreator.java
+++ src/main/java/com/flightstats/datahub/service/eventing/CustomWebSocketCreator.java
@@ -1,27 +1,22 @@
 package com.flightstats.datahub.service.eventing;
 
-import com.codahale.metrics.MetricRegistry;
 import com.google.inject.Inject;
 import org.eclipse.jetty.websocket.api.UpgradeRequest;
 import org.eclipse.jetty.websocket.api.UpgradeResponse;
 import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 
-public class CustomWebSocketCreator implements WebSocketCreator
-{
-	private MetricRegistry registry;
+public class CustomWebSocketCreator implements WebSocketCreator {
 	private final SubscriptionRoster subscriptions;
 	private final WebSocketChannelNameExtractor channelNameExtractor;
 
 	@Inject
-	public CustomWebSocketCreator( MetricRegistry registry, SubscriptionRoster subscriptions, WebSocketChannelNameExtractor channelNameExtractor ) {
-		this.registry = registry;
+	public CustomWebSocketCreator(SubscriptionRoster subscriptions, WebSocketChannelNameExtractor channelNameExtractor) {
 		this.subscriptions = subscriptions;
 		this.channelNameExtractor = channelNameExtractor;
 	}
 
 	@Override
 	public Object createWebSocket(UpgradeRequest req, UpgradeResponse resp) {
-//		return new MeteredDataHubWebSocket( registry, new DataHubWebSocket( subscriptions, channelNameExtractor) );
-		return new DataHubWebSocket( subscriptions, channelNameExtractor);
+		return new DataHubWebSocket(subscriptions, channelNameExtractor);
 	}
 }
