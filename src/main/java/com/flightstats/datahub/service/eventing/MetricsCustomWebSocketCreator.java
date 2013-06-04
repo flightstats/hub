ea1db7968b8ef212c0f544606efbfd3d8fcf3807
Jason Plumb <jplumb@flightstats.com>
2013-06-04 14:16:36 -0700

    Use counter instead of gauge.

--- src/main/java/com/flightstats/datahub/service/eventing/MetricsCustomWebSocketCreator.java
+++ src/main/java/com/flightstats/datahub/service/eventing/MetricsCustomWebSocketCreator.java
@@ -1,8 +1,7 @@
 package com.flightstats.datahub.service.eventing;
 
-import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
-import com.google.common.util.concurrent.AtomicLongMap;
 import com.google.inject.Inject;
 import org.eclipse.jetty.websocket.api.UpgradeRequest;
 import org.eclipse.jetty.websocket.api.UpgradeResponse;
@@ -15,7 +14,7 @@ public class MetricsCustomWebSocketCreator implements WebSocketCreator {
 	private final MetricRegistry registry;
 	private final SubscriptionRoster subscriptions;
 	private final WebSocketChannelNameExtractor channelNameExtractor;
-	private final AtomicLongMap<String> channelSubscriberCounts = AtomicLongMap.create();
+	private final Object mutex = new Object();
 
 	@Inject
 	public MetricsCustomWebSocketCreator(MetricRegistry registry, SubscriptionRoster subscriptions, WebSocketChannelNameExtractor channelNameExtractor) {
@@ -30,22 +29,17 @@ public class MetricsCustomWebSocketCreator implements WebSocketCreator {
 		final String channelName = channelNameExtractor.extractChannelName(requestUri);
 		final String meterName = "websocket-clients.channels." + channelName;
 
-		synchronized (channelSubscriberCounts) {
-			long count = channelSubscriberCounts.incrementAndGet(channelName);
-			if (count == 1) {    //First client registering for this channel, time to create a gauge
-				registry.register(meterName, new Gauge<Integer>() {
-					@Override
-					public Integer getValue() {
-						return (int) channelSubscriberCounts.get(channelName);
-					}
-				});
-			}
+		synchronized (mutex) {
+			registry.counter(meterName).inc();
 		}
+
 		return new DataHubWebSocket(subscriptions, channelNameExtractor, new Runnable() {
 			@Override
 			public void run() {
-				synchronized (channelSubscriberCounts) {
-					if (channelSubscriberCounts.decrementAndGet(channelName) == 0) {    //Last remaining client for this channel
+				synchronized (mutex) {
+					Counter counter = registry.counter(meterName);
+					counter.dec();
+					if (counter.getCount() == 0) {    //Last remaining client for this channel
 						registry.remove(meterName);
 					}
 				}
