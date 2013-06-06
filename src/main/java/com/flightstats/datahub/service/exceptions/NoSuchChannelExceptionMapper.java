2d026b0934ab1c89803ffe686bd07488e734f33c
Jason Plumb <jplumb@flightstats.com>
2013-06-06 10:11:02 -0700

    add test

--- src/main/java/com/flightstats/datahub/service/exceptions/NoSuchChannelExceptionMapper.java
+++ src/main/java/com/flightstats/datahub/service/exceptions/NoSuchChannelExceptionMapper.java
@@ -3,16 +3,15 @@ package com.flightstats.datahub.service.exceptions;
 import com.flightstats.datahub.model.exception.NoSuchChannelException;
 import com.flightstats.services.common.jersey.exception.mappers.AbstractExceptionMapper;
 import com.google.inject.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Singleton
 @Provider
 public class NoSuchChannelExceptionMapper extends AbstractExceptionMapper<NoSuchChannelException> {
 
-	protected Response.Status getResponseCode() { return Response.Status.NOT_FOUND; }
+	protected Response.Status getResponseCode() {
+		return Response.Status.NOT_FOUND;
+	}
 }
--- src/test/java/com/flightstats/datahub/service/ChannelResourceTest.java
+++ src/test/java/com/flightstats/datahub/service/ChannelResourceTest.java
@@ -5,14 +5,19 @@ import com.flightstats.datahub.model.ChannelConfiguration;
 import com.flightstats.datahub.model.ChannelCreationRequest;
 import com.flightstats.datahub.model.exception.AlreadyExistsException;
 import com.flightstats.datahub.model.exception.InvalidRequestException;
+import com.flightstats.rest.HalLink;
 import com.flightstats.rest.Linked;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import static junit.framework.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -35,7 +40,7 @@ public class ChannelResourceTest {
 													  .build();
 		UriInfo uriInfo = mock(UriInfo.class);
 		ChannelDao dao = mock(ChannelDao.class);
-		CreateChannelValidator createChannelValidator = mock( CreateChannelValidator.class );
+		CreateChannelValidator createChannelValidator = mock(CreateChannelValidator.class);
 		ChannelHypermediaLinkBuilder linkBuilder = mock(ChannelHypermediaLinkBuilder.class);
 
 		when(uriInfo.getRequestUri()).thenReturn(URI.create("http://path/to"));
@@ -45,24 +50,24 @@ public class ChannelResourceTest {
 		when(linkBuilder.buildLatestUri(channelName)).thenReturn(URI.create(latestUri));
 		when(linkBuilder.buildWsLinkFor(channelName)).thenReturn(URI.create(wsUri));
 
-		ChannelResource testClass = new ChannelResource(dao, linkBuilder, createChannelValidator );
+		ChannelResource testClass = new ChannelResource(dao, linkBuilder, createChannelValidator);
 
 		Response response = testClass.createChannel(channelCreationRequest);
 
 		verify(dao).createChannel(channelName);
 
 		assertEquals(201, response.getStatus());
-		assertEquals(new URI( channelUri ), response.getMetadata().getFirst( "location" ));
+		assertEquals(new URI(channelUri), response.getMetadata().getFirst("location"));
 		assertEquals(expected, response.getEntity());
 	}
 
-	@Test( expected = InvalidRequestException.class )
+	@Test(expected = InvalidRequestException.class)
 	public void testChannelCreation_emptyChannelName() throws Exception {
 		String channelName = "  ";
 
 		ChannelCreationRequest channelCreationRequest = new ChannelCreationRequest(channelName);
 		ChannelDao dao = mock(ChannelDao.class);
-		CreateChannelValidator createChannelValidator = new CreateChannelValidator( dao );
+		CreateChannelValidator createChannelValidator = new CreateChannelValidator(dao);
 		ChannelHypermediaLinkBuilder linkBuilder = mock(ChannelHypermediaLinkBuilder.class);
 
 		ChannelResource testClass = new ChannelResource(dao, linkBuilder, createChannelValidator);
@@ -70,17 +75,47 @@ public class ChannelResourceTest {
 		testClass.createChannel(channelCreationRequest);
 	}
 
-	@Test( expected = AlreadyExistsException.class )
+	@Test(expected = AlreadyExistsException.class)
 	public void testChannelCreation_channelAlreadyExists() throws Exception {
 		String channelName = "zippy";
 
 		ChannelCreationRequest channelCreationRequest = new ChannelCreationRequest(channelName);
 		ChannelDao dao = mock(ChannelDao.class);
-		CreateChannelValidator createChannelValidator = new CreateChannelValidator( dao );
+		CreateChannelValidator createChannelValidator = new CreateChannelValidator(dao);
 		ChannelHypermediaLinkBuilder linkBuilder = mock(ChannelHypermediaLinkBuilder.class);
 
 		ChannelResource testClass = new ChannelResource(dao, linkBuilder, createChannelValidator);
-		when( dao.channelExists( any( String.class ) ) ).thenReturn( true );
+		when(dao.channelExists(any(String.class))).thenReturn(true);
 		testClass.createChannel(channelCreationRequest);
+	}
+
+	@Test
+	public void testGetChannels() throws Exception {
+		//GIVEN
+		ChannelConfiguration channel1 = new ChannelConfiguration("foo", null);
+		ChannelConfiguration channel2 = new ChannelConfiguration("bar", null);
+		Iterable<ChannelConfiguration> channels = Arrays.asList(channel1, channel2);
+		String channel1Uri = "http://superfoo";
+		String channel2Uri = "http://superbar";
+
+		ChannelDao dao = mock(ChannelDao.class);
+		ChannelHypermediaLinkBuilder linkBuilder = mock(ChannelHypermediaLinkBuilder.class);
+
+		ChannelResource testClass = new ChannelResource(dao, linkBuilder, mock(CreateChannelValidator.class));
+
+		//WHEN
+		when(dao.getChannels()).thenReturn(channels);
+		when(linkBuilder.buildChannelUri(channel1.getName())).thenReturn(URI.create(channel1Uri));
+		when(linkBuilder.buildChannelUri(channel2.getName())).thenReturn(URI.create(channel2Uri));
+
+		Response result = testClass.getChannels();
+
+		//THEN
+
+		List<HalLink> links = ((Linked<Map>) result.getEntity()).getHalLinks().getLinks();
+		assertNull(((Linked<Map>) result.getEntity()).getObject());
+		assertEquals(2, links.size());
+		assertEquals(new HalLink(channel1.getName(), URI.create(channel1Uri)), links.get(0));
+		assertEquals(new HalLink(channel2.getName(), URI.create(channel2Uri)), links.get(1));
 	}
 }
