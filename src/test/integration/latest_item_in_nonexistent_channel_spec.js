e8ad23c18d176f54a3452bc7e8bdebe30978c767
Chris Kessel <ckessel@flightstats.com>
2013-08-29 12:12:31 -0700

    Unregister listeners from hazelcast when websocket clients disconnect.

---
+++ src/test/integration/latest_item_in_nonexistent_channel_spec.js
@@ -0,0 +1,13 @@
+require('./integration_config.js');
+
+var channelName = utils.randomChannelName();
+var testName = "latest_item_in_nonexistant_channel_spec";
+utils.configureFrisby();
+
+var latestLink = dataHubUrlBase + "/channel/"+channelName+"/latest";
+console.log(latestLink);
+frisby.create(testName + ":Test latest item is 404 when channel dosn't exist")
+    .get(latestLink)
+    .expectStatus(404)
+    .toss();
+
