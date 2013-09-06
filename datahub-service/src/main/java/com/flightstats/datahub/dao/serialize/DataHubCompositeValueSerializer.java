8bec27ff0fc26d22d56010fb6d8a49e9751837d4
Jason Plumb <jplumb@flightstats.com>
2013-09-06 16:30:09 -0700

    Change the serialization format to be more extensible.

--- datahub-service/src/main/java/com/flightstats/datahub/dao/serialize/DataHubCompositeValueSerializer.java
+++ datahub-service/src/main/java/com/flightstats/datahub/dao/serialize/DataHubCompositeValueSerializer.java
@@ -4,6 +4,7 @@ import com.flightstats.datahub.model.DataHubCompositeValue;
 import com.google.common.base.Optional;
 import me.prettyprint.cassandra.serializers.AbstractSerializer;
 import me.prettyprint.hector.api.Serializer;
+import me.prettyprint.hector.api.exceptions.HectorSerializationException;
 import org.apache.thrift.TBaseHelper;
 
 import java.io.ByteArrayOutputStream;
@@ -14,6 +15,7 @@ public class DataHubCompositeValueSerializer extends AbstractSerializer<DataHubC
 
 	private final static DataHubCompositeValueSerializer serializer = new DataHubCompositeValueSerializer();
 	public static final int BYTES_PER_INT = Integer.SIZE / Byte.SIZE;
+	static final byte FORMAT_VERSION = 66;
 
 	public static Serializer<DataHubCompositeValue> get() {
 		return serializer;
@@ -22,60 +24,99 @@ public class DataHubCompositeValueSerializer extends AbstractSerializer<DataHubC
 	private final OptionalStringSerializer optionalStringSerializer = OptionalStringSerializer.get();
 	private final ByteBlockReader byteBlockReader = new ByteBlockReader();
 
-	@Override
+    /**
+     * This format:
+     *
+     * vaaaaxxxx[d1]aaaaxxxx[d2]
+     *
+     * where:  v      the version byte
+     *         aaaa   the 4-byte field id (int)
+     *         xxxx   the 4-byte field length (int)
+     *         [d1]   the data for the field, variable length
+     *
+     */
+    @Override
+    public DataHubCompositeValue fromByteBuffer(ByteBuffer byteBuffer) {
+        ByteBuffer correctedBuffer = TBaseHelper.rightSize(byteBuffer);
+        correctedBuffer.rewind();
+
+        byte versionByte = byteBuffer.get();
+        //Note: In the future, we could dispatch to an appropriate version reader if/when this changes.
+        if(versionByte != FORMAT_VERSION){
+            throw new HectorSerializationException("Unrecognized format version: " + versionByte);
+        }
+        return deserializeCompositeValue(correctedBuffer);
+    }
+
+    private DataHubCompositeValue deserializeCompositeValue(ByteBuffer correctedBuffer) {
+        Optional<String> contentType = Optional.absent();
+        Optional<String> contentLanguage = Optional.absent();
+        byte[] valueData = new byte[]{};
+
+        while(correctedBuffer.hasRemaining()){
+            FieldId fieldId = getFieldId(correctedBuffer);
+            if(fieldId == null){
+                byteBlockReader.readByteBlock(correctedBuffer); //consume field and ignore content
+                continue;
+            }
+            switch(fieldId){
+                case CONTENT_TYPE:
+                    contentType = optionalStringSerializer.fromByteBuffer(correctedBuffer);
+                    break;
+                case CONTENT_LANGUAGE:
+                    contentLanguage = optionalStringSerializer.fromByteBuffer(correctedBuffer);
+                    break;
+                case CONTENT:
+                    valueData = byteBlockReader.readByteBlock(correctedBuffer);
+                    break;
+            }
+        }
+        return new DataHubCompositeValue(contentType, contentLanguage, valueData);
+    }
+
+    private FieldId getFieldId(ByteBuffer byteBuffer) {
+        try {
+            return FieldId.fromInt(byteBuffer.getInt());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    @Override
 	public ByteBuffer toByteBuffer(DataHubCompositeValue obj) {
 
 		try {
-			ByteArrayOutputStream out = new ByteArrayOutputStream(calculateBufferLength(obj));
-
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+            out.write(FORMAT_VERSION);
 			writeContentType(obj, out);
 			writeContentLanguage(obj, out);
-			writeData(obj, out);
-
+			writeContent(obj, out);
 			return ByteBuffer.wrap(out.toByteArray());
 		} catch (IOException e) {
 			throw new RuntimeException("Unable to serialize DataHubCompositeValue: ", e);
 		}
 	}
 
-	@Override
-	public DataHubCompositeValue fromByteBuffer(ByteBuffer byteBuffer) {
-		ByteBuffer correctedBuffer = TBaseHelper.rightSize(byteBuffer);
-		correctedBuffer.rewind();
-
-		Optional<String> contentType = optionalStringSerializer.fromByteBuffer(correctedBuffer);
-		Optional<String> contentLanguage = optionalStringSerializer.fromByteBuffer(correctedBuffer);
-
-		byte[] valueData = readValueData(correctedBuffer);
-
-		return new DataHubCompositeValue(contentType, contentLanguage, valueData);
-	}
-
-	private int calculateBufferLength(DataHubCompositeValue value) {
-		return BYTES_PER_INT + optionalLength(value.getContentType()) +
-				BYTES_PER_INT + optionalLength(value.getContentLanguage()) +
-				BYTES_PER_INT + value.getDataLength();
-	}
-
-	private int optionalLength(Optional<String> contentType) {
-		return contentType.isPresent() ? contentType.get().length() : 0;
-	}
-
-	private void writeData(DataHubCompositeValue obj, ByteArrayOutputStream out) throws IOException {
-		out.write(ByteBuffer.allocate(BYTES_PER_INT).putInt(obj.getDataLength()).array());
-		out.write(obj.getData());
-	}
-
 	private void writeContentType(DataHubCompositeValue obj, ByteArrayOutputStream out) throws IOException {
-		out.write(optionalStringSerializer.toBytes(obj.getContentType()));
+        writeOptionalString(out, FieldId.CONTENT_TYPE, obj.getContentType());
 	}
 
-	private void writeContentLanguage(DataHubCompositeValue obj, ByteArrayOutputStream out) throws IOException {
-		out.write(optionalStringSerializer.toBytes(obj.getContentLanguage()));
+    private void writeContentLanguage(DataHubCompositeValue obj, ByteArrayOutputStream out) throws IOException {
+		writeOptionalString(out, FieldId.CONTENT_LANGUAGE, obj.getContentLanguage());
 	}
 
-	private byte[] readValueData(ByteBuffer correctedBuffer) {
-		return byteBlockReader.readByteBlock(correctedBuffer);
-	}
+    private void writeOptionalString(ByteArrayOutputStream out, FieldId fieldId, Optional<String> optionalString) throws IOException {
+        writeInt(out, fieldId.getId());
+        out.write(optionalStringSerializer.toBytes(optionalString));
+    }
 
+    private void writeContent(DataHubCompositeValue obj, ByteArrayOutputStream out) throws IOException {
+        writeInt(out, FieldId.CONTENT.getId());
+        writeInt(out, obj.getDataLength());
+        out.write(obj.getData());
+    }
+
+    private void writeInt(ByteArrayOutputStream out, int id) throws IOException {
+        out.write(ByteBuffer.allocate(BYTES_PER_INT).putInt(id).array());
+    }
 }
