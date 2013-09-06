dd97901892ff2aba6df7d55d0cc1da0501905229
Jason Plumb <jplumb@flightstats.com>
2013-09-06 16:30:09 -0700

    Change the serialization format to be more extensible.

--- datahub-service/src/test/java/com/flightstats/datahub/dao/serialize/DataHubCompositeValueSerializerTest.java
+++ datahub-service/src/test/java/com/flightstats/datahub/dao/serialize/DataHubCompositeValueSerializerTest.java
@@ -3,6 +3,7 @@ package com.flightstats.datahub.dao.serialize;
 import com.flightstats.datahub.model.DataHubCompositeValue;
 import com.google.common.base.Optional;
 import me.prettyprint.hector.api.Serializer;
+import me.prettyprint.hector.api.exceptions.HectorSerializationException;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
@@ -18,6 +19,47 @@ public class DataHubCompositeValueSerializerTest {
         ByteBuffer byteBuffer = testClass.toByteBuffer(instance);
         DataHubCompositeValue result = testClass.fromByteBuffer(byteBuffer);
         assertEquals(result, instance);
+    }
+
+    @Test
+    public void testFromBytes_UnknownField() throws Exception {
+        Serializer<DataHubCompositeValue> testClass = DataHubCompositeValueSerializer.get();
+        DataHubCompositeValue instance = new DataHubCompositeValue(Optional.of("nomatta"), Optional.of("en, mi"), "God lived as a devil dog.".getBytes());
+        ByteBuffer byteBuffer = testClass.toByteBuffer(instance);
+        byte[] bytes = byteBuffer.array();
+        //Stomp over the first field, which is currently the content type.  It's at offset 1 (0th byte is version)
+        System.arraycopy(new byte[]{0, 0, 1, 2}, 0, bytes, 1, 4);
+
+        DataHubCompositeValue result = testClass.fromBytes(bytes);
+        DataHubCompositeValue expected = new DataHubCompositeValue(Optional.<String>absent(), instance.getContentLanguage(), instance.getData());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFromBytes_MissingField() throws Exception {
+        Serializer<DataHubCompositeValue> testClass = DataHubCompositeValueSerializer.get();
+        DataHubCompositeValue instance = new DataHubCompositeValue(Optional.of("a"), Optional.of("b"), "c".getBytes());
+        ByteBuffer byteBuffer = testClass.toByteBuffer(instance);
+        byte[] bytes = byteBuffer.array();
+        byte[] newBytes = new byte[bytes.length - 9];   // 9 = 4-byte int(field id) + 4-byte int(length) + 1 byte "a" string
+        newBytes[0] = bytes[0];
+        // Skip bytes 0-9 (inclusive)
+        System.arraycopy(bytes, 10, newBytes, 1, bytes.length - (4 + 4 + 1) - 1);
+
+        DataHubCompositeValue result = testClass.fromBytes(newBytes);
+        DataHubCompositeValue expected = new DataHubCompositeValue(Optional.<String>absent(), instance.getContentLanguage(), instance.getData());
+        assertEquals(expected, result);
+    }
+
+    @Test(expected = HectorSerializationException.class)
+    public void testFromBytes_VersionMismatch() throws Exception {
+        Serializer<DataHubCompositeValue> testClass = DataHubCompositeValueSerializer.get();
+        DataHubCompositeValue instance = new DataHubCompositeValue(Optional.of("a"), Optional.of("b"), "c".getBytes());
+        ByteBuffer byteBuffer = testClass.toByteBuffer(instance);
+        byte[] bytes = byteBuffer.array();
+        byte[] newBytes = new byte[bytes.length];
+        newBytes[0] = (byte) (bytes[0] + 1);
+        testClass.fromBytes(newBytes);
     }
 
 }
