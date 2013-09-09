3d9de493529b4bf9fe62bab195ff1e96f568ede4
aashishpatel <apatel@flightstats.com>
2013-09-09 09:55:38 -0700

    fixed broken integration test

--- datahub-proxy-service/src/test/integration/integration_config.js
+++ datahub-proxy-service/src/test/integration/integration_config.js
@@ -6,8 +6,8 @@
 frisby = require('frisby');
 utils = require('./utils.js');
 
-dataHubUrlBase = 'http://datahub-01.cloud-east.dev:8080';
-dataHubProxyUrlBase = 'http://datahub-proxy-01.cloud-east.dev:8080';
+dataHubUrlBase = 'http://datahub.svc.dev';
+dataHubProxyUrlBase = 'http://datahub-proxy.svc.dev';
 
 // Try to load a local file if it exists. Allows folks to trump default values defined above.
 try {
