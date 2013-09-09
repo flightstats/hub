da84de496c52efcc02d94aef73118bb1fabad94b
aashishpatel <apatel@flightstats.com>
2013-09-09 09:10:44 -0700

    initial proxy service project.  junit tests and basica POST and GET integration tests.  Still need to work on deployment etc.

---
+++ datahub-proxy-service/src/main/java/com/flightstats/datahubproxy/service/HealthCheck.java
@@ -0,0 +1,23 @@
+package com.flightstats.datahubproxy.service;
+
+import com.google.inject.Inject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/health")
+public class HealthCheck {
+
+    @Inject
+    public HealthCheck() {
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String check() {
+        return "OK";
+    }
+
+}
