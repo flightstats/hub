b88b9806ba4f5ef0a087442cf73dccde68f984a4
Jason Plumb <jplumb@flightstats.com>
2013-09-10 14:55:51 -0700

    fix broken integration test

--- datahub-service/src/test/integration/content_encoding_header_spec.js
+++ datahub-service/src/test/integration/content_encoding_header_spec.js
@@ -13,13 +13,13 @@ utils.runInTestChannel(channelName, function () {
     // Note: We have to use request directly here, because Frisby insists on having a content-type specified.
     frisby.create(testName + ": Testing the content-encoding header")
         .post(thisChannelResource, null, { body: messageText})
-        .addHeader("Content-Encoding", "gzip")
         .expectStatus(201)
         .afterJSON(function (result) {
             var valueUrl = result['_links']['self']['href'];
             console.info("yeah " + valueUrl);
             frisby.create("Fetching to confirm header")
                 .get(valueUrl)
+				.addHeader('accept-encoding', 'gzip')
                 .expectHeader('content-encoding', 'gzip')
                 .toss()
         })
