e5e56773d973554051d17f1e82c8e1c347d383c8
Jason Plumb <jplumb@flightstats.com>
2013-10-31 09:59:13 -0700

    merge with master, add test

--- datahub-service/src/test/java/com/flightstats/datahub/service/SingleChannelResourceTest.java
+++ datahub-service/src/test/java/com/flightstats/datahub/service/SingleChannelResourceTest.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
