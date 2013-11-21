071d479bf74ac3da6b46d24165ec3689509eddb2
Jason Plumb <jplumb@flightstats.com>
2013-11-21 11:08:10 -0800

    fix the binary test to use jasmine asynchronous support.

--- datahub-service/src/test/integration/insert_binary_spec.js
+++ datahub-service/src/test/integration/insert_binary_spec.js
@@ -5,31 +5,46 @@ var channelName = utils.randomChannelName();
 var thisChannelResource = channelUrl + "/" + channelName;
 var testName = "insert_binary_spec";
 
-utils.configureFrisby();
-
 utils.runInTestChannel(channelName, function () {
 
     catUrl = 'http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg';
     console.info(testName + ": Fetching some binary content to insert....");
-    utils.download(catUrl, function (imagedata) {
 
-        console.info("Inserting an image (" + imagedata.length + " bytes).");
+    var error;
+    var comparisonHasBeenFetched = false;
+    var returnedHref = "";
+    var hubdata = [1];
+    var imagedata = [0];
 
-        buf = new Buffer(imagedata, 'binary');
-        request.post({url: thisChannelResource, headers: {"Content-Type": "image/jpeg"}, body: buf}, function (error, response, body) {
-            expect(error).toBeNull();
+    runs(function(){
+        utils.download(catUrl, function (imgdata) {
+        imagedata = imgdata;
+        console.info("Inserting an image (" + imgdata.length + " bytes).");
+        buf = new Buffer(imgdata, 'binary');
+        request.post({url: thisChannelResource, headers: {"Content-Type": "image/jpeg"}, body: buf}, function (err, response, body) {
+            error = err;
             resultObj = JSON.parse(body);
-            expect(resultObj['_links']['channel']['href']).toBe(thisChannelResource);
-
+            returnedHref = resultObj['_links']['channel']['href'];
             var valueUrl = resultObj['_links']['self']['href'];
             console.info(testName + ": Now to retrieve and compare cats: " + valueUrl);
-            utils.download(valueUrl, function (hubdata) {
-                console.info(testName + ": Checking to see if the cats match...");
-                expect(hubdata.length).toEqual(imagedata.length);
-                expect(hubdata).toEqual(imagedata);
-                console.info(testName + ": The cats match!");
-
+            utils.download(valueUrl, function (data) {
+                hubdata = data;
+                comparisonHasBeenFetched = true;
             });
         });
+        });
+    });
+
+    waitsFor(function(){
+        return comparisonHasBeenFetched;
+    }, 5000);
+
+    runs(function(){
+        expect(error).toBeNull();
+        expect(resultObj['_links']['channel']['href']).toBe(thisChannelResource);
+        console.info(testName + ": Checking to see if the cats match...");
+        expect(hubdata.length).toEqual(imagedata.length);
+        expect(hubdata).toEqual(imagedata);
+        console.info(testName + ": The cats match!");
     });
 });
