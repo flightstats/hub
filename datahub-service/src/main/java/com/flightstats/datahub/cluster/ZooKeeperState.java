1e60eaf715e4330e35b02761a52170e1b8fa85d8
Greg Moulliet <gmoulliet@flightstats.com>
2014-01-13 10:54:42 -0800

    hunting down memory issue

--- datahub-service/src/main/java/com/flightstats/datahub/cluster/ZooKeeperState.java
+++ datahub-service/src/main/java/com/flightstats/datahub/cluster/ZooKeeperState.java
@@ -1,22 +1,28 @@
 package com.flightstats.datahub.cluster;
 
+import com.google.inject.Inject;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.framework.state.ConnectionStateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ZooKeeperState {
     private final static Logger logger = LoggerFactory.getLogger(ZooKeeperState.class);
 
     private ConnectionState connectionState = ConnectionState.SUSPENDED;
     private final ConnectionStateListener connectionStateListener;
+    private final AtomicBoolean stopWorking = new AtomicBoolean(false);
 
+    @Inject
     public ZooKeeperState() {
         connectionStateListener = new ConnectionStateListener() {
             @Override
             public void stateChanged(CuratorFramework client, ConnectionState newState) {
                 connectionState = newState;
+                stopWorking.set(!isHealthy());
                 logger.info("state change from " + connectionState + " to " + newState);
             }
         };
@@ -33,5 +39,9 @@ public class ZooKeeperState {
                 return true;
         }
         return false;
+    }
+
+    public boolean shouldStopWorking() {
+        return stopWorking.get();
     }
 }
