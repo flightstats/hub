42544792300266c66b56b50a172ac06cf8aaa891
Greg Moulliet <gmoulliet@flightstats.com>
2014-02-04 21:05:40 -0800

    cleaning up replication integration test

--- src/test/integration/replication_remote_spec.js
+++ src/test/integration/replication_remote_spec.js
@@ -1,120 +1,116 @@
 require('./integration_config.js');
 var request = require('request');
 
-var testName = "replication_remote_spec";
+/**
+ * This test could be fragile relative to time.  If a remote server is running slowly, this may fail.
+ */
+describe("replication_remote_spec", function () {
 
-describe("replication suite", function () {
+    var channelName = utils.randomChannelName();
+    var remoteUrl = "http://" + remoteDomain + "/channel";
+    it("creates remote channel", function (done) {
+        console.info(channelName);
+        request.post({url: remoteUrl,
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ "name": channelName})},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(201);
+                done();
+            });
+    });
 
-    it("doing all replication", function () {
+    it("adds remote items", function (done) {
+        addItem();
+        addItem(done);
+    });
 
-        var channelName = utils.randomChannelName();
+    it("creates local replication config", function (done) {
+        request.put({url: hubUrlBase + "/replication/" + remoteDomain,
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ historicalDays: 1, excludeExcept: [channelName] })},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(201);
+                done();
+            });
+    });
 
-        var jsonBody = JSON.stringify({
-            historicalDays: 1,
-            excludeExcept: [channelName]
-        });
-        var remoteUrl = "http://" + remoteDomain + "/channel";
-        var replicationResource = hubUrlBase + "/replication/" + remoteDomain;
+    it("waits for connection to remote server", function () {
+        var connected = false;
 
-        var createdChannels = false;
-        var createdReplication = false;
-        var gotReplication = false;
-
-        runs(function () {
-            createChannel();
-        });
-
-        waitsFor(function () {
-            return createdChannels;
-        }, 15000);
-
-        runs(function () {
-            //start replication process
-            request.put({url: replicationResource,
-                    headers: {"Content-Type": "application/json"},
-                    body: jsonBody},
-                function (err, response, body) {
-                    //todo verify
-                    expect(err).toBeNull();
-                    console.info("set up replication");
-                    expect(response.statusCode).toBe(201);
-                    createdReplication = true;
-                });
-        });
-
-        waitsFor(function () {
-            return createdReplication;
-        }, 15000);
-
-        runs(function () {
-            addData("one");
-            addData("two");
-        });
-
-        waits(5000);
-
-        runs(function () {
-            addData("three");
-            addData("four");
-        });
-
-        waits(5000);
-
-        runs(function () {
-            addData("five");
-            addData("six");
-        });
-
-        waits(5000);
+        function replicationCallback(body) {
+            var statuses = JSON.parse(body)['status'];
+            for (var i = 0; i < statuses.length; i++) {
+                var status = statuses[i];
+                if (status.name === channelName && status.connected === true) {
+                    connected = true;
+                }
+            }
+            if (!connected) {
+                getReplication(replicationCallback);
+            }
+        }
 
         runs(function() {
-            getReplication();
-        })
+            getReplication(replicationCallback);
+        });
 
-        waitsFor(function () {
-            return gotReplication;
-        }, 10000)
+        waitsFor(function() {
+            return connected;
+        }, 60000);
 
-
-
-        function getReplication() {
-            request.get({url: hubUrlBase + "/replication",
-                    headers: {"Content-Type": "application/json"} },
-                function (err, response, body) {
-                    expect(err).toBeNull();
-                    console.info("get repl " + response.statusCode + " " + body);
-                    expect(response.statusCode).toBe(200);
-                    resultObj = JSON.parse(body);
-                    expect(resultObj['status'][0]['name']).toBe(channelName);
-                    expect(resultObj['status'][0]['replicationLatest']).toBe(1005);
-                    gotReplication = true;
-                });
-        }
-
-        function createChannel() {
-            console.info("creating channel " + channelName + " at " + remoteUrl);
-            request.post({url: remoteUrl,
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify({ "name": channelName})},
-                function (err, response, body) {
-                    expect(err).toBeNull();
-                    expect(response.statusCode).toBe(201);
-                    console.info("creating channel " + remoteUrl + "/" + channelName + " response " + response.statusCode);
-                    createdChannels = true;
-                });
-
-        }
-
-        function addData(data) {
-            request.post({url: remoteUrl + "/" + channelName,
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify({ "data": data})},
-                function (err, response, body) {
-                    expect(err).toBeNull();
-                    expect(response.statusCode).toBe(201);
-                    console.info("added data to channel " + channelName + " " + data);
-                });
-        }
     });
+
+    it("adds more remote items", function (done) {
+        addItem();
+        addItem(done);
+    });
+
+    it("verifies replication progress", function () {
+        var verified = false;
+
+        function replicationCallback(body) {
+            var status = JSON.parse(body)['status'][0];
+            if (status['name'] === channelName && status['replicationLatest'] === 1003) {
+                verified = true;
+            } else {
+                getReplication(replicationCallback);
+            }
+        }
+
+        runs(function() {
+            getReplication(replicationCallback);
+        });
+
+        waitsFor(function() {
+            return verified;
+        }, 60000);
+
+    });
+
+    //todo - gfm - 2/4/14 - this could also verify that the remote sequence is the same as the local, may need to span times
+
+    function getReplication(callback) {
+        request.get({url: hubUrlBase + "/replication",
+                headers: {"Content-Type": "application/json"}},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(200);
+                callback(body);
+            });
+    };
+
+    function addItem(done) {
+        done = done || function () { };
+        request.post({url: remoteUrl + "/" + channelName,
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ "data": Date.now()})},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(201);
+                done();
+            });
+    }
 
 });
