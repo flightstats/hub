a1fb6c8489118815affb5f36361024107895aa32
Greg Moulliet <gmoulliet@flightstats.com>
2014-03-31 19:11:41 -0700

    66487276 - replication status ordering

--- src/main/java/com/flightstats/hub/replication/ReplicationBean.java
+++ src/main/java/com/flightstats/hub/replication/ReplicationBean.java
@@ -1,6 +1,8 @@
 package com.flightstats.hub.replication;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 /**
  *
@@ -11,8 +13,21 @@ public class ReplicationBean {
     Collection<ReplicationStatus> status;
 
     public ReplicationBean(Collection<ReplicationDomain> domains, Collection<ReplicationStatus> status) {
-        this.domains = domains;
-        this.status = status;
+        this.domains = new TreeSet<>(new Comparator<ReplicationDomain>() {
+
+            @Override
+            public int compare(ReplicationDomain rd1, ReplicationDomain rd2) {
+                return rd1.getDomain().compareTo(rd2.getDomain());
+            }
+        });
+        this.domains.addAll(domains);
+        this.status = new TreeSet<>(new Comparator<ReplicationStatus>() {
+            @Override
+            public int compare(ReplicationStatus rs1, ReplicationStatus rs2) {
+                return rs1.getUrl().compareTo(rs2.getUrl());
+            }
+        });
+        this.status.addAll(status);
     }
 
     public Collection<ReplicationDomain> getDomains() {
