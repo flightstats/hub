99c16f801b56f317fc90a04af0abc5a7e15b7622
Greg Moulliet <gmoulliet@flightstats.com>
2014-04-03 13:57:33 -0700

    adding payload check

--- src/test/java/com/flightstats/hub/replication/ChannelVerifier.java
+++ src/test/java/com/flightstats/hub/replication/ChannelVerifier.java
@@ -7,6 +7,8 @@ import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -20,38 +22,53 @@ public class ChannelVerifier implements Callable<VerifierResult> {
     private JsonNode channelStatus;
     private String replicationUri;
     private final int frequencyHours;
-    private final int verificationPercent;
+    private final int payloadPercent;
     private final SequenceFinder sequenceFinder;
     private final ChannelUtils channelUtils;
+    private final Random random = new Random();
 
-    public ChannelVerifier(JsonNode channelStatus, String replicationUri, int frequencyHours, int verificationPercent,
+    public ChannelVerifier(JsonNode channelStatus, String replicationUri, int frequencyHours, int payloadPercent,
                            SequenceFinder sequenceFinder, ChannelUtils channelUtils) {
         this.channelStatus = channelStatus;
         this.replicationUri = replicationUri;
         this.frequencyHours = frequencyHours;
-        this.verificationPercent = verificationPercent;
+        this.payloadPercent = payloadPercent;
         this.sequenceFinder = sequenceFinder;
         this.channelUtils = channelUtils;
     }
 
     @Override
     public VerifierResult call() throws Exception {
-        VerifierResult result = new VerifierResult();
         long sequence = getStartSequence();
         int sourceLatest = getSourceLatest();
+        String sourceUrl = getSourceUrl();
         String channelUri = replicationUri + "channel/" + getName() + "/";
+        VerifierResult result = new VerifierResult(channelUri);
         logger.info("verifying {} from {} to {}, {} items", channelUri, sequence, sourceLatest, sourceLatest - sequence);
         while (sequence <= sourceLatest) {
-            Optional<Content> content = channelUtils.getContent(channelUri, sequence);
+            Optional<Content> replicatedContent = channelUtils.getContent(channelUri, sequence);
             result.incrementSequencesChecked();
-            //todo - gfm - 4/2/14 - also check payload
-            if (!content.isPresent()) {
+            if (!replicatedContent.isPresent()) {
                 result.addMissingSequence(String.valueOf(sequence));
+            } else  {
+                if (shouldVerifyPayload()) {
+                    Optional<Content> sourceContent = channelUtils.getContent(sourceUrl, sequence);
+                    if (sourceContent.isPresent()) {
+                        result.incrementPayloadsChecked();
+                        if (!Arrays.equals(replicatedContent.get().getData(), sourceContent.get().getData())) {
+                            result.addMissingSequence(String.valueOf(sequence));
+                        }
+                    }
+                }
             }
             sequence+=1;
         }
         logger.info("completed " + channelUri + " " + result);
         return result;
+    }
+
+    private boolean shouldVerifyPayload() {
+        return random.nextInt(100) < payloadPercent;
     }
 
     private long getStartSequence() {
