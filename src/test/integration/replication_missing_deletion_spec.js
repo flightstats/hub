62f206d85ea37ae7f8cd65a2f5859c500887f5f4
Greg Moulliet <gmoulliet@flightstats.com>
2014-04-10 17:37:22 -0700

    69223650 - cleaning up test

--- src/test/integration/replication_missing_deletion_spec.js
+++ src/test/integration/replication_missing_deletion_spec.js
@@ -1,9 +1,5 @@
 require('./integration_config.js');
 
-var jsonBody = JSON.stringify({
-    historicalDays: -1,
-    includeExcept: ["two", "one"]
-});
 var replicationResource = hubUrlBase + "/replication/nonExistent";
 var testName = "replication_missing_deletion_spec";
 utils.configureFrisby();
