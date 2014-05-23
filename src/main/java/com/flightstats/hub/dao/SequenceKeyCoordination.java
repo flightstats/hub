5e3156d5b6e003b72128f18f04d81f37a524a2ea
Greg Moulliet <gmoulliet@flightstats.com>
2014-05-23 14:19:29 -0700

    68687320 - clearing last updated after delete

--- src/main/java/com/flightstats/hub/dao/SequenceKeyCoordination.java
+++ src/main/java/com/flightstats/hub/dao/SequenceKeyCoordination.java
@@ -5,39 +5,25 @@ import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.model.SequenceContentKey;
 import com.flightstats.hub.websocket.WebsocketPublisher;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.google.common.primitives.Longs;
 import com.google.inject.Inject;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.shared.SharedValue;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- */
+//todo - gfm - 5/23/14 - clean this up
 public class SequenceKeyCoordination implements KeyCoordination {
     private final static Logger logger = LoggerFactory.getLogger(SequenceKeyCoordination.class);
 
     private final WebsocketPublisher websocketPublisher;
     private final CuratorFramework curator;
-    private final LoadingCache<String, SharedValue> cache;
 
     @Inject
     public SequenceKeyCoordination(WebsocketPublisher websocketPublisher,
-                                   final CuratorFramework curator) {
+                                      final CuratorFramework curator) {
         this.websocketPublisher = websocketPublisher;
         this.curator = curator;
-        cache = CacheBuilder.newBuilder().build(new CacheLoader<String, SharedValue>() {
-            @Override
-            public SharedValue load(String key) throws Exception {
-                SharedValue sharedValue = new SharedValue(curator, key, Longs.toByteArray(SequenceContentKey.START_VALUE));
-                sharedValue.start();
-                return sharedValue;
-            }
-        });
     }
 
     @Override
@@ -47,16 +33,18 @@ public class SequenceKeyCoordination implements KeyCoordination {
     }
 
     @Timed(name = "sequence.setLastUpdated")
-    private void setLastUpdateKey(final String channelName, final ContentKey key) {
-        final SequenceContentKey sequence = (SequenceContentKey) key;
+    private void setLastUpdateKey(String channelName, ContentKey key) {
         try {
+            SequenceContentKey sequence = (SequenceContentKey) key;
             byte[] bytes = Longs.toByteArray(sequence.getSequence());
-            SharedValue sharedValue = getSharedValue(channelName);
+            String path = getPath(channelName);
             int attempts = 0;
             while (attempts < 3) {
-                long existing = Longs.fromByteArray(sharedValue.getValue());
-                if (sequence.getSequence() > existing) {
-                    if (sharedValue.trySetValue(bytes)) return;
+                LastUpdated existing = getLongValue(channelName);
+                if (sequence.getSequence() > existing.value) {
+                    if (setValue(path, bytes, existing)) {
+                        return;
+                    }
                 } else {
                     return;
                 }
@@ -67,31 +55,60 @@ public class SequenceKeyCoordination implements KeyCoordination {
         }
     }
 
+    private boolean setValue(String path, byte[] bytes, LastUpdated existing) throws Exception {
+        try {
+            if (existing.version == -1) {
+                curator.create().creatingParentsIfNeeded().forPath(path, bytes);
+            } else {
+                curator.setData().withVersion(existing.version).forPath(path, bytes);
+            }
+            return true;
+        } catch (Exception e) {
+            logger.info("what happened? " + path, e);
+            return false;
+        }
+    }
+
     @VisibleForTesting
-    SharedValue getSharedValue(String channelName) {
-        return cache.getUnchecked(getKey(channelName));
+    LastUpdated getLongValue(String channelName) {
+        try {
+            Stat stat = new Stat();
+            byte[] bytes = curator.getData().storingStatIn(stat).forPath(getPath(channelName));
+            return new LastUpdated(Longs.fromByteArray(bytes), stat.getVersion());
+        } catch (Exception e) {
+            logger.info("unable to get value " + channelName + " " + e.getMessage());
+            return new LastUpdated(SequenceContentKey.START_VALUE, -1);
+        }
     }
 
     @Override
     @Timed(name = "sequence.getLastUpdated")
     public ContentKey getLastUpdated(final String channelName) {
-        byte[] value = getSharedValue(channelName).getValue();
-        return new SequenceContentKey(Longs.fromByteArray(value));
+        return new SequenceContentKey(getLongValue(channelName).value);
     }
 
     @Override
     public void delete(String channelName) {
-        String key = getKey(channelName);
+        String path = getPath(channelName);
         try {
-            cache.invalidate(key);
-            curator.delete().deletingChildrenIfNeeded().forPath(key);
+            curator.delete().deletingChildrenIfNeeded().forPath(path);
         } catch (Exception e) {
             logger.warn("unable to delete key " + channelName);
         }
     }
 
-    private String getKey(String channelName) {
+    private String getPath(String channelName) {
         return "/lastUpdated/" + channelName;
+    }
+
+    private class LastUpdated {
+        long value;
+        int version;
+
+        private LastUpdated(long value, int version) {
+            this.value = value;
+            this.version = version;
+        }
     }
 
 }
