ce51727083729761eccf9d6cc734af21fa57d04f
Greg Moulliet <gmoulliet@flightstats.com>
2014-06-02 15:06:36 -0700

    ading group name validation

---
+++ src/main/java/com/flightstats/hub/group/GroupValidator.java
@@ -0,0 +1,33 @@
+package com.flightstats.hub.group;
+
+import com.flightstats.hub.model.exception.InvalidRequestException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class GroupValidator {
+
+    public void validate(Group group) {
+        String name = group.getName();
+        if (StringUtils.isEmpty(name)) {
+            throw new InvalidRequestException("{\"error\": \"Group name is required\"}");
+        }
+        if (!name.matches("^[a-zA-Z0-9_]+$")) {
+            throw new InvalidRequestException("{\"error\": \"Group name can only contain characters a-z, A-Z, _ and 0-9\"}");
+        }
+        if (name.length() > 48) {
+            throw new InvalidRequestException("{\"error\": \"Group name must be less than 48 bytes\"}");
+        }
+        try {
+            new URI(group.getCallbackUrl());
+        } catch (URISyntaxException e) {
+            throw new InvalidRequestException("{\"error\": \"Invalid callbackUrl\"}");
+        }
+        try {
+            new URI(group.getChannelUrl());
+        } catch (URISyntaxException e) {
+            throw new InvalidRequestException("{\"error\": \"Invalid channelUrl\"}");
+        }
+    }
+}
---
+++ src/test/java/com/flightstats/hub/group/GroupValidatorTest.java
@@ -0,0 +1,59 @@
+package com.flightstats.hub.group;
+
+import com.flightstats.hub.model.exception.InvalidRequestException;
+import com.google.common.base.Strings;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GroupValidatorTest {
+
+    private GroupValidator groupValidator;
+    private Group group;
+
+    @Before
+    public void setUp() throws Exception {
+        groupValidator = new GroupValidator();
+        group = Group.builder()
+                .callbackUrl("http://client/url")
+                .channelUrl("http://hub/channel/channelName")
+                .build();
+    }
+
+    @Test
+    public void testName() throws Exception {
+        groupValidator.validate(group.withName("aA9"));
+    }
+
+    @Test
+    public void testNameLarge() throws Exception {
+        groupValidator.validate(group.withName(Strings.repeat("B", 48)));
+    }
+
+    @Test(expected = InvalidRequestException.class)
+     public void testNameSizeTooBig() throws Exception {
+        groupValidator.validate(group.withName(Strings.repeat("B", 49)));
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void testNameChars() throws Exception {
+        groupValidator.validate(group.withName("aA9-"));
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void testInvalidChannelUrl() throws Exception {
+        groupValidator.validate(Group.builder()
+                .callbackUrl("http:/client/url")
+                .channelUrl("http:\\hub/channel/channelName")
+                .name("nothing")
+                .build());
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void testInvalidCallbackUrl() throws Exception {
+        groupValidator.validate(Group.builder()
+                .callbackUrl("not a url")
+                .channelUrl("http://hub/channel/channelName")
+                .name("nothing")
+                .build());
+    }
+}
