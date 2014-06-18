dd2bdb73322b278158b954aa9e7573abb1b9a0e8
Greg Moulliet <gmoulliet@flightstats.com>
2014-06-17 22:59:44 -0700

    renaming extractor, adding validation to group's channel url

--- src/main/java/com/flightstats/hub/websocket/MetricsWebSocketCreator.java
+++ src/main/java/com/flightstats/hub/websocket/MetricsWebSocketCreator.java
@@ -3,7 +3,7 @@ package com.flightstats.hub.websocket;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import com.flightstats.hub.service.ChannelLinkBuilder;
-import com.flightstats.hub.util.ChannelNameExtractor;
+import com.flightstats.hub.util.ChannelNameUtils;
 import com.google.inject.Inject;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
@@ -15,29 +15,29 @@ public class MetricsWebSocketCreator implements WebSocketCreator {
 
 	private final MetricRegistry registry;
     private final WebsocketSubscribers subscriptions;
-    private final ChannelNameExtractor channelNameExtractor;
+    private final ChannelNameUtils channelNameUtils;
 	private final Object mutex = new Object();
 	private final ChannelLinkBuilder linkBuilder;
 
 	@Inject
-	public MetricsWebSocketCreator(MetricRegistry registry, WebsocketSubscribers subscriptions, ChannelNameExtractor channelNameExtractor, ChannelLinkBuilder linkBuilder) {
+	public MetricsWebSocketCreator(MetricRegistry registry, WebsocketSubscribers subscriptions, ChannelNameUtils channelNameUtils, ChannelLinkBuilder linkBuilder) {
 		this.registry = registry;
         this.subscriptions = subscriptions;
-        this.channelNameExtractor = channelNameExtractor;
+        this.channelNameUtils = channelNameUtils;
 		this.linkBuilder = linkBuilder;
 	}
 
     @Override
     public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
         URI requestUri = req.getRequestURI();
-        final String channelName = channelNameExtractor.extractFromWS(requestUri);
+        final String channelName = channelNameUtils.extractFromWS(requestUri);
         final String meterName = "websocket-clients.channels." + channelName;
 
         synchronized (mutex) {
             registry.counter(meterName).inc();
         }
 
-        return new HubWebSocket( subscriptions,  channelNameExtractor, linkBuilder, new Runnable() {
+        return new HubWebSocket( subscriptions, channelNameUtils, linkBuilder, new Runnable() {
             @Override
             public void run() {
                 synchronized (mutex) {
