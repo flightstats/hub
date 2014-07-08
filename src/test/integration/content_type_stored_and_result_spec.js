de9bdb27cb0fd666e7d3a7950befd3448936f4a6
Greg Moulliet <gmoulliet@flightstats.com>
2014-07-07 17:19:04 -0700

    adding test names to channel creation, increasing timeout

--- src/test/integration/content_type_stored_and_result_spec.js
+++ src/test/integration/content_type_stored_and_result_spec.js
@@ -3,19 +3,19 @@ require('./integration_config.js');
 var channelName = utils.randomChannelName();
 var thisChannelResource = channelUrl + "/" + channelName;
 var messageText = "MY SUPER TEST CASE: this & <that>. " + Math.random().toString();
-var testName = "content_type_stored_and_result_spec";
+var testName = __filename;
 
 utils.configureFrisby();
 
-utils.runInTestChannel(channelName, function () {
+utils.runInTestChannel(testName, channelName, function () {
     frisby.create(testName + ': Checking that the content-type is returned.')
-        .post(thisChannelResource, null, { body: messageText})
+        .post(thisChannelResource, null, { body : messageText})
         .addHeader("Content-Type", "application/fractals")
         .expectStatus(201)
         .expectHeader('content-type', 'application/json')
         .expectJSON('_links', {
-            channel: {
-                href: thisChannelResource
+            channel : {
+                href : thisChannelResource
             }
         })
         .afterJSON(function (result) {
