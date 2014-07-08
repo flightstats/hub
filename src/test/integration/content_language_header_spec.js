4bc291abb1be9ae2fdc4e62a2ebfbfa92bc885eb
Greg Moulliet <gmoulliet@flightstats.com>
2014-07-07 17:19:04 -0700

    adding test names to channel creation, increasing timeout

--- src/test/integration/content_language_header_spec.js
+++ src/test/integration/content_language_header_spec.js
@@ -4,14 +4,14 @@ var request = require('request');
 var channelName = utils.randomChannelName();
 var thisChannelResource = channelUrl + "/" + channelName;
 var messageText = "Testing that the Content-Language header is returned";
-var testName = "content_language_header_spec";
+var testName = __filename;
 
 utils.configureFrisby();
 
-utils.runInTestChannel(channelName, function () {
+utils.runInTestChannel(testName, channelName, function () {
     // Note: We have to use request directly here, because Frisby insists on having a content-type specified.
     frisby.create(testName + " Testing the content-language header")
-        .post(thisChannelResource, null, { body: messageText})
+        .post(thisChannelResource, null, { body : messageText})
         .addHeader("Content-Language", "en, sp")
         .expectStatus(201)
         .afterJSON(function (result) {
--- src/test/integration/insertion_response_has_location_header_spec.js
+++ src/test/integration/insertion_response_has_location_header_spec.js
@@ -3,13 +3,13 @@ require('./integration_config.js');
 var channelName = utils.randomChannelName();
 var thisChannelResource = channelUrl + "/" + channelName;
 var messageText = "MY SUPER TEST CASE: this & <that>. " + Math.random().toString();
-var testName = "insertion_response_has_location_header_spec";
+var testName = __filename;
 
 utils.configureFrisby();
 
-utils.runInTestChannel(channelName, function () {
+utils.runInTestChannel(testName, channelName, function () {
     frisby.create(testName + ': Inserting a value into a channel.')
-        .post(thisChannelResource, null, { body: messageText})
+        .post(thisChannelResource, null, { body : messageText})
         .addHeader("Content-Type", "text/plain")
         .expectStatus(201)
         .after(function (err, res, body) {
