5f391f7d406473a2e1ffd060dbf17aa09933bd91
Benjamin Corliss <bcorliss@flightstats.com>
2014-07-14 12:44:53 -0700

    add convenience git command

--- devmerge.sh
+++ devmerge.sh
@@ -2,6 +2,7 @@
 FEATURE_BRANCH=git rev-parse --abbrev-ref HEAD
 git checkout develop
 git pull origin develop
+echo "about to merge"
 git merge ${FEATURE_BRANCH} --no-edit
 git push origin develop
 git checkout ${FEATURE_BRANCH}
