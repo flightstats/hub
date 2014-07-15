b4ccaaba109e749f2bf7ef3538a9b59033d16b87
Greg Moulliet <gmoulliet@flightstats.com>
2014-07-15 14:46:07 -0700

    shutting down group as part of prestop

--- src/main/java/com/flightstats/hub/app/HubServices.java
+++ src/main/java/com/flightstats/hub/app/HubServices.java
@@ -13,17 +13,23 @@ import java.util.List;
 public class HubServices {
     private final static Logger logger = LoggerFactory.getLogger(HubServices.class);
 
-    private static List<Service> services = new ArrayList<>();
+    private static List<Service> allServices = new ArrayList<>();
+    private static List<Service> preStopServices = new ArrayList<>();
 
+    public static void registerPreStop(Service service) {
+        logger.info("registering pre stop" + service.getClass().getName());
+        preStopServices.add(service);
+        register(service);
+    }
 
     public static void register(Service service) {
         logger.info("registering " + service.getClass().getName());
-        services.add(service);
+        allServices.add(service);
     }
 
     public static void startAll() {
         try {
-            for (Service service : services) {
+            for (Service service : allServices) {
                 logger.info("starting service " + service.getClass().getName());
                 service.startAsync();
                 service.awaitRunning();
@@ -36,15 +42,29 @@ public class HubServices {
     }
 
     public static void stopAll() {
+        stop(allServices);
+    }
+
+    public static void preStopAll() {
+        stop(preStopServices);
+    }
+
+    private static void stop(List<Service> services) {
+        for (Service service : services) {
+            logger.info("stopping service " + service.getClass().getName());
+            service.stopAsync();
+        }
+        for (Service service : services) {
+            await(service);
+            logger.info("stopped service " + service.getClass().getName());
+        }
+    }
+
+    private static void await(Service service) {
         try {
-            for (Service service : services) {
-                logger.info("stopping service " + service.getClass().getName());
-                service.stopAsync();
-                service.awaitTerminated();
-                logger.info("stopped service " + service.getClass().getName());
-            }
+            service.awaitTerminated();
         } catch (Exception e) {
-            logger.error("unable to stop services, exiting", e);
+            logger.error("unable to stop service" + service.getClass().getName(), e);
         }
     }
 }
