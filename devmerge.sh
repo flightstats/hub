cf31a25411386b4c6ac63807dfa6c33f9f165d18
Benjamin Corliss <bcorliss@flightstats.com>
2014-07-14 23:20:57 -0700

    delete develop after we switch to feature branch

--- devmerge.sh
+++ devmerge.sh
@@ -3,7 +3,7 @@ FEATURE_BRANCH=`git rev-parse --abbrev-ref HEAD`
 git branch -d develop
 git checkout develop
 git pull origin develop
-git merge --no-edit ${FEATURE_BRANCH}
+git merge --no-edit -X theirs ${FEATURE_BRANCH}
 git push origin develop
 git checkout ${FEATURE_BRANCH}
 git branch -d develop
