1cef5830aef145a71099fae2bb6c23001982f94b
Greg Moulliet <gmoulliet@flightstats.com>
2014-07-18 15:44:33 -0700

    debuging on jenkins

--- deploy/release-hub.bash
+++ deploy/release-hub.bash
@@ -62,7 +62,7 @@ function deploy {
 }
 
 function restart {
-    ssh -v utlity@${SERVER} "sudo service hub restart"
+    ssh -v utility@${SERVER} "sudo service hub restart"
 
     jasmine-node --captureExceptions --config host ${SERVER} ./deploy/wait_for_health_spec.js
     if [ $? -eq 0 ]
