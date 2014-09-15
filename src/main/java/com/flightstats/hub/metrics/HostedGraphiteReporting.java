db0cf1cc199d42e8bb3842427ca96951792eb735
Greg Moulliet <gmoulliet@flightstats.com>
2014-09-15 13:09:27 -0700

    removing host name from metric key

--- src/main/java/com/flightstats/hub/metrics/HostedGraphiteReporting.java
+++ src/main/java/com/flightstats/hub/metrics/HostedGraphiteReporting.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -41,27 +42,27 @@ public class HostedGraphiteReporting implements Haltable {
             if (registerJvm) {
                 registerJvmMetrics(registry);
             }
-            final String prefix = graphitePrefix + "." + InetAddress.getLocalHost().getHostName().split("\\.")[0];
-                logger.info("Enabling Hosted Graphite metrics for " + host + ":" + port + " - reporting interval " + rateSeconds + " seconds");
-                final GraphiteLogger graphite = new GraphiteLogger(new Graphite(new InetSocketAddress(host, port)));
-                final HostedGraphiteReporter reporter = HostedGraphiteReporter.forRegistry(registry)
-                        .prefixedWith(prefix)
-                        .convertRatesTo(TimeUnit.SECONDS)
-                        .convertDurationsTo(TimeUnit.MILLISECONDS)
-                        .filter(new NoTestChannelsMetricsFilter())
-                        .build(graphite);
+            logger.info("Enabling Hosted Graphite metrics for " + host + ":" + port + " - reporting interval " + rateSeconds + " seconds");
+            final GraphiteLogger graphite = new GraphiteLogger(new Graphite(new InetSocketAddress(host, port)));
+            final HostedGraphiteReporter reporter = HostedGraphiteReporter.forRegistry(registry)
+                    .prefixedWith(graphitePrefix)
+                    .convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS)
+                    .filter(new NoTestChannelsMetricsFilter())
+                    .build(graphite);
 
-                reporter.start(rateSeconds, TimeUnit.SECONDS);
-                reporters.add(reporter);
+            reporter.start(rateSeconds, TimeUnit.SECONDS);
+            reporters.add(reporter);
         } catch (Exception filledWithHate) {
             throw new RuntimeException("Failure while enabling MetricsReporting", filledWithHate);
         }
     }
 
-    private void registerJvmMetrics(MetricRegistry registry) {
-        registry.registerAll(prefix("jvm.gc", new GarbageCollectorMetricSet()));
-        registry.registerAll(prefix("jvm.memory", new MemoryUsageGaugeSet()));
-        registry.registerAll(prefix("jvm.threads", new ThreadStatesGaugeSet()));
+    private void registerJvmMetrics(MetricRegistry registry) throws UnknownHostException {
+        String hostPrefix = InetAddress.getLocalHost().getHostName().split("\\.")[0];
+        registry.registerAll(prefix(hostPrefix + "jvm.gc", new GarbageCollectorMetricSet()));
+        registry.registerAll(prefix(hostPrefix + "jvm.memory", new MemoryUsageGaugeSet()));
+        registry.registerAll(prefix(hostPrefix + "jvm.threads", new ThreadStatesGaugeSet()));
     }
 
     @Override
