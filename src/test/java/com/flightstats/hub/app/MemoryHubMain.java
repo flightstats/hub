0f1cb4b8bd55d2bc38e8601fe73f746137cd8f81
Greg Moulliet <gmoulliet@flightstats.com>
2014-10-28 13:48:40 -0700

    remove memory hub

--- src/test/java/com/flightstats/hub/app/MemoryHubMain.java
+++
@@ -1,9 +0,0 @@
-package com.flightstats.hub.app;
-
-import com.flightstats.hub.test.Integration;
-
-public class MemoryHubMain {
-    public static void main(String[] args) throws Exception {
-        Integration.startMemoryHub();
-    }
-}
