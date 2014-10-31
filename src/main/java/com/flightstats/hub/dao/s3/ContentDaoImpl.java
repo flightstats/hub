ca54cd79f45e49d93fcbc9bcd4404daeba0da1f9
Greg Moulliet <gmoulliet@flightstats.com>
2014-10-31 14:42:47 -0700

    cleaning up old time index

--- src/main/java/com/flightstats/hub/dao/s3/ContentDaoImpl.java
+++ src/main/java/com/flightstats/hub/dao/s3/ContentDaoImpl.java
@@ -2,7 +2,6 @@ package com.flightstats.hub.dao.s3;
 
 import com.flightstats.hub.app.HubServices;
 import com.flightstats.hub.dao.ContentDao;
-import com.flightstats.hub.dao.timeIndex.TimeIndex;
 import com.flightstats.hub.model.ChannelConfiguration;
 import com.flightstats.hub.model.Content;
 import com.flightstats.hub.model.ContentKey;
@@ -24,17 +23,11 @@ public class ContentDaoImpl implements ContentDao {
 
     private final static Logger logger = LoggerFactory.getLogger(ContentDaoImpl.class);
 
-    private final ZooKeeperIndexDao zooKeeperIndexDao;
     private final S3ContentDao s3ContentDao;
-    private final S3IndexDao s3IndexDao;
 
     @Inject
-    public ContentDaoImpl(ZooKeeperIndexDao zooKeeperIndexDao,
-                          S3ContentDao s3ContentDao,
-                          S3IndexDao s3IndexDao) {
+    public ContentDaoImpl(S3ContentDao s3ContentDao) {
         this.s3ContentDao = s3ContentDao;
-        this.zooKeeperIndexDao = zooKeeperIndexDao;
-        this.s3IndexDao = s3IndexDao;
         HubServices.register(new S3ContentDaoInit());
     }
 
@@ -48,7 +41,6 @@ public class ContentDaoImpl implements ContentDao {
         ContentKey key = content.getContentKey().get();
         DateTime dateTime = new DateTime(content.getMillis());
         s3ContentDao.writeS3(channelName, content, key);
-        zooKeeperIndexDao.writeIndex(channelName, dateTime, key);
         return new InsertedContentKey(key, dateTime.toDate());
     }
 
@@ -59,22 +51,7 @@ public class ContentDaoImpl implements ContentDao {
 
     @Override
     public Collection<ContentKey> getKeys(String channelName, DateTime dateTime) {
-        /**
-         * The TimeIndex is written to ZK, then TimeIndexProcessor write the data to S3, then deletes the keys.
-         * We try reading from S3 first, because the ZK cache may be partial if it is already written to S3.
-         * There is a bit of a race condition here, especially with S3 eventual consistency.
-         */
-        String hashTime = TimeIndex.getHash(dateTime);
-        try {
-            return s3IndexDao.getKeys(channelName, hashTime);
-        } catch (Exception e) {
-            logger.debug("unable to find keys in S3 " + channelName + hashTime + e.getMessage());
-        }
-        try {
-            return zooKeeperIndexDao.getKeys(channelName, hashTime);
-        } catch (Exception e) {
-            logger.debug("unable to find keys in ZK " + channelName + hashTime + e.getMessage());
-        }
+        //todo - gfm - 10/31/14 - query, and allow time range
         return Collections.emptyList();
     }
 
@@ -92,8 +69,7 @@ public class ContentDaoImpl implements ContentDao {
     @Override
     public void delete(String channelName) {
         s3ContentDao.delete(channelName);
-        //todo - gfm - 10/31/14 - delete this too...
-        zooKeeperIndexDao.delete(channelName);
+        //todo - gfm - 10/31/14 - delete records from Riak
     }
 
     private class S3ContentDaoInit extends AbstractIdleService {
