a38f1a2ae93aa404b7fe2ad6cc1719c62310177a
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-02 16:18:47 -0800

    fixing typo

--- src/test/load/read-write-ws.py
+++ src/test/load/read-write-ws.py
@@ -1,10 +1,14 @@
 # locust.py
 
 import json
+import time
+import thread
 import string
 import random
 
 from locust import Locust, TaskSet, task
+import websocket
+import httplib2
 
 
 
@@ -15,7 +19,7 @@ from locust import Locust, TaskSet, task
 # Be sure to update gevent to get around DNS issue
 # pip install https://github.com/surfly/gevent/releases/download/1.0rc3/gevent-1.0rc3.tar.gz
 
-#todo variable timing from command line
+# todo variable timing from command line
 
 class WebsiteTasks(TaskSet):
     channelNum = 0
@@ -34,16 +38,34 @@ class WebsiteTasks(TaskSet):
                          headers={"Content-Type": "application/json"}
         )
 
+        thread.start_new_thread(self.reader, ())
+
     @task
     def index(self):
         payload = {"name": self.payload, "count": self.count}
-        response = self.client.post("/channel/" + self.channel,
+        self.client.post("/channel/" + self.channel,
                          data=json.dumps(payload),
                          headers={"Content-Type": "application/json"}
         )
-        print "Response status code:", response.status_code
-        print "Response content:", response.content
         self.count += 1
+
+    def reader(self):
+        time.sleep(5)
+        self._http = httplib2.Http()
+        meta = self._load_metadata()
+        ws_uri = meta['_links']['ws']['href']
+        print ws_uri
+        ws = websocket.WebSocketApp(ws_uri, on_message=self.on_message)
+        ws.run_forever()
+
+    def on_message(self, ws, message):
+        r, c = self._http.request(message, 'GET')
+        #print("--%s" %(c))
+
+    def _load_metadata(self):
+        print("Fetching channel metadata...")
+        r, c = self._http.request(self.client.base_url + "/channel/" + self.channel, 'GET')
+        return json.loads(c)
 
     def payload_generator(self, size, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for x in range(size))
