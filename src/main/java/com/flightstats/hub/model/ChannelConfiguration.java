ac4921095a0fe4946d8c4e555df62b55631a1912
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-10 13:24:41 -0800

    exorcising the ghost of ttlMillis past

--- src/main/java/com/flightstats/hub/model/ChannelConfiguration.java
+++ src/main/java/com/flightstats/hub/model/ChannelConfiguration.java
@@ -14,45 +14,25 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @ToString
 @EqualsAndHashCode(of = {"name"})
 public class ChannelConfiguration implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    private static final Gson gson = new GsonBuilder().registerTypeAdapter(Date.class, new HubDateTypeAdapter()).create();
     private final String name;
-	private final Date creationDate;
-	private final long ttlDays;
+    private final Date creationDate;
+    private final long ttlDays;
     private final ChannelType type;
-    private final int contentSizeKB;
-    private final int peakRequestRateSeconds;
-    private final Long ttlMillis;
     private final String description;
     private final Set<String> tags = new TreeSet<>();
-
-    private static final Gson gson = new GsonBuilder().registerTypeAdapter(Date.class, new HubDateTypeAdapter()).create();
-
-    public enum ChannelType { Sequence }
 
     public ChannelConfiguration(Builder builder) {
         this.name = StringUtils.trim(builder.name);
         this.creationDate = builder.creationDate;
         this.type = builder.type;
-        this.contentSizeKB = builder.contentSizeKB;
-        this.peakRequestRateSeconds = builder.peakRequestRateSeconds;
-        if (null == builder.ttlMillis) {
-            this.ttlDays = builder.ttlDays;
-            this.ttlMillis = TimeUnit.DAYS.toMillis(ttlDays);
-        } else {
-            int extraDay = 0;
-            if (builder.ttlMillis % TimeUnit.DAYS.toMillis(1) > 0) {
-                extraDay = 1;
-            }
-            this.ttlDays = TimeUnit.MILLISECONDS.toDays(builder.ttlMillis) + extraDay;
-            this.ttlMillis = builder.ttlMillis;
-        }
+        this.ttlDays = builder.ttlDays;
         if (builder.description == null) {
             this.description = "";
         } else {
@@ -61,52 +41,40 @@ public class ChannelConfiguration implements Serializable {
         this.tags.addAll(builder.tags);
     }
 
+    public static ChannelConfiguration fromJson(String json) {
+        if (StringUtils.isEmpty(json)) {
+            throw new InvalidRequestException("this method requires at least a json name");
+        }
+        return gson.fromJson(json, ChannelConfiguration.Builder.class).build();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
     @JsonProperty("name")
     public String getName() {
-		return name;
-	}
+        return name;
+    }
 
     @JsonProperty("creationDate")
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
     @JsonProperty("ttlDays")
-	public long getTtlDays() {
-		return ttlDays;
-	}
+    public long getTtlDays() {
+        return ttlDays;
+    }
 
     @JsonIgnore()
     public boolean isSequence() {
         return ChannelType.Sequence.equals(type);
     }
 
-    @JsonIgnore()
-    public long getContentThroughputInSeconds() {
-        return contentSizeKB * getPeakRequestRateSeconds();
-    }
-
-    @JsonProperty("contentSizeKB")
-    public int getContentSizeKB() {
-        return contentSizeKB;
-    }
-
-    @JsonProperty("peakRequestRateSeconds")
-    public int getPeakRequestRateSeconds() {
-        return peakRequestRateSeconds;
-    }
-
     @JsonProperty("type")
     public ChannelType getType() {
         return type;
-    }
-
-    /**
-     * @deprecated this can go away eventually, use ttlDays instead
-     */
-    @JsonProperty("ttlMillis")
-    public Long getTtlMillis() {
-        return ttlMillis;
     }
 
     @JsonProperty("description")
@@ -119,43 +87,29 @@ public class ChannelConfiguration implements Serializable {
         return tags;
     }
 
-    public static ChannelConfiguration fromJson(String json) {
-        if (StringUtils.isEmpty(json)) {
-            throw new InvalidRequestException("this method requires at least a json name");
-        }
-        return gson.fromJson(json, ChannelConfiguration.Builder.class).build();
-    }
+    public enum ChannelType {Sequence}
 
-    public static Builder builder() {
-		return new Builder();
-	}
-
-	public static class Builder {
-		private String name;
-		private Date creationDate = new Date();
-		private long ttlDays = 120;
+    public static class Builder {
+        private static final ObjectMapper mapper = new ObjectMapper();
+        private String name;
+        private Date creationDate = new Date();
+        private long ttlDays = 120;
         private ChannelType type = ChannelType.Sequence;
-        private int contentSizeKB = 1;
-        private int peakRequestRateSeconds = 1;
-        private Long ttlMillis;
         private String description = "";
         private Set<String> tags = new HashSet<>();
-        private static final ObjectMapper mapper = new ObjectMapper();
 
         public Builder() {
         }
 
         public Builder withChannelConfiguration(ChannelConfiguration config) {
-			this.name = config.name;
-			this.creationDate = config.creationDate;
-			this.ttlDays = config.ttlDays;
+            this.name = config.name;
+            this.creationDate = config.creationDate;
+            this.ttlDays = config.ttlDays;
             this.type = config.type;
-            this.contentSizeKB = config.contentSizeKB;
-            this.peakRequestRateSeconds = config.peakRequestRateSeconds;
             this.description = config.description;
             this.tags.addAll(config.getTags());
-			return this;
-		}
+            return this;
+        }
 
         public Builder withUpdateJson(String json) throws IOException {
             JsonNode rootNode = mapper.readTree(json);
@@ -168,20 +122,6 @@ public class ChannelConfiguration implements Serializable {
             }
             if (rootNode.has("ttlDays")) {
                 withTtlDays(rootNode.get("ttlDays").asLong());
-            } else if (rootNode.has("ttlMillis")) {
-                JsonNode millisNode = rootNode.get("ttlMillis");
-                //filled with hate for null
-                if (millisNode.asText().equals("null")) {
-                    withTtlDays(120);
-                } else {
-                    withTtlMillis(millisNode.asLong());
-                }
-            }
-            if (rootNode.has("contentSizeKB")) {
-                withContentKiloBytes(rootNode.get("contentSizeKB").asInt());
-            }
-            if (rootNode.has("peakRequestRateSeconds")) {
-                withPeakRequestRate(rootNode.get("peakRequestRateSeconds").asInt());
             }
             if (rootNode.has("tags")) {
                 tags.clear();
@@ -193,38 +133,23 @@ public class ChannelConfiguration implements Serializable {
             return this;
         }
 
-		public Builder withName(String name) {
-			this.name = name;
-			return this;
-		}
-
-        public Builder withTtlMillis(Long ttlMillis) {
-            this.ttlMillis = ttlMillis;
+        public Builder withName(String name) {
+            this.name = name;
             return this;
         }
 
-		public Builder withTtlDays(long ttlDays) {
-			this.ttlDays = ttlDays;
-			return this;
-		}
+        public Builder withTtlDays(long ttlDays) {
+            this.ttlDays = ttlDays;
+            return this;
+        }
 
-		public Builder withCreationDate(Date date) {
-			this.creationDate = date;
-			return this;
-		}
+        public Builder withCreationDate(Date date) {
+            this.creationDate = date;
+            return this;
+        }
 
         public Builder withType(ChannelType channelType) {
             this.type = channelType;
-            return this;
-        }
-
-        public Builder withContentKiloBytes(int contentKiloBytes) {
-            this.contentSizeKB = contentKiloBytes;
-            return this;
-        }
-
-        public Builder withPeakRequestRate(int peakRequestRateSeconds) {
-            this.peakRequestRateSeconds = peakRequestRateSeconds;
             return this;
         }
 
@@ -232,9 +157,10 @@ public class ChannelConfiguration implements Serializable {
             this.tags.addAll(tags);
             return this;
         }
-		public ChannelConfiguration build() {
-			return new ChannelConfiguration(this);
-		}
+
+        public ChannelConfiguration build() {
+            return new ChannelConfiguration(this);
+        }
 
         public Builder withDescription(String description) {
             this.description = description;
--- src/test/integration/channel_creation_with_ttl_spec.js
+++ src/test/integration/channel_creation_with_ttl_spec.js
@@ -3,7 +3,7 @@ require('./integration_config.js');
 var channelName = utils.randomChannelName();
 var jsonBody = JSON.stringify({ "name": channelName, "ttlMillis": 30000});
 var channelResource = channelUrl + "/" + channelName;
-var testName = 'channel_creation_with_ttl_spec';
+var testName = __filename;
 
 utils.configureFrisby();
 
@@ -11,19 +11,18 @@ frisby.create(testName + ': Making sure channel resource does not yet exist.')
     .get(channelResource)
     .expectStatus(404)
     .after(function () {
-        frisby.create(testName + 'Test create channel with valid ttl')
+        frisby.create(testName + ' Test create channel with valid ttl')
             .post(channelUrl, null, { body: jsonBody})
             .addHeader("Content-Type", "application/json")
             .expectStatus(201)
-            .expectJSON({"ttlMillis": 86400000})
+            .expectJSON({"ttlDays" : 120})
             .afterJSON(function (result) {
                 frisby.create(testName + ': Now fetching metadata')
                     .get(channelResource)
                     .expectStatus(200)
                     .expectHeader('content-type', 'application/json')
                     .expectJSON({"name": channelName})
-                    .expectJSON({"ttlDays": 1})
-                    .expectJSON({"ttlMillis": 86400000})
+                    .expectJSON({"ttlDays" : 120})
                     .toss();
             })
             .toss();
