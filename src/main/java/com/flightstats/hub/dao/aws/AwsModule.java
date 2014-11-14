efa64b4a34d04ba072989234954ad822d5babaf4
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-14 14:38:27 -0800

    working on support for S3 & queries

--- src/main/java/com/flightstats/hub/dao/aws/AwsModule.java
+++ src/main/java/com/flightstats/hub/dao/aws/AwsModule.java
@@ -12,6 +12,8 @@ import com.flightstats.hub.dao.dynamo.DynamoChannelConfigurationDao;
 import com.flightstats.hub.dao.dynamo.DynamoUtils;
 import com.flightstats.hub.dao.encryption.AuditChannelService;
 import com.flightstats.hub.dao.encryption.BasicChannelService;
+import com.flightstats.hub.dao.s3.S3Config;
+import com.flightstats.hub.dao.s3.S3ContentDao;
 import com.flightstats.hub.group.DynamoGroupDao;
 import com.flightstats.hub.group.GroupCallback;
 import com.flightstats.hub.group.GroupCallbackImpl;
@@ -57,6 +59,7 @@ public class AwsModule extends AbstractModule {
         bind(ChannelUtils.class).asEagerSingleton();
         bind(CuratorLock.class).asEagerSingleton();
         bind(AwsConnectorFactory.class).asEagerSingleton();
+        bind(S3Config.class).asEagerSingleton();
 
         if (Boolean.parseBoolean(properties.getProperty("app.encrypted"))) {
             logger.info("using encrypted hub");
@@ -74,13 +77,20 @@ public class AwsModule extends AbstractModule {
         bind(ReplicationDao.class).to(CachedReplicationDao.class).asEagerSingleton();
         bind(ReplicationDao.class)
                 .annotatedWith(Names.named(CachedReplicationDao.DELEGATE))
-                .to(DynamoReplicationDao.class);
+                .to(DynamoReplicationDao.class).asEagerSingleton();
 
         bind(ContentService.class).to(ContentServiceImpl.class).asEagerSingleton();
 
         bind(FileSpokeStore.class).asEagerSingleton();
         bind(RemoteSpokeStore.class).asEagerSingleton();
-        bind(ContentDao.class).to(SpokeContentDao.class).asEagerSingleton();
+
+        bind(ContentDao.class)
+                .annotatedWith(Names.named(ContentDao.CACHE))
+                .to(SpokeContentDao.class).asEagerSingleton();
+
+        bind(ContentDao.class)
+                .annotatedWith(Names.named(ContentDao.LONG_TERM))
+                .to(S3ContentDao.class).asEagerSingleton();
 
         bind(DynamoUtils.class).asEagerSingleton();
         bind(DynamoGroupDao.class).asEagerSingleton();
