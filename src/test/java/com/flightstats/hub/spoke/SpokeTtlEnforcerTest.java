56f8a953eb91f6e84b4c451140b5f515a51f362b
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-18 11:15:31 -0800

    fixing test

--- src/test/java/com/flightstats/hub/spoke/SpokeTtlEnforcerTest.java
+++ src/test/java/com/flightstats/hub/spoke/SpokeTtlEnforcerTest.java
@@ -1,11 +1,10 @@
 package com.flightstats.hub.spoke;
 
+import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.util.TimeUtil;
 import com.google.common.io.Files;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -15,9 +14,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class SpokeTtlEnforcerTest {
-
-    //todo - gfm - 11/14/14 - this comes from SpokeContentDao.  should be standardized somewhere
-    private final static DateTimeFormatter pathFormatter = DateTimeFormat.forPattern("yyyy/MM/dd/HH/mm/ssSSS").withZoneUTC();
 
     @Test
     public void testCleanup() throws Exception {
@@ -31,12 +27,12 @@ public class SpokeTtlEnforcerTest {
             String channel = RandomStringUtils.randomAlphanumeric(5);
             DateTime now = TimeUtil.now();
             for (int j = 0; j <= ttlMinutes; j += 10) {
-                String path = channel + "/" + now.minusMinutes(j).toString(pathFormatter);
+                String path = channel + "/" + new ContentKey(now.minusMinutes(j), "AA").toUrl();
                 spokeStore.write(path, "stuff".getBytes());
                 insideTtl.add(path);
             }
             for (int j = ttlMinutes + 1; j <= ttlMinutes * 50; j += 50) {
-                String path = channel + "/" + now.minusMinutes(j).toString(pathFormatter);
+                String path = channel + "/" + new ContentKey(now.minusMinutes(j), "BB").toUrl();
                 spokeStore.write(path, "stuff".getBytes());
                 outsideTtl.add(path);
             }
