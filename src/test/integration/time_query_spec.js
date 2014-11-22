d5a82219e2f3a976fdaecc5526743710404e62b0
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-22 11:22:06 -0800

    handling missing folder in spoke

--- src/test/integration/time_query_spec.js
+++ src/test/integration/time_query_spec.js
@@ -23,6 +23,16 @@ var groupConfig = {
 describe(testName, function () {
     utils.createChannel(channelName);
 
+    it('queries before insertion', function (done) {
+        request.get({url : channelResource + '/time/minute', json : true},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(200);
+                expect(body._links.uris.length).toBe(0);
+                done();
+            })
+    });
+
     for (var i = 0; i < 4; i++) {
         utils.addItem(channelResource);
     }
