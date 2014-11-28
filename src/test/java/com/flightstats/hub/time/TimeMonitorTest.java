aeb071672d79fe3f6a1085d7b2c2c1a36abeaa45
Greg Moulliet <gmoulliet@flightstats.com>
2014-11-28 11:38:40 -0800

    sending cluster time delta to hosted graphite

---
+++ src/test/java/com/flightstats/hub/time/TimeMonitorTest.java
@@ -0,0 +1,49 @@
+package com.flightstats.hub.time;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class TimeMonitorTest {
+
+    @Test
+    public void testPositive() throws Exception {
+        String[] output = {
+                "     remote           refid      st t when poll reach   delay   offset  jitter",
+                "==============================================================================",
+                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269    0.606   0.449",
+                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390    0.277   0.146"
+        };
+        assertEquals(0.606, TimeMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
+    }
+
+    @Test
+    public void testNegative() throws Exception {
+        String[] output = {
+                "     remote           refid      st t when poll reach   delay   offset  jitter",
+                "==============================================================================",
+                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269   -0.606   0.449",
+                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390   -0.277   0.146"
+        };
+        assertEquals(0.606, TimeMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
+    }
+
+    @Test
+    public void testPlusMinus() throws Exception {
+        String[] output = {
+                "     remote           refid      st t when poll reach   delay   offset  jitter",
+                "==============================================================================",
+                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269    0.606   0.449",
+                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390   -0.277   0.146"
+        };
+        assertEquals(0.883, TimeMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
+    }
+}
