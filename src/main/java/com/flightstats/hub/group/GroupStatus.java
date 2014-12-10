7ce2e6d806e4f2939ea3f9122ad60c18027766ab
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-10 14:05:52 -0800

    working on group status

--- src/main/java/com/flightstats/hub/group/GroupStatus.java
+++ src/main/java/com/flightstats/hub/group/GroupStatus.java
@@ -1,14 +1,15 @@
 package com.flightstats.hub.group;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flightstats.hub.model.ContentKey;
 import lombok.Getter;
 import lombok.experimental.Builder;
 
 @Getter
 @Builder
 public class GroupStatus {
-    private long lastCompleted;
-    private long channelLatest;
+    private ContentKey lastCompleted;
+    private ContentKey channelLatest;
     private Group group;
 
     @JsonIgnore
