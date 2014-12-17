bfb657eafc49906f1ada895a711946e2ff77af3d
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 10:40:04 -0800

    cleaning up locust deploy

--- src/test/load/deploy.sh
+++ src/test/load/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-scp *.py ubuntu@hub-node-tester.cloud-east.dev:load/.
-scp *.sh ubuntu@hub-node-tester.cloud-east.dev:load/.
-ssh ubuntu@hub-node-tester.cloud-east.dev "chmod +x load/*.sh"
-ssh gmoulliet@hub-node-tester.cloud-east.dev "sudo service hub-locust restart"
+SERVER=${1:-hub-node-tester.cloud-east.dev}
+APP_PATH=/home/ubuntu/load
+
+scp *.py ubuntu@${SERVER}:${APP_PATH}/.
+scp *.sh ubuntu@${SERVER}:${APP_PATH}/.
+ssh ubuntu@${SERVER} "chmod +x ${APP_PATH}/*.sh"
+ssh ubuntu@${SERVER} "sudo service hub-locust restart"
 
