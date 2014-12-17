d74440fcba74a4a68641aba7f60a3f0a12cd6cee
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 10:40:04 -0800

    cleaning up locust deploy

--- src/test/load/start.sh
+++ src/test/load/start.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-CLI="locust -f /home/ubuntu/load/read-write-group.py -H http://hub-v2.svc.dev"
+HOST="hub-node-tester.cloud-east.dev"
+
+case ${HOST} in
+	hub-node-tester.cloud-east.dev)
+	    HUB="http://hub-v2.svc.dev"
+	     ;;
+	hub-node-tester.cloud-east.staging)
+	    HUB="http://hub-v2.svc.dev"
+	     ;;
+	*)
+		echo "host not supported ${HOST}" ; exit ;;
+esac
+
+CLI="locust -f /home/ubuntu/load/read-write-group.py -H ${HUB}"
 
 set -f
 echo "Running : ${CLI}"
