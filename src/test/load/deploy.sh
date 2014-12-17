b3bdfa445733523a334fb9ce5de8632b26844179
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 12:14:44 -0800

    making file output verbose

--- src/test/load/deploy.sh
+++ src/test/load/deploy.sh
@@ -4,6 +4,8 @@ SERVER=${1:-hub-node-tester.cloud-east.dev}
 APP_PATH=/home/ubuntu/load
 FILE_PATH=${2:-.}
 
+echo "deploying to ${SERVER} from local path ${FILE_PATH}"
+
 rsync -av ${FILE_PATH}/*.py ubuntu@${SERVER}:${APP_PATH}/.
 rsync -av ${FILE_PATH}/*.sh ubuntu@${SERVER}:${APP_PATH}/.
 ssh ubuntu@${SERVER} "chmod +x ${APP_PATH}/*.sh"
