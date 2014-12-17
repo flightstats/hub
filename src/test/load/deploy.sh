7d232b6a27be6f2379f181cc9fe8c1b2143f7ba6
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 11:05:04 -0800

    adding path option

--- src/test/load/deploy.sh
+++ src/test/load/deploy.sh
@@ -2,9 +2,10 @@
 
 SERVER=${1:-hub-node-tester.cloud-east.dev}
 APP_PATH=/home/ubuntu/load
+FILE_PATH=${2:-.}
 
-scp *.py ubuntu@${SERVER}:${APP_PATH}/.
-scp *.sh ubuntu@${SERVER}:${APP_PATH}/.
+scp ${FILE_PATH}/*.py ubuntu@${SERVER}:${APP_PATH}/.
+scp ${FILE_PATH}/*.sh ubuntu@${SERVER}:${APP_PATH}/.
 ssh ubuntu@${SERVER} "chmod +x ${APP_PATH}/*.sh"
 ssh ubuntu@${SERVER} "sudo service hub-locust restart"
 
