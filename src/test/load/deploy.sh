5c7389c4a4644ab81fb952e2b1f73b432075bf73
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 11:13:00 -0800

    making file output verbose

--- src/test/load/deploy.sh
+++ src/test/load/deploy.sh
@@ -4,8 +4,8 @@ SERVER=${1:-hub-node-tester.cloud-east.dev}
 APP_PATH=/home/ubuntu/load
 FILE_PATH=${2:-.}
 
-scp ${FILE_PATH}/*.py ubuntu@${SERVER}:${APP_PATH}/.
-scp ${FILE_PATH}/*.sh ubuntu@${SERVER}:${APP_PATH}/.
+rsync -av ${FILE_PATH}/*.py ubuntu@${SERVER}:${APP_PATH}/.
+rsync -av ${FILE_PATH}/*.sh ubuntu@${SERVER}:${APP_PATH}/.
 ssh ubuntu@${SERVER} "chmod +x ${APP_PATH}/*.sh"
 ssh ubuntu@${SERVER} "sudo service hub-locust restart"
 
