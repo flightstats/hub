0cb140825eb439a5b1fd2996c9637ad1ff81953b
Greg Moulliet <gmoulliet@flightstats.com>
2014-12-17 13:56:31 -0800

    working on in flight

---
+++ src/main/java/com/flightstats/hub/group/GroupContentKeySet.java
@@ -0,0 +1,73 @@
+package com.flightstats.hub.group;
+
+import com.flightstats.hub.model.ContentKey;
+import com.google.inject.Inject;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GroupContentKeySet {
+    private final static Logger logger = LoggerFactory.getLogger(GroupContentKeySet.class);
+
+    private final CuratorFramework curator;
+
+    @Inject
+    public GroupContentKeySet(CuratorFramework curator) {
+        this.curator = curator;
+    }
+
+    public void add(String groupName, ContentKey key) {
+        String path = getPath(groupName, key);
+        try {
+            curator.create().creatingParentsIfNeeded().forPath(path);
+        } catch (KeeperException.NodeExistsException ignore) {
+            logger.info("node exists " + path);
+        } catch (Exception e) {
+            logger.warn("unable to create " + path , e);
+        }
+    }
+
+    public void remove(String groupName, ContentKey key) {
+        String path = getPath(groupName, key);
+        try {
+            curator.delete().forPath(path);
+        } catch (Exception e) {
+            logger.warn("unable to delete " + path , e);
+        }
+    }
+
+    public Set<ContentKey> getSet(String groupName) {
+        String path = getPath(groupName);
+        Set<ContentKey> keys = new HashSet<>();
+        try {
+            List<String> strings = curator.getChildren().forPath(path);
+            for (String string : strings) {
+                keys.add(ContentKey.fromZk(string));
+            }
+        } catch (Exception e) {
+            logger.warn("unable to get set " + path , e);
+        }
+        return keys;
+    }
+
+    private String getPath(String groupName) {
+        return "/GroupInFlight/" + groupName;
+    }
+
+    private String getPath(String groupName, ContentKey key) {
+        return getPath(groupName) + "/" + key.toZk();
+    }
+
+    public static void delete(String path, CuratorFramework curator)  {
+        try {
+            curator.delete().deletingChildrenIfNeeded().forPath(path);
+        } catch (Exception e) {
+            logger.warn("unable to delete {} {}", path, e.getMessage());
+        }
+    }
+}
