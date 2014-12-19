a0bdcf7c117dbef3bff0aca0e0656eca97160967
moulliet <gmoulliet@flightstats.com>
2014-12-19 12:40:19 -0800

    moving test

--- src/test/integration/latest_item_in_channel_spec.js
+++ src/test/integration/latest_item_in_channel_spec.js
@@ -1,27 +1,50 @@
-require('./../integration/integration_config.js');
+require('./integration_config.js');
 
+var request = require('request');
 var channelName = utils.randomChannelName();
+var channelResource = channelUrl + "/" + channelName;
 var testName = __filename;
-
 utils.configureFrisby();
 
-utils.runInTestChannel(testName, channelName, function (channelResponse) {
-    var channelResource = channelResponse['_links']['self']['href'];
-    var latestResource = channelResponse['_links']['latest']['href'];
-    var messageText = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA_bee_buzzzzzzzz";
-    frisby.create(testName + ': Inserting latest item')
-        .post(channelResource, null, { body : messageText})
-        .addHeader("Content-Type", "text/plain")
-        .expectStatus(201)
-        .afterJSON(function (response) {
-            var itemUrl = response['_links']['self']['href'];
-            frisby.create(testName + ': Fetching latest item from channel')
-                .get(latestResource, {followRedirect : false})
-                .expectStatus(303)
-                // This sucks, but apparently frisby or request or somebody hides this from us.
-                //.expectHeader('Location', itemUrl)
-                .toss();
-        })
-        .toss();
-});
 
+/**
+ * create a channel
+ * post an item
+ * does get the item back out with latest - stable
+ * get the item back out with latest - unstable
+ */
+describe(testName, function () {
+
+    utils.putChannel(channelName);
+
+    utils.addItem(channelResource, 201);
+
+    var posted;
+
+    it('posts item', function () {
+        utils.postItemQ(channelResource)
+            .then(function (value) {
+                posted = value.response.headers.location;
+            });
+    });
+
+    it("gets latest stable in channel ", function (done) {
+        request.get({url: channelResource + '/latest', followRedirect: false},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+    });
+
+    it("gets latest unstable in channel ", function (done) {
+        request.get({url: channelResource + '/latest?stable=false', followRedirect: false},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(303);
+                expect(response.headers.location).toBe(posted);
+                done();
+            });
+    });
+
+});
