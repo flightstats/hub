11554e16184a059c9b9ff949b3f56ec8b86e2a9e
moulliet <gmoulliet@flightstats.com>
2014-12-20 09:27:56 -0800

    rearranging for tests

--- src/test/java/com/flightstats/hub/spoke/SpokeContentDaoTest.java
+++ src/test/java/com/flightstats/hub/spoke/SpokeContentDaoTest.java
@@ -1,26 +1,39 @@
 package com.flightstats.hub.spoke;
 
+import com.flightstats.hub.model.Content;
+import com.flightstats.hub.model.ContentKey;
+import com.flightstats.hub.test.Integration;
+import com.google.inject.Injector;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class SpokeContentDaoTest {
 
+    private static SpokeContentDao spokeContentDao;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Injector injector = Integration.startRealHub();
+        spokeContentDao = injector.getInstance(SpokeContentDao.class);
+    }
+
     @Test
     public void testWriteRead() throws Exception {
-
-       /* SpokeContentDao spokeContentDao = new SpokeContentDao(new FileSpokeStore(Files.createTempDir().getPath()));
         Content content = Content.builder()
                 .withData("this is my dataz!".getBytes())
                 .withContentType("typing")
                 .withUser("awesome")
                 .withContentLanguage("english")
                 .build();
-        InsertedContentKey insertedContentKey = spokeContentDao.write("stuff", content, 1);
+        ContentKey insertedContentKey = spokeContentDao.write("stuff", content);
         assertNotNull(insertedContentKey);
 
-        Content read = spokeContentDao.read("stuff", insertedContentKey.getKey());
+        Content read = spokeContentDao.read("stuff", insertedContentKey);
         assertNotNull(read);
         assertArrayEquals(content.getData(), read.getData());
-        assertEquals(content, read);*/
+        assertEquals(content, read);
     }
 
 }
