0989668c8155d35e0c91c9de4f21d91a6aa7d938
moulliet <gmoulliet@flightstats.com>
2015-01-07 11:35:20 -0800

    fixing consistent & heisenberg for replication metrics

--- src/main/java/com/flightstats/hub/model/TracesImpl.java
+++ src/main/java/com/flightstats/hub/model/TracesImpl.java
@@ -39,6 +39,11 @@ public class TracesImpl implements Traces {
     }
 
     @Override
+    public long getStart() {
+        return start;
+    }
+
+    @Override
     public void logSlow(long millis, Logger logger) {
         long processingTime = System.currentTimeMillis() - start;
         if (processingTime >= millis) {
