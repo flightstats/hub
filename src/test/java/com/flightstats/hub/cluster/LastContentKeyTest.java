317ef238cc8c1b8ffea758207a3f20c1d4fd81a0
moulliet <gmoulliet@flightstats.com>
2015-01-10 18:35:19 -0800

    fixing for sequential ordering bug with replication

--- src/test/java/com/flightstats/hub/cluster/LastContentKeyTest.java
+++ src/test/java/com/flightstats/hub/cluster/LastContentKeyTest.java
@@ -1,6 +1,5 @@
 package com.flightstats.hub.cluster;
 
-import com.flightstats.hub.group.GroupContentKey;
 import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.test.Integration;
 import org.apache.curator.framework.CuratorFramework;
@@ -12,10 +11,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class GroupContentKeyTest {
+public class LastContentKeyTest {
 
     private static CuratorFramework curator;
-    private GroupContentKey contentKeyValue;
+    private LastContentKey contentKeyValue;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -24,7 +23,7 @@ public class GroupContentKeyTest {
 
     @Before
     public void setUp() throws Exception {
-        contentKeyValue = new GroupContentKey(curator);
+        contentKeyValue = new LastContentKey(curator);
     }
 
     @Test
@@ -32,32 +31,32 @@ public class GroupContentKeyTest {
         String name = "testLifeCycle";
         DateTime start = new DateTime(2014, 12, 3, 20, 45, DateTimeZone.UTC);
         ContentKey key1 = new ContentKey(start, "B");
-        contentKeyValue.initialize(name, key1);
-        assertEquals(key1, contentKeyValue.get(name, new ContentKey()));
+        contentKeyValue.initialize(name, key1, "/GroupLastCompleted/");
+        assertEquals(key1, contentKeyValue.get(name, new ContentKey(), "/GroupLastCompleted/"));
 
         ContentKey key2 = new ContentKey(start.plusMillis(1), "C");
-        contentKeyValue.updateIncrease(key2, name);
-        assertEquals(key2, contentKeyValue.get(name, new ContentKey()));
+        contentKeyValue.updateIncrease(key2, name, "/GroupLastCompleted/");
+        assertEquals(key2, contentKeyValue.get(name, new ContentKey(), "/GroupLastCompleted/"));
 
         ContentKey key3 = new ContentKey(start.minusMillis(1), "A");
-        contentKeyValue.updateIncrease(key3, name);
-        assertEquals(key2, contentKeyValue.get(name, new ContentKey()));
+        contentKeyValue.updateIncrease(key3, name, "/GroupLastCompleted/");
+        assertEquals(key2, contentKeyValue.get(name, new ContentKey(), "/GroupLastCompleted/"));
 
         ContentKey key4 = new ContentKey(start.plusMinutes(1), "D");
-        contentKeyValue.updateIncrease(key4, name);
-        assertEquals(key4, contentKeyValue.get(name, new ContentKey()));
+        contentKeyValue.updateIncrease(key4, name, "/GroupLastCompleted/");
+        assertEquals(key4, contentKeyValue.get(name, new ContentKey(), "/GroupLastCompleted/"));
 
-        contentKeyValue.delete(name);
+        contentKeyValue.delete(name, "/GroupLastCompleted/");
         ContentKey contentKey = new ContentKey();
-        assertEquals(contentKey, contentKeyValue.get(name, contentKey));
+        assertEquals(contentKey, contentKeyValue.get(name, contentKey, "/GroupLastCompleted/"));
     }
 
     @Test
     public void testCreateIfMissing() throws Exception {
         String name = "testCreateIfMissing";
         ContentKey key = new ContentKey();
-        assertEquals(key, contentKeyValue.get(name, key));
-        assertEquals(key, contentKeyValue.get(name, new ContentKey()));
+        assertEquals(key, contentKeyValue.get(name, key, "/GroupLastCompleted/"));
+        assertEquals(key, contentKeyValue.get(name, new ContentKey(), "/GroupLastCompleted/"));
     }
 
 }
