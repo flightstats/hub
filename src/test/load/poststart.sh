528e8e5ad9201da6a7567e221ccf6b2be4ec250c
moulliet <gmoulliet@flightstats.com>
2015-01-14 12:27:29 -0800

    reducing load in dev and staging

--- src/test/load/poststart.sh
+++ src/test/load/poststart.sh
@@ -4,5 +4,18 @@
 # locust_count:20
 # hatch_rate:1
 
-sleep 1
-curl --data "locust_count=20&hatch_rate=1" http://localhost:8089/swarm
+HOST=`hostname`
+
+case ${HOST} in
+	hub-node-tester.cloud-east.dev)
+	    COUNT=5
+	     ;;
+	hub-node-tester.cloud-east.staging)
+	    COUNT=5
+	     ;;
+	*)
+		echo "host not supported ${HOST}" ; exit ;;
+esac
+
+sleep 2
+curl --data "locust_count=${COUNT}&hatch_rate=1" http://localhost:8089/swarm
