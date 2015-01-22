5141efd1af17681de0122bde4b08920f4e1772f3
moulliet <gmoulliet@flightstats.com>
2015-01-22 13:44:30 -0800

    working on rest endpoints

--- src/main/java/com/flightstats/hub/model/ChannelConfiguration.java
+++ src/main/java/com/flightstats/hub/model/ChannelConfiguration.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flightstats.hub.exception.InvalidRequestException;
+import com.flightstats.hub.replication.ReplicationService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.EqualsAndHashCode;
@@ -36,10 +37,10 @@ public class ChannelConfiguration implements Serializable {
         this.tags.addAll(builder.tags);
         if (StringUtils.isBlank(builder.replicationSource)) {
             this.replicationSource = "";
-            tags.remove("replicated");
+            tags.remove(ReplicationService.REPLICATED);
         } else {
             this.replicationSource = builder.replicationSource;
-            tags.add("replicated");
+            tags.add(ReplicationService.REPLICATED);
         }
     }
 
--- src/main/java/com/flightstats/hub/replication/ReplicationResource.java
+++ src/main/java/com/flightstats/hub/replication/ReplicationResource.java
@@ -1,11 +1,14 @@
 package com.flightstats.hub.replication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.flightstats.hub.model.ChannelConfiguration;
 import com.google.inject.Inject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,25 +16,46 @@ import javax.ws.rs.core.UriInfo;
 
 @Path("/replication")
 public class ReplicationResource {
-    private final static Logger logger = LoggerFactory.getLogger(ReplicationResource.class);
-
-    private final ReplicationService replicationService;
-
-    private UriInfo uriInfo;
 
     @Inject
-    public ReplicationResource(ReplicationService replicationService, UriInfo uriInfo) {
-        this.replicationService = replicationService;
-        this.uriInfo = uriInfo;
-    }
+    private ReplicationService replicationService;
+
+    @Inject
+    private UriInfo uriInfo;
+
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getStatus() throws Exception {
-        //todo - gfm - 1/20/15 - this should return the list of replicated channels
-        return null;
+    public Response getReplicatedChannels() throws Exception {
+        ObjectNode root = mapper.createObjectNode();
+        ObjectNode links = root.putObject("_links");
+        ObjectNode self = links.putObject("self");
+        String baseUri = uriInfo.getRequestUri().toString();
+        self.put("href", baseUri);
+        Iterable<ChannelConfiguration> channels = replicationService.getReplicatingChannels();
+        ArrayNode replicated = links.putArray("replicated");
+        for (ChannelConfiguration channel : channels) {
+            ObjectNode object = replicated.addObject();
+            object.put("name", channel.getName());
+            object.put("href", baseUri + "/" + channel.getName());
+            object.put("replicationSource", channel.getReplicationSource());
+        }
+        return Response.ok(root).build();
     }
 
-    //todo - gfm - 1/20/15 - add get for channels from list returned in GET /
+    @GET
+    @Path("/{channel}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReplicatedChannel(@PathParam("channelName") String channelName) throws Exception {
+        ObjectNode root = mapper.createObjectNode();
+        ObjectNode links = root.putObject("_links");
+        ObjectNode self = links.putObject("self");
+        String baseUri = uriInfo.getRequestUri().toString();
+        self.put("href", baseUri);
+        replicationService.getStatus(channelName, root, uriInfo);
+
+        return Response.ok(root).build();
+    }
 
 }
