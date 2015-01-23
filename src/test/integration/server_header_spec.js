244bf0597bf7788c772cd3172fee0e6d321eebb3
moulliet <gmoulliet@flightstats.com>
2015-01-23 11:48:09 -0800

    adding hub-v2 response header everywhere

---
+++ src/test/integration/server_header_spec.js
@@ -0,0 +1,35 @@
+require('./integration_config.js');
+var request = require('request');
+var testName = __filename;
+
+describe(testName, function () {
+
+    var rootUrl = 'http://' + hubDomain + '/';
+
+    function verifyHeader(url, done) {
+
+        request.get({
+                url: url,
+                headers: {"Content-Type": "application/json"}
+            },
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(200);
+                expect(response.headers.server.substring(0, 6)).toBe('Hub/v2');
+                done();
+            });
+    }
+
+    it('verfies header at root ' + rootUrl, function (done) {
+        verifyHeader(rootUrl, done);
+    });
+
+    it('verfies header at channel ', function (done) {
+        verifyHeader(rootUrl + 'channel', done);
+    });
+
+    it('verfies header at health ', function (done) {
+        verifyHeader(rootUrl + 'health', done);
+    });
+
+});
