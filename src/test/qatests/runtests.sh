2168a2042c4d35702b761c675d66c96ba1f21ac2
moulliet <gmoulliet@flightstats.com>
2015-02-11 09:25:15 -0800

    removing unused qa tests

--- src/test/qatests/runtests.sh
+++
@@ -1,6 +0,0 @@
-curl http://artifactory.office/artifactory/qa/js_utils/randomUtils.js -o "randomUtils.js"
-curl http://artifactory.office/artifactory/qa/js_utils/arrayHelpers.js -o "arrayHelpers.js"
-curl http://artifactory.office/artifactory/qa/js_utils/contentTypes.js -o "contentTypes.js"
-curl http://artifactory.office/artifactory/qa/js_utils/genericUtils.js -o "genericUtils.js"
-#mocha $(find . -type f -name '*test.js') -R spec --timeout 45000 $@
-mocha ./Tests -R spec --timeout 45000 $@
