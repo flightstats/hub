1a4af040e3d906fdb18c1c47906b14bdd3100bf0
moulliet <gmoulliet@flightstats.com>
2015-02-12 11:36:48 -0800

    removing host names

--- src/test/java/com/flightstats/hub/time/NTPMonitorTest.java
+++ src/test/java/com/flightstats/hub/time/NTPMonitorTest.java
@@ -13,10 +13,10 @@ public class NTPMonitorTest {
         String[] output = {
                 "     remote           refid      st t when poll reach   delay   offset  jitter",
                 "==============================================================================",
-                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
-                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   -1.128   0.371",
-                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269    0.606   0.449",
-                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390    0.277   0.146"
+                "+util            91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*n2              20.9.10.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-01       20.20.20.5       5 u  622 1024  337    0.269    0.606   0.449",
+                "-hub-v2-02       20.20.20.5       5 u 1026 1024  376    0.390    0.277   0.146"
         };
         assertEquals(0.606, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
         assertEquals(-1.128, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
@@ -27,10 +27,10 @@ public class NTPMonitorTest {
         String[] output = {
                 "     remote           refid      st t when poll reach   delay   offset  jitter",
                 "==============================================================================",
-                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
-                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   -1.128   0.371",
-                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269   -0.606   0.449",
-                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390   -0.277   0.146"
+                "+util            91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*n2              20.9.10.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-01       20.20.20.5       5 u  622 1024  337    0.269   -0.606   0.449",
+                "-hub-v2-02       20.20.20.5       5 u 1026 1024  376    0.390   -0.277   0.146"
         };
         assertEquals(0.606, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
         assertEquals(-1.128, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
@@ -41,10 +41,10 @@ public class NTPMonitorTest {
         String[] output = {
                 "     remote           refid      st t when poll reach   delay   offset  jitter",
                 "==============================================================================",
-                "+utility01.util. 91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
-                "*ns2.cloud-east. 10.1.11.5        4 u  996 1024  377    1.527   1.128   0.371",
-                "+hub-v2-int-01.c 10.11.11.5       5 u  622 1024  337    0.269    0.606   0.449",
-                "-hub-v2-int-02.c 10.11.11.5       5 u 1026 1024  376    0.390   -0.277   0.146"
+                "+util            91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*n2              20.9.10.5        4 u  996 1024  377    1.527   1.128   0.371",
+                "+hub-v2-01       20.20.20.5       5 u  622 1024  337    0.269    0.606   0.449",
+                "-hub-v2-02       20.20.20.5       5 u 1026 1024  376    0.390   -0.277   0.146"
         };
         assertEquals(0.883, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
         assertEquals(1.128, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
@@ -55,7 +55,7 @@ public class NTPMonitorTest {
         String[] output = {
                 "remote           refid      st t when poll reach   delay   offset  jitter",
                 "==============================================================================",
-                "*ns2.cloud-east. 10.1.11.5        4 u  898 1024  377    1.538   -1.325   1.167"
+                "*n2             20.9.10.5        4 u  898 1024  377    1.538   -1.325   1.167"
         };
         assertEquals(0.0, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
         assertEquals(-1.325, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
