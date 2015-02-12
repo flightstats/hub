9aa6a8da749cb2a68a2dd6a8512e9bb91ca02b01
moulliet <gmoulliet@flightstats.com>
2015-02-12 11:36:48 -0800

    removing host names

--- src/test/java/com/flightstats/hub/dao/s3/S3WriterManagerTest.java
+++ src/test/java/com/flightstats/hub/dao/s3/S3WriterManagerTest.java
@@ -1,13 +1,14 @@
 package com.flightstats.hub.dao.s3;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class S3WriterManagerTest {
     @Test
     public void testServerOffset() throws Exception {
-        assertEquals(15, S3WriterManager.serverOffset("hub-v2-01.cloud-east.dev"));
-        assertEquals( 30, S3WriterManager.serverOffset("hub-v2-02.cloud-east.dev"));
-        assertEquals( 45, S3WriterManager.serverOffset("hub-v2-03.cloud-east.dev"));
+        assertEquals(15, S3WriterManager.serverOffset("hub-v2-01.hub"));
+        assertEquals(30, S3WriterManager.serverOffset("hub-v2-02.hub"));
+        assertEquals(45, S3WriterManager.serverOffset("hub-v2-03.hub"));
     }
 }
