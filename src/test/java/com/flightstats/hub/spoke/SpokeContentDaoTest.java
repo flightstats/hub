085254c1dd70abc4b48f5c756d62d06cb246206c
moulliet <gmoulliet@flightstats.com>
2015-03-24 10:36:17 -0700

    fixing sporadically failing test

--- src/test/java/com/flightstats/hub/spoke/SpokeContentDaoTest.java
+++ src/test/java/com/flightstats/hub/spoke/SpokeContentDaoTest.java
@@ -2,18 +2,33 @@ package com.flightstats.hub.spoke;
 
 import com.flightstats.hub.dao.ContentDaoUtil;
 import com.flightstats.hub.test.Integration;
+import com.flightstats.hub.util.Sleeper;
 import com.google.inject.Injector;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SpokeContentDaoTest {
 
+    private final static Logger logger = LoggerFactory.getLogger(SpokeContentDaoTest.class);
     private static ContentDaoUtil util;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
         Injector injector = Integration.startAwsHub();
         util = new ContentDaoUtil(injector.getInstance(SpokeContentDao.class));
+        CuratorSpokeCluster cluster = injector.getInstance(CuratorSpokeCluster.class);
+        for (int i = 0; i < 10; i++) {
+            if (cluster.getServers().size() == 0) {
+                logger.info("no servers yet...");
+                Sleeper.sleep(500);
+            } else {
+                logger.info("servers {}", cluster.getServers());
+                return;
+            }
+        }
+        logger.info("no servers found");
     }
 
     @Test
