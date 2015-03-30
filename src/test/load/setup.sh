6cf38bc9c1d610c48030f9d2e38774148c1874f0
moulliet <gmoulliet@flightstats.com>
2015-03-30 13:55:52 -0700

    updating locust setup

--- src/test/load/setup.sh
+++ src/test/load/setup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-#skipping
-#sudo apt-get install gcc
+
 sudo apt-get install build-essential
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 sudo apt-get install python2.7
-sudo apt-get install python-pip
 sudo apt-get install libevent-dev
 sudo apt-get install python-dev
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
 sudo pip install locustio --upgrade
-#sudo pip install https://github.com/surfly/gevent/releases/download/1.0.1/gevent-1.0.1.tar.gz
 sudo pip install websocket-client
 sudo pip install httplib2
 
