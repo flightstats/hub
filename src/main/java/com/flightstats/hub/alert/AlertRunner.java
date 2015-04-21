4c0ff0757d8b717c521157be4320d89e3d158f2b
moulliet <gmoulliet@flightstats.com>
2015-04-21 14:23:45 -0700

    creating channels on startup

--- src/main/java/com/flightstats/hub/alert/AlertRunner.java
+++ src/main/java/com/flightstats/hub/alert/AlertRunner.java
@@ -203,13 +203,17 @@ public class AlertRunner implements Leader {
     }
 
     private void createChannels() {
-        client.resource(hubAppUrl + "/channel/" + alertChannelName)
-                .put("{'ttlDays':1000, 'description:'Configuration for hub alerts'}");
-        client.resource(hubAppUrl + "/channel/" + alertChannelStatus)
-                .put("{'ttlDays':7, 'description:'Status for hub alerts'}");
-        String alertChannelEscalate = HubProperties.getProperty("alert.channel.escalate", "escalationAlerts");
-        client.resource(hubAppUrl + "/channel/" + alertChannelEscalate)
-                .put("{'ttlDays':14, 'description:'alerts to be sent and confirmations'}");
+        try {
+            client.resource(hubAppUrl + "/channel/" + alertChannelName)
+                    .put("{'ttlDays':1000, 'description:'Configuration for hub alerts'}");
+            client.resource(hubAppUrl + "/channel/" + alertChannelStatus)
+                    .put("{'ttlDays':7, 'description:'Status for hub alerts'}");
+            String alertChannelEscalate = HubProperties.getProperty("alert.channel.escalate", "escalationAlerts");
+            client.resource(hubAppUrl + "/channel/" + alertChannelEscalate)
+                    .put("{'ttlDays':14, 'description:'alerts to be sent and confirmations'}");
+        } catch (Exception e) {
+            logger.warn("hate filled donut", e);
+        }
     }
 
     private void start() {
