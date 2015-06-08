705f200dd90fe53329be110a1c4ae441626f2575
moulliet <gmoulliet@flightstats.com>
2015-06-08 10:44:51 -0700

    update test for self reference

--- src/test/java/com/flightstats/hub/time/NTPMonitorTest.java
+++ src/test/java/com/flightstats/hub/time/NTPMonitorTest.java
@@ -60,4 +60,20 @@ public class NTPMonitorTest {
         assertEquals(0.0, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
         assertEquals(-1.325, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
     }
+
+    @Test
+    public void testPositiveSelf() throws Exception {
+        String[] output = {
+                "     remote           refid      st t when poll reach   delay   offset  jitter",
+                "==============================================================================",
+                "+util            91.189.94.4      3 u  545 1024  377   99.523   -1.032   0.631",
+                "*n2              20.9.10.5        4 u  996 1024  377    1.527   -1.128   0.371",
+                "+hub-v2-01       20.20.20.5       5 u  622 1024  337    0.269    0.606   0.449",
+                "-hub-v2-02       20.20.20.5       5 u 1026 1024  376    0.390    0.277   0.146",
+                " hub-v2-03       .INIT.          16 u    -   64    0    0.000    0.000   0.000"
+        };
+        assertEquals(0.606, NTPMonitor.parseClusterRange(Arrays.asList(output)), 0.001);
+        assertEquals(-1.128, NTPMonitor.parsePrimary(Arrays.asList(output)), 0.001);
+    }
+
 }
