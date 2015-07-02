d9a90e3cad202ed53e6e4409598f09f06f04f207
moulliet <gmoulliet@flightstats.com>
2015-07-02 13:02:51 -0700

    adding validation for earliest

--- src/test/continuous/verify_pagination_spec.js
+++ src/test/continuous/verify_pagination_spec.js
@@ -30,8 +30,33 @@ describe(testName, function () {
 
     var uris = [];
 
+    var millis = 0;
+    var channel;
+    it('0 - loads channel info ', function (done) {
+        console.log('channelUrl', channelUrl);
+        agent
+            .get(channelUrl)
+            .end(function (err, res) {
+                expect(err).toBe(null);
+                expect(res.status).toBe(200);
+                channel = res.body;
+                expect(channel.ttlDays).toBeGreaterThan(0);
+
+                agent
+                    .get(res.body._links.time.href)
+                    .end(function (err, res) {
+                        expect(err).toBe(null);
+                        expect(res.status).toBe(200);
+                        channel.millis = res.body.now.millis;
+                        expect(channel.millis).toBeGreaterThan(1435865512097);
+                        console.log('channel', channel);
+                        done();
+                    })
+            })
+    }, 60 * 1000);
+
     it('1 - gets two days ago', function (done) {
-        console.log('channelUrl', twoDaysAgo);
+        console.log('twoDaysAgo', twoDaysAgo);
         agent
             .get(twoDaysAgo)
             .end(function (err, res) {
@@ -97,5 +122,23 @@ describe(testName, function () {
                 done();
             })
     }
+
+    it('6 - gets earliest', function (done) {
+        console.log('earliest', channel._links.earliest.href);
+        agent
+            .get(channel._links.earliest.href)
+            .redirects(0)
+            .end(function (err, res) {
+                expect(err).toBe(null);
+                expect(res.status).toBe(303);
+                var time = moment(channel.millis).subtract(channel.ttlDays, 'days').utc();
+                var timeUrlSegments = res.headers.location.substring(channelUrl.length)
+                timeUrlSegments = timeUrlSegments.substring(0, timeUrlSegments.lastIndexOf('/'));
+                var earliestTime = moment(timeUrlSegments + ' +0000', '/YYYY/MM/DD/HH/mm/ss/SSS Z');
+                expect(earliestTime.isAfter(time)).toBe(true);
+                expect(earliestTime.isBefore(time.add(1, 'hours'))).toBe(true);
+                done();
+            })
+    }, 60 * 1000);
 
 });
