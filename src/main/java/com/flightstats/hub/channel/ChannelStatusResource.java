b08fc42bacc876eb0967bda58cc7a1e408f7efef
moulliet <gmoulliet@flightstats.com>
2015-07-15 14:18:32 -0700

    returning 404 for non-existent channels

--- src/main/java/com/flightstats/hub/channel/ChannelStatusResource.java
+++ src/main/java/com/flightstats/hub/channel/ChannelStatusResource.java
@@ -33,6 +33,10 @@ public class ChannelStatusResource {
     public Response getLatest(@PathParam("channel") String channel,
                               @QueryParam("stable") @DefaultValue("true") boolean stable,
                               @QueryParam("trace") @DefaultValue("false") boolean trace) {
+        ChannelConfig channelConfig = channelService.getChannelConfig(channel);
+        if (null == channelConfig) {
+            return Response.status(404).build();
+        }
         ObjectNode root = mapper.createObjectNode();
         ObjectNode links = root.putObject("_links");
         ObjectNode self = links.putObject("self");
