b5ef01ff60160b884ebcc598fb234cf93657a7fc
moulliet <gmoulliet@flightstats.com>
2015-08-20 14:14:39 -0700

    changing boundary

--- src/main/java/com/flightstats/hub/channel/MultiPartBuilder.java
+++ src/main/java/com/flightstats/hub/channel/MultiPartBuilder.java
@@ -7,7 +7,6 @@ import com.flightstats.hub.model.Content;
 import com.flightstats.hub.model.ContentKey;
 import com.google.common.base.Optional;
 import com.google.common.io.ByteStreams;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,45 +17,51 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collection;
-import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class MultiPartBuilder {
 
-    public static final String CRLF = "\r\n";
     private final static Logger logger = LoggerFactory.getLogger(MultiPartBuilder.class);
+
+    private static final byte[] CRLF = "\r\n".getBytes();
+    private static final String BOUNDARY = "||||||~~~~~~||||||~~~~~~||||||~~~~~~||||||~~~~~~||||||~~~~~~||||||";
+    private static final byte[] START_BOUNDARY = ("--" + BOUNDARY + "\r\n").getBytes();
+    private static final byte[] END_BOUNDARY = ("--" + BOUNDARY + "--").getBytes();
+    private static final String MULTIPART = "multipart/mixed; boundary=" + BOUNDARY;
+    private static final byte[] CONTENT_TYPE = "Content-Type: ".getBytes();
+    private static final byte[] CONTENT_KEY = "Content-Key: ".getBytes();
 
     public static Response build(Collection<ContentKey> keys, String channel,
                                  ChannelService channelService, UriInfo uriInfo) {
-        return write((BufferedOutputStream output, String boundary) -> {
+        return write((BufferedOutputStream output) -> {
             for (ContentKey key : keys) {
-                writeContent(uriInfo, output, key, channel, channelService, boundary);
+                writeContent(uriInfo, output, key, channel, channelService);
             }
         });
     }
 
     public static Response buildTag(String tag, Collection<ChannelContentKey> keys,
                                     ChannelService channelService, UriInfo uriInfo) {
-        return write((BufferedOutputStream output, String boundary) -> {
+        return write((BufferedOutputStream output) -> {
             for (ChannelContentKey key : keys) {
-                writeContent(uriInfo, output, key.getContentKey(), key.getChannel(), channelService, boundary);
+                writeContent(uriInfo, output, key.getContentKey(), key.getChannel(), channelService);
             }
         });
     }
 
-    private static Response write(final BiConsumer<BufferedOutputStream, String> consumer) {
-        String boundary = RandomStringUtils.randomAlphanumeric(70);
+    private static Response write(final Consumer<BufferedOutputStream> consumer) {
         Response.ResponseBuilder builder = Response.ok((StreamingOutput) os -> {
             BufferedOutputStream output = new BufferedOutputStream(os);
-            consumer.accept(output, boundary);
-            output.write(("--" + boundary + "--").getBytes());
+            consumer.accept(output);
+            output.write(END_BOUNDARY);
             output.flush();
         });
-        builder.type("multipart/mixed; boundary=" + boundary);
+        builder.type(MULTIPART);
         return builder.build();
     }
 
     private static void writeContent(UriInfo uriInfo, BufferedOutputStream output, ContentKey key, String channel,
-                                     ChannelService channelService, String boundary) {
+                                     ChannelService channelService) {
         try {
             Request request = Request.builder()
                     .channel(channel)
@@ -66,15 +71,19 @@ public class MultiPartBuilder {
             if (content.isPresent()) {
                 URI channelUri = LinkBuilder.buildChannelUri(channel, uriInfo);
                 Content item = content.get();
-                output.write(("--" + boundary + CRLF).getBytes());
+                output.write(START_BOUNDARY);
                 if (item.getContentType().isPresent()) {
-                    output.write(("Content-Type: " + item.getContentType().get() + CRLF).getBytes());
+                    output.write(CONTENT_TYPE);
+                    output.write(item.getContentType().get().getBytes());
+                    output.write(CRLF);
                 }
                 URI uri = LinkBuilder.buildItemUri(key, channelUri);
-                output.write(("Content-Key: " + uri.toString() + CRLF).getBytes());
-                output.write(CRLF.getBytes());
+                output.write(CONTENT_KEY);
+                output.write(uri.toString().getBytes());
+                output.write(CRLF);
+                output.write(CRLF);
                 ByteStreams.copy(item.getStream(), output);
-                output.write(CRLF.getBytes());
+                output.write(CRLF);
                 output.flush();
             }
         } catch (IOException e) {
