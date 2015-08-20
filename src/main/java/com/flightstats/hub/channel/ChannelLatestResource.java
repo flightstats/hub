26746b223f5ae0db1cb0ed4131b0aae640a3a959
moulliet <gmoulliet@flightstats.com>
2015-08-19 17:11:19 -0700

    simple batching for latest

--- src/main/java/com/flightstats/hub/channel/ChannelLatestResource.java
+++ src/main/java/com/flightstats/hub/channel/ChannelLatestResource.java
@@ -2,15 +2,23 @@ package com.flightstats.hub.channel;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flightstats.hub.dao.ChannelService;
+import com.flightstats.hub.dao.Request;
+import com.flightstats.hub.model.Content;
 import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.model.DirectionQuery;
 import com.google.common.base.Optional;
+import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.util.Collection;
 
@@ -20,6 +28,7 @@ import static javax.ws.rs.core.Response.Status.SEE_OTHER;
 @Path("/channel/{channel: .*}/latest")
 public class ChannelLatestResource {
 
+    public static final String CRLF = "\r\n";
     @Inject
     private UriInfo uriInfo;
     @Inject
@@ -54,6 +63,7 @@ public class ChannelLatestResource {
                                    @PathParam("count") int count,
                                    @QueryParam("stable") @DefaultValue("true") boolean stable,
                                    @QueryParam("trace") @DefaultValue("false") boolean trace,
+                                   @QueryParam("batch") @DefaultValue("false") boolean batch,
                                    @QueryParam("tag") String tag) {
         if (tag != null) {
             return tagLatestResource.getLatestCount(tag, count, stable, trace);
@@ -73,7 +83,46 @@ public class ChannelLatestResource {
         query.trace(trace);
         Collection<ContentKey> keys = channelService.getKeys(query);
         keys.add(latest.get());
-        return LinkBuilder.directionalResponse(channel, keys, count, query, mapper, uriInfo, true);
+        String boundary = RandomStringUtils.randomAlphanumeric(70);
+        if (batch) {
+            //todo - gfm - 8/19/15 - factor this out
+            StreamingOutput stream = new StreamingOutput() {
+                @Override
+                public void write(OutputStream os) throws IOException,
+                        WebApplicationException {
+                    URI channelUri = LinkBuilder.buildChannelUri(channel, uriInfo);
+                    BufferedOutputStream output = new BufferedOutputStream(os);
+                    for (ContentKey key : keys) {
+                        Request request = Request.builder()
+                                .channel(channel)
+                                .key(key)
+                                .build();
+                        Optional<Content> content = channelService.getValue(request);
+                        if (content.isPresent()) {
+                            Content item = content.get();
+                            output.write(("--" + boundary + CRLF).getBytes());
+                            if (item.getContentType().isPresent()) {
+                                output.write(("Content-Type: " + item.getContentType().get() + CRLF).getBytes());
+                            }
+                            URI uri = LinkBuilder.buildItemUri(key, channelUri);
+                            output.write(("Content-Key: " + uri.toString() + CRLF).getBytes());
+                            output.write(CRLF.getBytes());
+                            ByteStreams.copy(item.getStream(), output);
+                            output.write(CRLF.getBytes());
+                            output.flush();
+                        }
+                    }
+                    output.write(("--" + boundary + "--").getBytes());
+                    output.flush();
+                }
+            };
+            Response.ResponseBuilder builder = Response.ok(stream);
+            builder.type("multipart/mixed; boundary=" + boundary);
+
+            return builder.build();
+        } else {
+            return LinkBuilder.directionalResponse(channel, keys, count, query, mapper, uriInfo, true);
+        }
 
     }
 
---
+++ src/test/integration/latest_item_in_channel_batch_spec.js
@@ -0,0 +1,44 @@
+require('./integration_config.js');
+
+var request = require('request');
+var channelName = utils.randomChannelName();
+var channelResource = channelUrl + "/" + channelName;
+var testName = __filename;
+utils.configureFrisby();
+
+
+/**
+ * create a channel
+ * post two items
+ * stream both items back with batch
+ */
+describe(testName, function () {
+
+    utils.putChannel(channelName, function () {
+    }, {"name": channelName, "ttlDays": 1});
+
+    utils.addItem(channelResource, 201);
+
+    var posted;
+
+    it('posts item', function (done) {
+        utils.postItemQ(channelResource)
+            .then(function (value) {
+                posted = value.response.headers.location;
+                done();
+            });
+    });
+
+
+    it("gets multipart items ", function (done) {
+        request.get({url: channelResource + '/latest/10?stable=false&batch=true', followRedirect: false},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(200);
+                //todo - gfm - 8/19/15 - parse multipart
+                console.log("headers", response.headers);
+                console.log("body", response.body);
+                done();
+            });
+    });
+});
