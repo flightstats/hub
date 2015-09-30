e3ae37a567e218e5d4e558fbb69f5b5db79d5a69
moulliet <gmoulliet@flightstats.com>
2015-09-30 15:33:33 -0700

    increasing query volume

--- src/test/load/load.py
+++ src/test/load/load.py
@@ -18,6 +18,7 @@ from flask import request, jsonify
 
 
 
+
 # Usage:
 # locust -f read-write-group.py -H http://localhost:9080
 # nohup locust -f read-write-group.py -H http://hub &
@@ -123,7 +124,7 @@ class WebsiteTasks(TaskSet):
     def hour_query(self):
         self.client.get(self.time_path("hour"), name="time_hour")
 
-    @task(1)
+    @task(10)
     def minute_query(self):
         self.client.get(self.time_path("minute"), name="time_minute")
 
