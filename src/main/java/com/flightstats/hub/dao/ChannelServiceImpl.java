b54cfb6fb5b074ea3c9f807994decf669fd16346
moulliet <gmoulliet@flightstats.com>
2015-12-03 12:50:05 -0800

    removing sleep code

--- src/main/java/com/flightstats/hub/dao/ChannelServiceImpl.java
+++ src/main/java/com/flightstats/hub/dao/ChannelServiceImpl.java
@@ -280,7 +280,6 @@ public class ChannelServiceImpl implements ChannelService {
         if (replicating) {
             replicatorManager.notifyWatchers();
         }
-        sleep(start);
         return true;
     }
 }
