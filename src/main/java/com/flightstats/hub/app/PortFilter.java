fa1f7bb50e72ef8ce18e86b2ba133497169427c8
moulliet <gmoulliet@flightstats.com>
2016-01-12 15:47:15 -0800

    more trying to fix port

--- src/main/java/com/flightstats/hub/app/PortFilter.java
+++ src/main/java/com/flightstats/hub/app/PortFilter.java
@@ -7,6 +7,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URI;
@@ -19,11 +20,12 @@ public class PortFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext request) throws IOException {
-        URI requestUri = request.getUriInfo().getRequestUri();
-        if (requestUri.getPort() == 80) {
-            URI uri = UriBuilder.fromUri(requestUri).port(-1).build();
-            request.setRequestUri(uri);
-            logger.trace("ignoring default port with {} instead of {}", uri, requestUri);
+        UriInfo uriInfo = request.getUriInfo();
+        if (uriInfo.getBaseUri().getPort() == 80) {
+            URI baseUri = UriBuilder.fromUri(uriInfo.getBaseUri()).port(-1).build();
+            URI requestUri = UriBuilder.fromUri(uriInfo.getRequestUri()).port(-1).build();
+            request.setRequestUri(baseUri, requestUri);
+            logger.trace("ignoring default port with {} and {}", baseUri, requestUri);
         }
     }
 }
