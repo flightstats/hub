1010ca954dd3a0e5b1706a6330a81568bf4641ae
moulliet <gmoulliet@flightstats.com>
2016-03-07 14:43:25 -0800

    updating npm versions

--- src/test/integration/install.sh
+++ src/test/integration/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo npm install -g async@~0.2.0 chai@~1.9.0 feed-read@~0.0.1 frisby@~0.7.0 jasmine-node@~1.14.5 lodash@~2.4.0 moment@~2.5.0 request@~2.33.0 superagent@~0.16.0 ws@~0.4.0 ip@0.3.0 bower grunt-cli q@1.0.1
+sudo npm install -g async@~1.3.0 chai@~1.9.0 feed-read@~0.0.1 frisby@~0.7.0 jasmine-node@~1.14.5 lodash@3.7.0 moment@~2.5.0 request@~2.33.0 superagent@~0.16.0 ws@~0.4.0 q@1.0.1 ip@0.3.0 parse-link-header eventsource@0.1.6
