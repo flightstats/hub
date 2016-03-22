bfb340f7350f988d89477495bd7f80f4f1d5c295
tflug <tflug@flightstats.com>
2016-03-21 21:37:41 -0700

    Trying to remove sending of data.

--- src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
+++ src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
@@ -1,13 +1,9 @@
 package com.flightstats.hub.filter;
 
-import com.timgroup.statsd.NonBlockingStatsDClient;
-import com.timgroup.statsd.StatsDClient;
-
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URI;
 
@@ -15,10 +11,10 @@ import java.net.URI;
  * Filter class to handle intercepting requests and respones from the Hub and pipe statistics to
  * DogStatsD (DataDog) agent running on the server.
  */
-@Provider
+//@Provider
 public class DataDogRequestFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-    private final static StatsDClient statsd = new NonBlockingStatsDClient(null, "localhost", 8125, new String[]{"tag:value"});
+//    private final static StatsDClient statsd = new NonBlockingStatsDClient(null, "localhost", 8125, new String[]{"tag:value"});
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
