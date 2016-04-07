1d0977a9901bed985e70189e60526c69907c74dd
tflug <tflug@flightstats.com>
2016-04-07 10:31:39 -0700

    Improved tests and isolated key words for parsing.

--- src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
+++ src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
@@ -5,6 +5,8 @@ import com.flightstats.hub.metrics.Traces;
 import com.flightstats.hub.model.Trace;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +17,7 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.ListIterator;
 
 /**
@@ -26,22 +29,64 @@ public class DataDogRequestFilter implements ContainerRequestFilter, ContainerRe
 
     private static final Logger logger = LoggerFactory.getLogger(DataDogRequestFilter.class);
 
-    private static final String CHANNEL = "channel";
-    private static final String YEAR = "year";
-    private static final String MONTH = "month";
-    private static final String DAY = "day";
-    private static final String HOUR = "hour";
-    private static final String MINUTE = "minute";
-    private static final String SECOND = "second";
-    private static final String MILISECONDS = "miliseconds";
+    public static final String CHANNEL = "C";
+    public static final String YEAR = "Y";
+    public static final String MONTH = "M";
+    public static final String DAY = "D";
+    public static final String HOUR = "h";
+    public static final String MINUTE = "m";
+    public static final String SECOND = "s";
+    public static final String MILISECONDS = "ms";
+    public static final String HASH = "hash";
+    public static final String COUNT = "count";
 
-    private static final String REQUEST_LATEST = "latest";
-    private static final String REQUEST_EARLIEST = "earliest";
-    private static final String REQUEST_BULK = "bulk";
-    private static final String REQUEST_WS = "ws";
-    private static final String REQUEST_EVENTS = "events";
-    private static final String REQUEST_TIME = "time";
-    private static final String REQUEST_STATUS = "status";
+
+    public static final String KEY_CHANNEL = "channel";
+    public static final String KEY_EARLIEST = "earliest";
+    public static final String KEY_LATEST = "latest";
+    public static final String KEY_BATCH = "batch";
+    public static final String KEY_BULK = "bulk";
+    public static final String KEY_EVENTS = "events";
+    public static final String KEY_STATUS = "status";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_SECOND = "second";
+    public static final String KEY_MINUTE = "minute";
+    public static final String KEY_HOUR = "hour";
+    public static final String KEY_DAY = "day";
+    public static final String KEY_PROVIDER = "provider";
+    public static final String KEY_TAG = "tag";
+    public static final String KEY_INTERNAL = "internal";
+    public static final String KEY_ZOOKEEPER = "zookeeper";
+    public static final String KEY_S3BATCH = "s3Batch";
+    public static final String KEY_TRACES = "traces";
+    public static final String KEY_REPLICATION = "replication";
+    public static final String KEY_SPOKE = "spoke";
+    public static final String KEY_PAYLOAD = "payload";
+    public static final String KEY_NEXT = "next";
+    //    public static final String KEY_TEST = "test";
+    public static final String KEY_MILLIS = "millis";
+    public static final String KEY_REMOTE = "remote";
+    public static final String KEY_LOCAL = "local";
+    public static final String KEY_WS = "ws";
+    public static final String KEY_GROUP = "group";
+    public static final String KEY_HEALTH = "health";
+    public static final String KEY_METRICS = "metrics";
+    public static final String KEY_TRACE = "trace";
+    public static final String KEY_PREVIOUS = "previous";
+
+    private static final String[] RESERVED_WORDS = {KEY_CHANNEL, KEY_EARLIEST, KEY_LATEST, KEY_BATCH, KEY_BULK, KEY_EVENTS,
+            KEY_STATUS, KEY_TIME, KEY_SECOND, KEY_MINUTE, KEY_HOUR, KEY_DAY, KEY_PROVIDER, KEY_TAG, KEY_INTERNAL, KEY_ZOOKEEPER,
+            KEY_S3BATCH, KEY_TRACES, KEY_REPLICATION, KEY_SPOKE, KEY_PAYLOAD, KEY_NEXT, KEY_MILLIS, KEY_REMOTE,
+            KEY_LOCAL, KEY_WS, KEY_GROUP, KEY_HEALTH, KEY_METRICS, KEY_TRACE, KEY_PREVIOUS};
+
+//    private static final String[] RESERVED_WORDS = {"channel", "earliest", "latest", "batch", "bulk", "events", "status",
+//            "time", "second", "minute", "hour", "day", "provider", "tag", "internal", "zookeeper", "s3Batch",
+//            "traces", "replication", "spoke", "payload", "next", "test", "millis", "remote", "local", "ws",
+//            "group", "health", "metrics", "trace"
+//    };
+
+    private static final String[] OPTIONAL_COUNT_WORDS = {KEY_NEXT, KEY_PREVIOUS, KEY_EARLIEST, KEY_LATEST};
+
 
     public static final String HUB_DATADOG_METRICS_FLAG = "data_dog.enable";
 
@@ -103,57 +148,227 @@ public class DataDogRequestFilter implements ContainerRequestFilter, ContainerRe
      * <p>
      * Encountered query key words are included in the templated path.
      *
+     * Patterns:
+     *  /internal/:type (ws):/id
+     *  /internal/:type (spoke):/:function (payload):/path
+     *  /internal/:type (spoke):/:function (time, next, latest):/timeBasedPath
+     *  /internal/:type (spoke):/:function (next):/channel/count/startkey
+     *  /internal/:type (spoke, replication, payload, time, latest):/timeBasedPath
+     *
+     *  /channel/channelName/timeBasedPath
+     *  /channel/channelName/:function (bulk, latest)?:
+     *  /channel/channelName/time/hour
+     *
+     *  /health
+     *
+     *  /tag
+     *  /tag/:tagName:/timeDatePath
+     *  /tag/:tagName:/timeDatePath/:direction:/:count:
+     *  /tag/:tagName:/latest
+     *
      * @param path
      * @param method
      * @return
      */
     protected String constructDeclaredpath(String path, String method) {
         logger.debug("Constructing template path from {} with method {}.", path, method);
-        StringBuffer sbuff = new StringBuffer();
-        String[] splits = path.split("\\/");
-        for (int i = 0; i < splits.length; i++) {
-            if (splits[i].equals(REQUEST_LATEST) || splits[i].equals(REQUEST_EARLIEST)
-                    || splits[i].equals(REQUEST_BULK) || splits[i].equals(REQUEST_WS)
-                    || splits[i].equals(REQUEST_EVENTS) || splits[i].equals(REQUEST_TIME)
-                    || splits[i].equals(REQUEST_STATUS)) {
 
-                sbuff.append("/").append(splits[i]);
-            } else {
-                switch (i) {
-                    case 0:
-                        sbuff.append(splits[i]);
-                        break;
-                    case 1:
-                        sbuff.append("/").append(CHANNEL);
-                        break;
-                    case 2: // year
-                        sbuff.append("/").append(YEAR);
-                        break;
-                    case 3: // Month
-                        sbuff.append("/").append(MONTH);
-                        break;
-                    case 4: // day
-                        sbuff.append("/").append(DAY);
-                        break;
-                    case 5: // hour
-                        sbuff.append("/").append(HOUR);
-                        break;
-                    case 6: // minute
-                        sbuff.append("/").append(MINUTE);
-                        break;
-                    case 7: // second
-                        sbuff.append("/").append(SECOND);
-                        break;
-                    case 8: // miliseconds
-                        sbuff.append("/").append(MILISECONDS);
-                        break;
-                }
-            }
+        StringBuilder sbuff = new StringBuilder();
+        String[] splits = path.split("\\/");
+
+        if (splits[0].equals("internal")) {
+            handleInternalPath(sbuff, splits);
+        } else if (splits[0].equals("health")) {
+
+        } else if (splits[0].equals(KEY_CHANNEL) || splits[0].equals(KEY_TAG)) {  // channel request most likely
+            handleTagOrChannelPath(sbuff, splits);
         }
+
+
+//        } else if (splits[0].equals("tag")) {
+//            handleTagPath(sbuff, splits);
+//        }
 
         logger.debug("Generated template path: {}", sbuff.toString());
         return sbuff.toString();
     }
 
+    private void handleTagOrChannelPath(StringBuilder sbuff, String[] path) {
+        for (int i = 0; i < path.length; i++) {
+            int paramIndex = path[i].indexOf("?");
+            String pathItem = paramIndex == -1 ? path[i] : path[i].substring(0, paramIndex);
+            if (isOptionalParamWord(pathItem) && i < path.length - 1) {
+                sbuff.append("/").append(pathItem).append("/").append("count");
+                i++;
+            } else if (isReservedWord(pathItem)) {
+                sbuff.append("/").append(pathItem);
+                if (isOptionalParamWord(pathItem) && i < path.length - 1) {
+                    sbuff.append("/").append(COUNT);
+                    i++;
+                }
+            } else {
+                if (i == 1) {
+                    sbuff.append("/").append(path[0]);
+                } else if (i > 1) {
+                    i += appendDateTimePath(sbuff, Arrays.copyOfRange(path, i, path.length)) - 1;
+                }
+            }
+        }
+    }
+
+    /**
+     * /tag
+     * /tag/:tagName:/timeDatePath
+     * /tag/:tagName:/timeDatePath/:direction:/:count:
+     * /tag/:tagName:/latest
+     * <p>
+     * tag/test/latest?stable=true
+     * tag/test/2016/04/01/17/59/54/656/KXo3du/previous/10000
+     * tag/test/2016/04/01/14/48/23/468/E9Yfkc/next/100
+     * tag/test/2016/03/30/12/43/33/430/y8t02V
+     *
+     * @param sbuff
+     * @param path
+     */
+    private void handleTagPath(StringBuilder sbuff, String[] path) {
+        for (int i = 0; i < path.length; i++) {
+            if (isReservedWord(path[i])) {
+                sbuff.append("/").append(path[i]);
+            } else if (i == 1) {
+                // assume this is the tag name
+                sbuff.append("/tag");
+            } else if (i > 1) {
+                // it's either the time date path, direction, count
+                if (path[i].startsWith("latest?")) {
+                    sbuff.append("/latest");
+                } else {
+                    i += appendDateTimePath(sbuff, Arrays.copyOfRange(path, i, path.length));
+                }
+            }
+
+
+        }
+    }
+
+    /**
+     * /internal/:type (ws):/id
+     * /internal/:type (spoke):/:function (payload):/path
+     * /internal/:type (spoke):/:function (time, next, latest):/timeBasedPath
+     * /internal/:type (spoke):/:function (next):/channel/count/startkey
+     * /internal/:type (spoke, replication, payload, time, latest):/timeBasedPath
+     *
+     * @param sbuff
+     * @param path
+     */
+    private void handleInternalPath(StringBuilder sbuff, String[] path) {
+        for (int i = 0; i < path.length; i++) {
+            if (isReservedWord(path[i])) {
+                sbuff.append("/").append(path[i]);
+                if (path[i].equals("payload")) {
+                    // assume everything after payload is the payload path so we'll just shorthand it as payload
+                    // this may be an invalid assumption
+                    sbuff.append("/path");
+                    return;
+                } else if (path[i].equals("time")) {
+                    // build the date/time based path
+                    i += appendDateTimePath(sbuff, Arrays.copyOfRange(path, i + 1, path.length));
+                }
+            }
+        }
+    }
+
+    /**
+     * /channel/channelName/timeBasedPath
+     * /channel/channelName/:function (bulk, latest)?:
+     * /channel/channelName/time/hour
+     *
+     * @param sbuff
+     * @param path
+     */
+    private void handleChannelPath(StringBuilder sbuff, String[] path) {
+        sbuff.append(CHANNEL);
+        for (Integer i = 0; i < path.length; i++) {
+            if (isReservedWord(path[i])) {
+                sbuff.append("/").append(path[i]);
+                if (path[i].equals("time") && i < path.length - 1) {// grab the hour
+                    sbuff.append("/").append("h");
+                }
+                break;
+            } else {
+                if (i == 0) {
+                    sbuff.append("/").append(CHANNEL);
+                } else {
+                    i += appendDateTimePath(sbuff, Arrays.copyOfRange(path, i, path.length)) - 1; // minus 1 becuase it's zero based
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Handles templatizing the date/time path
+     *
+     * @param sbuff  - templated path is appended to sbuff
+     * @param splits
+     * @return i - the number of elements traversed
+     */
+    private int appendDateTimePath(StringBuilder sbuff, String[] splits) {
+        int i = 0;
+        while (i < splits.length) {
+            if (isOptionalParamWord(splits[i])) {
+                sbuff.append("/").append(splits[i]);
+                if (i < splits.length - 1) {
+                    sbuff.append("/").append("count");
+                }
+            } else {
+
+                switch (i) {
+                    case 0: // year
+                        sbuff.append("/").append(YEAR);
+                        break;
+                    case 1: // Month
+                        sbuff.append("/").append(MONTH);
+                        break;
+                    case 2: // day
+                        sbuff.append("/").append(DAY);
+                        break;
+                    case 3: // hour
+                        sbuff.append("/").append(HOUR);
+                        break;
+                    case 4: // minute
+                        sbuff.append("/").append(MINUTE);
+                        break;
+                    case 5: // second
+                        sbuff.append("/").append(SECOND);
+                        break;
+                    case 6: // milliseconds
+                        sbuff.append("/").append(MILISECONDS);
+                        break;
+                    case 7:
+                        if (isReservedWord(splits[i])) {
+                            sbuff.append("/").append(splits[i]);
+                            break;
+                        }
+
+                        sbuff.append("/").append(HASH);
+                        break;
+                }
+            }
+            i++;
+        }
+        return i;
+    }
+
+    private boolean isReservedWord(String aWord) {
+        return ArrayUtils.contains(RESERVED_WORDS, aWord);
+    }
+
+    private boolean isOptionalParamWord(String aWord) {
+        return ArrayUtils.contains(OPTIONAL_COUNT_WORDS, aWord);
+    }
+
+    private boolean isNumber(String aString) {
+        return StringUtils.isNumeric(aString);
+    }
 
 }
--- src/test/java/com/flightstats/hub/filter/TestDataDogFilterTest.java
+++ src/test/java/com/flightstats/hub/filter/TestDataDogFilterTest.java
@@ -1,50 +1,155 @@
 package com.flightstats.hub.filter;
 
 import com.flightstats.hub.app.HubProperties;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class TestDataDogFilterTest {
 
+    @BeforeClass
+    public static void setupClass() {
+        HubProperties.setProperty(DataDogRequestFilter.HUB_DATADOG_METRICS_FLAG, "true");
+    }
+
     @Test
     public void testChannelTemplateLatestPaths() {
-        HubProperties.setProperty(DataDogRequestFilter.HUB_DATADOG_METRICS_FLAG, "true");
         DataDogRequestFilter filter = new DataDogRequestFilter();
         String method = "GET";
         List<String[]> testsExpects = new ArrayList<>();
-        testsExpects.add(new String[]{"channel/test/latest", "channel/channel/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/latest", "channel/channel/year/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/03/latest", "channel/channel/year/month/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/latest", "channel/channel/year/month/day/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14/latest", "channel/channel/year/month/day/hour/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/latest", "channel/channel/year/month/day/hour/minute/latest"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/25/latest", "channel/channel/year/month/day/hour/minute/second/latest"});
+        testsExpects.add(new String[]{"channel/test/latest", "/channel/channel/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/latest", "/channel/channel/Y/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/03/latest", "/channel/channel/Y/M/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/latest", "/channel/channel/Y/M/D/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14/latest", "/channel/channel/Y/M/D/h/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/latest", "/channel/channel/Y/M/D/h/m/latest"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/25/latest", "/channel/channel/Y/M/D/h/m/s/latest"});
 
         for (String[] test : testsExpects) {
-            assertTrue(filter.constructDeclaredpath(test[0], method).equals(test[1]));
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
         }
     }
 
     @Test
     public void testChanelWildcards() {
-        HubProperties.setProperty(DataDogRequestFilter.HUB_DATADOG_METRICS_FLAG, "true");
         DataDogRequestFilter filter = new DataDogRequestFilter();
         String method = "GET";
         List<String[]> testsExpects = new ArrayList<>();
-        testsExpects.add(new String[]{"channel/test", "channel/channel"});
-        testsExpects.add(new String[]{"channel/test/2016", "channel/channel/year"});
-        testsExpects.add(new String[]{"channel/test/2016/03", "channel/channel/year/month"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24", "channel/channel/year/month/day"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14", "channel/channel/year/month/day/hour"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31", "channel/channel/year/month/day/hour/minute"});
-        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/25", "channel/channel/year/month/day/hour/minute/second"});
+        testsExpects.add(new String[]{"channel/test", "/channel/channel"});
+        testsExpects.add(new String[]{"channel/test/2016", "/channel/channel/Y"});
+        testsExpects.add(new String[]{"channel/test/2016/03", "/channel/channel/Y/M"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24", "/channel/channel/Y/M/D"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14", "/channel/channel/Y/M/D/h"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31", "/channel/channel/Y/M/D/h/m"});
+        testsExpects.add(new String[]{"channel/test/2016/03/24/14/31/25", "/channel/channel/Y/M/D/h/m/s"});
 
         for (String[] test : testsExpects) {
-            assertTrue(filter.constructDeclaredpath(test[0], method).equals(test[1]));
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
         }
     }
+
+    @Test
+    public void testChannelTime() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"channel/test/time", "/channel/channel/time"});
+        testsExpects.add(new String[]{"channel/test/time/hour", "/channel/channel/time/hour"});
+        testsExpects.add(new String[]{"channel/test/time/minute", "/channel/channel/time/minute"});
+        testsExpects.add(new String[]{"channel/test/time/second", "/channel/channel/time/second"});
+        testsExpects.add(new String[]{"channel/test/time/day", "/channel/channel/time/day"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testChannelEarliest() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"channel/test/earliest", "/channel/channel/earliest"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testChannelBulk() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"channel/test/bulk", "/channel/channel/bulk"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testChannelBatch() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"channel/test/batch", "/channel/channel/batch"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testSpokePayloadPath() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"internal/spoke/payload/somechannel/somekey", "/internal/spoke/payload/path"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testInternalSpokeTimeTimePath() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"internal/spoke/time/2016", "/internal/spoke/time/Y"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03", "/internal/spoke/time/Y/M"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03/21", "/internal/spoke/time/Y/M/D"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03/21/12", "/internal/spoke/time/Y/M/D/h"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03/21/12/31", "/internal/spoke/time/Y/M/D/h/m"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03/21/12/31/15", "/internal/spoke/time/Y/M/D/h/m/s"});
+        testsExpects.add(new String[]{"internal/spoke/time/2016/03/21/12/31/15/500", "/internal/spoke/time/Y/M/D/h/m/s/ms"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+    @Test
+    public void testTags() {
+        DataDogRequestFilter filter = new DataDogRequestFilter();
+        String method = "GET";
+        List<String[]> testsExpects = new ArrayList<>();
+        testsExpects.add(new String[]{"tag", "/tag"});
+        testsExpects.add(new String[]{"tag/test", "/tag/tag"});
+        testsExpects.add(new String[]{"tag/test/latest?stable=true", "/tag/tag/latest"});
+        testsExpects.add(new String[]{"tag/test/2016/04/01/17/59/54/656/KXo3du/previous/10000", "/tag/tag/Y/M/D/h/m/s/ms/hash/previous/count"});
+        testsExpects.add(new String[]{"tag/test/2016/04/01/14/48/23/468/E9Yfkc/next/100", "/tag/tag/Y/M/D/h/m/s/ms/hash/next/count"});
+        testsExpects.add(new String[]{"tag/test/2016/03/30/12/43/33/430/y8t02V", "/tag/tag/Y/M/D/h/m/s/ms/hash"});
+
+        for (String[] test : testsExpects) {
+            assertEquals(filter.constructDeclaredpath(test[0], method), test[1]);
+        }
+    }
+
+
 }
