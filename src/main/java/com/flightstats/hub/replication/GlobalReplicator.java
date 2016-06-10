6278a939351850b140c28213f63559915964bbe7
moulliet <gmoulliet@flightstats.com>
2016-06-10 16:42:57 -0700

    adding logging to global replication start

--- src/main/java/com/flightstats/hub/replication/GlobalReplicator.java
+++ src/main/java/com/flightstats/hub/replication/GlobalReplicator.java
@@ -6,8 +6,12 @@ import com.flightstats.hub.group.GroupService;
 import com.flightstats.hub.model.ChannelConfig;
 import com.flightstats.hub.util.HubUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GlobalReplicator implements Replicator {
+
+    private final static Logger logger = LoggerFactory.getLogger(GlobalReplicator.class);
 
     private static final HubUtils hubUtils = HubProvider.getInstance(HubUtils.class);
     private static final GroupService groupService = HubProvider.getInstance(GroupService.class);
@@ -22,15 +26,23 @@ class GlobalReplicator implements Replicator {
 
     public void start() {
         String channelName = channel.getName();
-        hubUtils.putChannel(satellite + "internal/global/satellite/" + channelName, channel);
-        Group group = Group.builder()
-                .name(getGroupName())
-                .callbackUrl(satellite + "internal/global/repl/" + channelName)
-                .channelUrl(channel.getGlobal().getMaster() + "channel/" + channelName)
-                .heartbeat(true)
-                .batch(Group.SECOND)
-                .build();
-        groupService.upsertGroup(group);
+        try {
+            logger.info("starting global replication {}", channel);
+            hubUtils.putChannel(satellite + "internal/global/satellite/" + channelName, channel);
+            String groupName = getGroupName();
+            logger.info("put channel {} {}", channel, groupName);
+            Group group = Group.builder()
+                    .name(groupName)
+                    .callbackUrl(satellite + "internal/global/repl/" + channelName)
+                    .channelUrl(channel.getGlobal().getMaster() + "channel/" + channelName)
+                    .heartbeat(true)
+                    .batch(Group.SECOND)
+                    .build();
+            groupService.upsertGroup(group);
+            logger.info("upserted group {} {}", channel, group);
+        } catch (Exception e) {
+            logger.warn("unable to start " + channelName + " " + getGroupName(), e);
+        }
     }
 
     private String getGroupName() {
@@ -47,7 +59,7 @@ class GlobalReplicator implements Replicator {
 
     String getKey() {
         String domain = StringUtils.removeEnd(StringUtils.substringAfter(satellite, "://"), "/");
-        return StringUtils.replace(domain, ".", "_") + "_" + channel.getName();
+        return StringUtils.replace(StringUtils.replace(domain, ":", "_"), ".", "_") + "_" + channel.getName();
     }
 
 }
