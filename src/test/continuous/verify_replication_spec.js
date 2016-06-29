440941cc8f44ad01b64f1d7d2a0d5a9ef47b3f1c
moulliet <gmoulliet@flightstats.com>
2016-06-29 10:16:25 -0700

    returning to 1% verification

--- src/test/continuous/verify_replication_spec.js
+++ src/test/continuous/verify_replication_spec.js
@@ -151,7 +151,7 @@ describe(testName, function () {
             console.log('working on', channel);
             channels[channel].forEach(function (uri) {
                 var replicatedChannel = replicatedChannels[channel];
-                if (Math.random() > 0.995) {
+                if (Math.random() > 0.99) {
                     var contentKey = getContentKey(uri, channel);
                     itemsToVerify.push({name: channel, uri: uri, contentKey: contentKey});
                 }
