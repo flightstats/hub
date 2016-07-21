8074d356b848ddfb94d9c830c004495180436e62
Ryan Zander <exide@hotmail.com>
2016-07-21 09:25:20 -0700

    Added | to the invalid character list.

--- src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
+++ src/main/java/com/flightstats/hub/filter/DataDogRequestFilter.java
@@ -33,7 +33,7 @@ public class DataDogRequestFilter implements ContainerRequestFilter, ContainerRe
     private static final Logger logger = LoggerFactory.getLogger(DataDogRequestFilter.class);
     private final static StatsDClient statsd = DataDog.statsd;
     private static final ThreadLocal<DataDogState> threadLocal = new ThreadLocal<>();
-    private static final String INVALID_CHARACTERS = "[\\[\\]]";
+    private static final String INVALID_CHARACTERS = "[\\[\\]\\|]";
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) {
