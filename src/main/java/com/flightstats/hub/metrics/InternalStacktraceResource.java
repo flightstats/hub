18b02b08775a2f14ed856c2bfd3f4de8f74cdde9
moulliet <gmoulliet@flightstats.com>
2016-08-26 14:53:49 -0700

    sorting threads

--- src/main/java/com/flightstats/hub/metrics/InternalStacktraceResource.java
+++ src/main/java/com/flightstats/hub/metrics/InternalStacktraceResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.TreeMap;
 
 @SuppressWarnings("WeakerAccess")
 @Path("/internal/stacktrace")
@@ -27,43 +28,66 @@ public class InternalStacktraceResource {
         ObjectNode root = InternalTracesResource.serverAndServers("/internal/stacktrace");
         try {
             Map<Thread, StackTraceElement[]> threadMap = Thread.getAllStackTraces();
+            TreeMap<String, Map<String, ArrayNode>> threadStates = new TreeMap<>();
             for (Thread thread : threadMap.keySet()) {
-                StackTraceElement[] elements = threadMap.get(thread);
-                Thread.State state = thread.getState();
-                ArrayNode stateArray = root.withArray(state.toString());
-                ObjectNode threadNode = stateArray.addObject();
-                ArrayNode stacktrace = threadNode.putArray(thread.getName());
-                if (elements.length > 0) {
-                    String first = elements[0].toString();
-                    if (first.startsWith("sun.nio.ch")) {
-                        stacktrace.add(first);
-                        stacktrace.add("...");
-                    } else if (first.startsWith("sun.misc.Unsafe.park")) {
-                        stacktrace.add(first);
-                        stacktrace.add("...");
-                        for (StackTraceElement element : elements) {
-                            String string = element.toString();
-                            if (string.startsWith("com.flightstats")) {
-                                stacktrace.add(string);
-                            }
-                        }
-                    } else {
-                        for (StackTraceElement element : elements) {
-                            String string = element.toString();
-                            stacktrace.add(string);
-                            if (string.startsWith("sun.reflect.NativeMethodAccessorImpl.invoke")
-                                    || string.startsWith("org.apache.curator.framework")
-                                    || string.startsWith("java.util.concurrent.FutureTask")) {
-                                stacktrace.add("...");
-                                break;
-                            }
-                        }
-                    }
+                String state = thread.getState().toString();
+                if (!threadStates.containsKey(state)) {
+                    threadStates.put(state, new TreeMap<>());
+                }
+                Map<String, ArrayNode> traces = threadStates.get(state);
+                mapThread(thread, threadMap.get(thread), traces);
+            }
+            for (String state : threadStates.keySet()) {
+                Map<String, ArrayNode> arrayNodeMap = threadStates.get(state);
+                ArrayNode stateArray = root.putArray(state);
+                for (String thread : arrayNodeMap.keySet()) {
+                    ObjectNode node = stateArray.addObject();
+                    node.set(thread, arrayNodeMap.get(thread));
                 }
             }
         } catch (Exception e) {
             logger.warn("?", e);
         }
         return Response.ok(root).build();
+    }
+
+    private void mapThread(Thread thread, StackTraceElement[] elements, Map<String, ArrayNode> threadTraces) {
+        ArrayNode stacktrace = mapper.createArrayNode();
+        threadTraces.put(thread.getName(), stacktrace);
+        if (elements.length > 0) {
+            filterElements(elements, stacktrace);
+        }
+    }
+
+    private void filterElements(StackTraceElement[] elements, ArrayNode stacktrace) {
+        String first = elements[0].toString();
+        if (first.startsWith("sun.nio.ch")) {
+            stacktrace.add(first);
+            stacktrace.add("...");
+        } else if (first.startsWith("sun.misc.Unsafe.park")) {
+            stacktrace.add(first);
+            stacktrace.add("...");
+            for (StackTraceElement element : elements) {
+                String string = element.toString();
+                if (string.startsWith("com.flightstats")) {
+                    stacktrace.add(string);
+                }
+            }
+        } else {
+            writeElements(elements, stacktrace);
+        }
+    }
+
+    private void writeElements(StackTraceElement[] elements, ArrayNode stacktrace) {
+        for (StackTraceElement element : elements) {
+            String string = element.toString();
+            stacktrace.add(string);
+            if (string.startsWith("sun.reflect.NativeMethodAccessorImpl.invoke")
+                    || string.startsWith("org.apache.curator.framework")
+                    || string.startsWith("java.util.concurrent.FutureTask")) {
+                stacktrace.add("...");
+                break;
+            }
+        }
     }
 }
