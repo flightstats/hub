39c688f7756eb19dd6abf6398830edb3e2fe39a6
moulliet <gmoulliet@flightstats.com>
2016-09-06 08:31:21 -0700

    adding targetted explicit logging to LastContentPath

--- src/main/java/com/flightstats/hub/cluster/LastContentPath.java
+++ src/main/java/com/flightstats/hub/cluster/LastContentPath.java
@@ -1,5 +1,6 @@
 package com.flightstats.hub.cluster;
 
+import com.flightstats.hub.app.HubProperties;
 import com.flightstats.hub.exception.ConflictException;
 import com.flightstats.hub.model.ContentPath;
 import com.google.common.base.Optional;
@@ -18,6 +19,8 @@ import java.util.function.Function;
 public class LastContentPath {
     private final static Logger logger = LoggerFactory.getLogger(LastContentPath.class);
 
+    private static final String tracing = HubProperties.getProperty("LastContentPathTracing", "locust_verifier_test_2");
+
     private final CuratorFramework curator;
 
     @Inject
@@ -25,8 +28,17 @@ public class LastContentPath {
         this.curator = curator;
     }
 
+    private void trace(String nameOrPath, String text, Object... context) {
+        if (logger.isTraceEnabled()) {
+            if (nameOrPath.contains(tracing)) {
+                logger.trace(text + " nameorPath " + nameOrPath, context);
+            }
+        }
+    }
+
     public void initialize(String name, ContentPath defaultPath, String basePath) {
         try {
+            trace(name, "initialize {} {}", defaultPath, basePath);
             curator.create().creatingParentsIfNeeded().forPath(basePath + name, defaultPath.toBytes());
         } catch (KeeperException.NodeExistsException ignore) {
             //this will typically happen, except the first time
@@ -50,9 +62,12 @@ public class LastContentPath {
     public ContentPath get(String name, ContentPath defaultPath, String basePath) {
         String path = basePath + name;
         try {
-            return get(path);
+            ContentPath contentPath = get(path);
+            trace(name, "get default {} found {}", defaultPath, contentPath);
+            return contentPath;
         } catch (KeeperException.NoNodeException e) {
             if (defaultPath == null) {
+                trace(name, "get default {} null", defaultPath);
                 return null;
             } else {
                 logger.warn("missing value for {} {}", name, basePath);
@@ -67,7 +82,9 @@ public class LastContentPath {
 
     private ContentPath get(String path) throws Exception {
         byte[] bytes = curator.getData().forPath(path);
-        return ContentPath.fromUrl(new String(bytes, Charsets.UTF_8)).get();
+        String found = new String(bytes, Charsets.UTF_8);
+        trace(path, "get found {}", found);
+        return ContentPath.fromUrl(found).get();
     }
 
     public void updateIncrease(ContentPath nextPath, String name, String basePath) {
@@ -78,22 +95,27 @@ public class LastContentPath {
         String path = basePath + name;
         try {
             while (true) {
+                trace(name, "updateIncrease {}", name);
                 LastUpdated existing = getLastUpdated(path);
                 ContentPath nextPath = function.apply(existing.key);
                 if (nextPath.compareTo(existing.key) > 0) {
                     if (setValue(path, nextPath, existing)) {
+                        trace(name, "updateIncrease set {} next {} existing {}", name, nextPath, nextPath);
                         return true;
                     }
                 } else {
+                    trace(name, "updateIncrease less {} next {} existing{}", name, nextPath, nextPath);
                     return true;
                 }
             }
         } catch (KeeperException.NoNodeException e) {
             logger.info("values does not exist, creating {}", path);
+            trace(name, "updateIncrease NoNodeException {}", name);
             ContentPath nextPath = function.apply(null);
             initialize(name, nextPath, basePath);
             return true;
         } catch (ConflictException e) {
+            trace(name, "ConflictException " + e.getMessage());
             throw e;
         } catch (Exception e) {
             logger.warn("unable to set lastUpdated " + path, e);
@@ -106,6 +128,7 @@ public class LastContentPath {
         try {
             LastUpdated existing = getLastUpdated(path);
             setValue(path, nextPath, existing);
+            trace(path, "update {} next {} existing{}", path, nextPath, existing);
         } catch (KeeperException.NoNodeException e) {
             logger.info("values does not exist, creating {}", path);
             initialize(name, nextPath, basePath);
