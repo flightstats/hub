9dcc07b2c1f4118ba485103fc68947a4f888c4c2
Aaron Sharpe <asharpe@hub-01.dls.dev.flightstats.io>
2016-10-13 17:41:42 +0000

    check in changes.

--- docker/attackHubCodeWithGradle.sh
+++ docker/attackHubCodeWithGradle.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-rm -rf docker/singlehub/
-gradle clean compileJava distTar
+# Clean up any old code
+rm -rf docker/hub/
+
+# Attack hub code with gradle. It's semi-effective.
+/usr/bin/gradle clean compileJava distTar
+
+# Thanks for tarring that, gradle, we'll take it from here
 tar -xvf build/distributions/hub-*.tgz -C .
-mv hub-*/ docker/singlehub/
+
+# Put the hub code where the Dockerfile wants it
+mv hub-*/ docker/hub/
