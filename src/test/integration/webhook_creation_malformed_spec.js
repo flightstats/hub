640e2b000de01226fdaff0e0a4a8500fb3c6430a
moulliet <gmoulliet@flightstats.com>
2016-12-08 15:59:14 -0800

    returning 400 for malformed json in webhooks

---
+++ src/test/integration/webhook_creation_malformed_spec.js
@@ -0,0 +1,28 @@
+require('./integration_config.js');
+
+var request = require('request');
+var http = require('http');
+var webhookName = utils.randomChannelName();
+var testName = __filename;
+
+describe("creates malformed" + testName, function () {
+
+    var webhookUrl = hubUrlBase + '/webhook';
+    var webhookResource = webhookUrl + "/" + webhookName;
+
+    it('creates webhook ' + webhookName, function (done) {
+        console.log('creating webhook ' + webhookName + ' for ' + testName);
+        request.put({
+                url: webhookResource,
+                headers: {"Content-Type": "application/json"},
+                body: "{ callbackUrl : 'http://nothing/callback', channelUrl : 'http://nothing/channel/notHere' }"
+            },
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(400);
+                done();
+            });
+    });
+
+});
+
