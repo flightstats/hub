b3c64d0afb72e6817fa35d6c62fce7736a2e31d6
moulliet <gmoulliet@flightstats.com>
2017-01-31 13:16:09 -0800

    cleaning up chunking

--- src/main/java/com/flightstats/hub/util/ChunkStrategy.java
+++ src/main/java/com/flightstats/hub/util/ChunkStrategy.java
@@ -1,22 +1,17 @@
 package com.flightstats.hub.util;
 
+import com.flightstats.hub.app.HubProperties;
+
 class ChunkStrategy {
 
     private static final int MEGABYTES = 1024 * 1024;
 
+    private static int getMaxChunk() {
+        return HubProperties.getProperty("s3.maxChunkMB", 40);
+    }
+
     static int getSize(int count) {
-        int factor = 160;
-        if (count <= 3) {
-            factor = 5;
-        } else if (count <= 6) {
-            factor = 10;
-        } else if (count <= 9) {
-            factor = 20;
-        } else if (count <= 12) {
-            factor = 40;
-        } else if (count <= 15) {
-            factor = 80;
-        }
-        return factor * MEGABYTES;
+        int chunkMB = (Math.floorDiv(count, 3) + 1) * 5;
+        return Math.min(chunkMB, getMaxChunk()) * MEGABYTES;
     }
 }
