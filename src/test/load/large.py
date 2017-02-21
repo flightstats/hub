55d60250589d3ac03b2b3373371f836b215188d3
moulliet <gmoulliet@flightstats.com>
2017-02-21 14:28:46 -0800

    d'oh

--- src/test/load/large.py
+++ src/test/load/large.py
@@ -86,9 +86,9 @@ class LargeTasks(TaskSet):
             print "closing " + large_file_name
             target.close()
         elif tasks.number == 2:
-            os.system("cat large1.out large1.out > large2.out")
+            os.system("cat /mnt/large1.out /mnt/large1.out > /mnt/large2.out")
         elif tasks.number == 3:
-            os.system("cat large2.out large2.out > large3.out")
+            os.system("cat /mnt/large2.out /mnt/large2.out > /mnt/large3.out")
 
     # @task(1)
     # def sequential(self):
