121cabb919a7cd5a80f91b9903602c65b22480dd
moulliet <gmoulliet@flightstats.com>
2017-02-22 10:31:15 -0800

    changing large test to ignore single hub

--- src/test/integration/insert_and_fetch_large_spec.js
+++ src/test/integration/insert_and_fetch_large_spec.js
@@ -14,6 +14,27 @@ var MINUTE = 60 * 1000;
  * 3 - fetch the item and verify bytes
  */
 describe(testName, function () {
+
+    var execute = true;
+
+    it("checks the hub for large item suport", function (done) {
+        request.get({
+                url: hubUrlBase + '/internal/properties'
+            },
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(response.statusCode).toBe(200);
+                var parse = utils.parseJson(response, testName);
+                console.log(response.body);
+
+                var hubType = parse['properties']['hub.type'];
+                console.log(hubType);
+                execute = hubType == 'aws';
+                console.log('execute', execute);
+                done();
+            });
+    }, 5 * MINUTE);
+
     var channelBody = {
         tags: ["test"]
     };
@@ -25,31 +46,39 @@ describe(testName, function () {
     const SIZE = 41 * 1024 * 1024;
 
     it("posts a large item to " + channelResource, function (done) {
-        request.post({
-                url: channelResource,
-                headers: {'Content-Type': "text/plain"},
-                body: Array(SIZE).join("a")
-            },
-            function (err, response, body) {
-                expect(err).toBeNull();
-                expect(response.statusCode).toBe(201);
-                var parse = utils.parseJson(response, testName);
-                console.log(response.body);
-                location = response.headers.location;
-                console.log(location);
-                done();
-            });
+        if (execute) {
+            request.post({
+                    url: channelResource,
+                    headers: {'Content-Type': "text/plain"},
+                    body: Array(SIZE).join("a")
+                },
+                function (err, response, body) {
+                    expect(err).toBeNull();
+                    expect(response.statusCode).toBe(201);
+                    var parse = utils.parseJson(response, testName);
+                    console.log(response.body);
+                    location = response.headers.location;
+                    console.log(location);
+                    done();
+                });
+        } else {
+            done();
+        }
     }, 5 * MINUTE);
 
     it("gets item " + channelResource, function (done) {
-        request.get({url: location},
-            function (err, response, body) {
-                expect(err).toBeNull();
-                expect(response.statusCode).toBe(200);
-                expect(response.headers['content-type']).toBe('text/plain');
-                expect(response.body.length).toBe(SIZE - 1);
-                done();
-            });
+        if (execute) {
+            request.get({url: location},
+                function (err, response, body) {
+                    expect(err).toBeNull();
+                    expect(response.statusCode).toBe(200);
+                    expect(response.headers['content-type']).toBe('text/plain');
+                    expect(response.body.length).toBe(SIZE - 1);
+                    done();
+                });
+        } else {
+            done();
+        }
     }, 5 * MINUTE);
 
 });
