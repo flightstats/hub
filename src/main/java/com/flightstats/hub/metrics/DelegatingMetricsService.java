7a158437af33e939024a0f1eeaecceaa074abb7a
Benjamin Corliss <bcorliss@flightstats.com>
2017-03-03 12:28:11 -0800

    forgot to delegate mute message

--- src/main/java/com/flightstats/hub/metrics/DelegatingMetricsService.java
+++ src/main/java/com/flightstats/hub/metrics/DelegatingMetricsService.java
@@ -43,7 +43,9 @@ public class DelegatingMetricsService implements MetricsService {
     }
 
     @Override
-    public void mute() {};
+    public void mute() {
+        services.forEach((service) -> service.mute());
+    };
 
     @Override
     public void time(String name, long start, String... tags) {
