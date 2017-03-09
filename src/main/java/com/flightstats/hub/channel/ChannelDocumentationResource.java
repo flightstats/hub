bc2eff3948471b5aafd3bac0cc3017ab50301849
Ryan Zander <exide@hotmail.com>
2017-03-08 17:45:25 -0800

    Parse documentation as markdown when queried.

--- src/main/java/com/flightstats/hub/channel/ChannelDocumentationResource.java
+++ src/main/java/com/flightstats/hub/channel/ChannelDocumentationResource.java
@@ -2,6 +2,9 @@ package com.flightstats.hub.channel;
 
 import com.flightstats.hub.app.HubProvider;
 import com.flightstats.hub.dao.DocumentationDao;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +19,8 @@ public class ChannelDocumentationResource {
 
     private final static Logger logger = LoggerFactory.getLogger(ChannelDocumentationResource.class);
     private final static DocumentationDao documentationDao = HubProvider.getInstance(DocumentationDao.class);
+    private final static Parser markdownParser = Parser.builder().build();
+    private final static HtmlRenderer markdownRenderer = HtmlRenderer.builder().build();
 
     @GET
     public Response get(@PathParam("channel") String channel) {
@@ -29,9 +34,9 @@ public class ChannelDocumentationResource {
     }
 
     private String markdown(String raw) {
-        // TODO: parse the raw data and apply mark down rules
         logger.debug("marking down {}", raw);
-        return raw;
+        Node document = markdownParser.parse(raw);
+        return markdownRenderer.render(document);
     }
 
     @PUT
