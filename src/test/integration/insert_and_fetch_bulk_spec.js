02c78df0e2b4f0fd229cd2311faa6e7e5f023014
moulliet <gmoulliet@flightstats.com>
2017-03-17 12:16:10 -0700

    adding more previous validation

--- src/test/integration/insert_and_fetch_bulk_spec.js
+++ src/test/integration/insert_and_fetch_bulk_spec.js
@@ -3,11 +3,11 @@ require('./integration_config.js');
 var request = require('request');
 var http = require('http');
 var channelName = utils.randomChannelName();
-var channelResource = channelUrl + '/' + channelName + '/batch';
+var channelResource = channelUrl + '/' + channelName + '/bulk';
 var testName = __filename;
 
 describe(testName, function () {
-    utils.createChannel(channelName);
+    utils.putChannel(channelName, false, {"ttlDays": 1, "tags": ["bulk"]}, testName);
 
     var multipart =
         'This is a message with multiple parts in MIME format.  This section is ignored.\r\n' +
@@ -19,11 +19,11 @@ describe(testName, function () {
         'Content-Type: application/json\r\n' +
         ' \r\n' +
         '{ "type" : "coffee", "roast" : "french" }\r\n' +
-        '--abcdefg--'
+        '--abcdefg--';
 
     var items = [];
 
-    it("batches items to " + channelResource, function (done) {
+    it("bulk items to " + channelResource, function (done) {
         request.post({
                 url: channelResource,
                 headers: {'Content-Type': "multipart/mixed; boundary=abcdefg"},
@@ -45,8 +45,8 @@ describe(testName, function () {
             function (err, response, body) {
                 expect(err).toBeNull();
                 expect(response.statusCode).toBe(200);
-                expect(response.body).toBe('<coffee><roast>french</roast><coffee>')
-                expect(response.headers['content-type']).toBe('application/xml')
+                expect(response.body).toBe('<coffee><roast>french</roast><coffee>');
+                expect(response.headers['content-type']).toBe('application/xml');
                 done();
             });
     });
@@ -56,11 +56,32 @@ describe(testName, function () {
             function (err, response, body) {
                 expect(err).toBeNull();
                 expect(response.statusCode).toBe(200);
-                expect(response.body).toBe('{ "type" : "coffee", "roast" : "french" }')
-                expect(response.headers['content-type']).toBe('application/json')
+                expect(response.body).toBe('{ "type" : "coffee", "roast" : "french" }');
+                expect(response.headers['content-type']).toBe('application/json');
                 done();
             });
     });
 
+    it("calls previous " + channelResource, function (done) {
+        request.get({url: items[1] + '/previous?trace=true&stable=false', followRedirect: false},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                //console.log('response', response);
+                expect(response.statusCode).toBe(303);
+                expect(response.headers.location).toBe(items[0]);
+                done();
+            });
+    });
+
+    it("calls next " + channelResource, function (done) {
+        request.get({url: items[0] + '/next?trace=true&stable=false', followRedirect: false},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                console.log('body', body);
+                expect(response.statusCode).toBe(303);
+                expect(response.headers.location).toBe(items[1]);
+                done();
+            });
+    });
 });
 
