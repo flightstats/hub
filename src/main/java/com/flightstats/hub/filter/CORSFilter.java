fa699d68daea31e938f844a48f7a4d62561d1413
Benjamin Corliss <bcorliss@flightstats.com>
2017-03-20 12:05:08 -0700

    remove "Allow-Credentials"

--- src/main/java/com/flightstats/hub/filter/CORSFilter.java
+++ src/main/java/com/flightstats/hub/filter/CORSFilter.java
@@ -20,7 +20,7 @@ public class CORSFilter implements ContainerResponseFilter {
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHeaders().add("Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+//        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
