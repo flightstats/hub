1010dd05e6bc6b58b07421cf54bff63e046f13eb
moulliet <gmoulliet@flightstats.com>
2017-05-25 08:40:33 -0700

    working on unknown host wild goose chase

---
+++ src/test/continuous/search_for_unknown_spec.js
@@ -0,0 +1,79 @@
+require('../integration/integration_config.js');
+var agent = require('superagent');
+var request = require('request');
+var async = require('async');
+var moment = require('moment');
+var _ = require('lodash');
+var testName = __filename;
+var hubUrl = process.env.hubUrl;
+hubUrl = 'http://' + hubUrl;
+console.log(hubUrl);
+
+var MINUTE = 60 * 1000;
+
+/**
+ * This should:
+ * Attempt to get known existing items from the hub which are outside the Spoke TTL.
+ * Log any non-2xx responses as failures.
+ *
+ */
+describe("test", function () {
+
+    var channels = ['verifier_test_1'];
+    var urisToVerify = [];
+
+    it('runs day queries ', function (done) {
+        async.eachLimit(channels, 2,
+            function (channel, callback) {
+                console.log('get channel', channel);
+                agent.get(hubUrl + '/channel/' + channel + '/time/day')
+                    .set('Accept', 'application/json')
+                    .end(function (res) {
+                        expect(res.error).toBe(false);
+                        var uris = res.body._links.uris;
+                        console.log('uris', uris);
+                        uris.forEach(function (uri) {
+                            if (Math.random() > 0.9) {
+                                urisToVerify.push(uri);
+                            }
+                        });
+                        callback();
+                    });
+            }, function (err) {
+                done(err);
+            });
+    }, 5 * MINUTE);
+
+    var success = 0;
+
+    it('verifies items', function (done) {
+        console.log('looking at ' + urisToVerify.length + ' items ');
+        async.eachLimit(urisToVerify, 50,
+            function (uri, callback) {
+                request({uri: uri, encoding: null},
+                    function (error, response, body) {
+                        if (error) {
+                            console.log('got error ', uri, error);
+                        } else {
+                            if (response.statusCode === 200) {
+                                console.log('right status ', uri, response.statusCode);
+                                success++
+                            } else {
+                                console.log('wrong status ', uri, response.statusCode);
+                            }
+                        }
+                        callback(null, body);
+                    });
+
+            }, function (err) {
+                done(err);
+            });
+    }, 30 * MINUTE);
+
+
+    it('compares results', function () {
+        console.log('looking at ' + urisToVerify.length + ' items with ' + success + ' success');
+        expect(success).toBe(urisToVerify.length);
+    });
+
+});
