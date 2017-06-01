e6090cf39bf626bb64e0a1b010b0c3e4d1d215be
moulliet <gmoulliet@flightstats.com>
2017-06-01 16:34:59 -0700

    formatting

--- src/main/java/com/flightstats/hub/app/InternalDeployResource.java
+++ src/main/java/com/flightstats/hub/app/InternalDeployResource.java
@@ -3,6 +3,7 @@ package com.flightstats.hub.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.flightstats.hub.cluster.Cluster;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,6 +12,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
 import java.util.Set;
 
 @SuppressWarnings("WeakerAccess")
@@ -23,13 +25,26 @@ public class InternalDeployResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response checkHealth(@Context UriInfo uriInfo) {
+    public Response deploy(@Context UriInfo uriInfo) {
         ArrayNode root = mapper.createArrayNode();
         Set<String> allServers = curatorCluster.getAllServers();
         for (String server : allServers) {
             root.add(server);
         }
         return Response.ok(root).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/text")
+    public Response text(@Context UriInfo uriInfo) {
+        Set<String> allServers = curatorCluster.getAllServers();
+        ArrayList<String> list = new ArrayList<>();
+        for (String allServer : allServers) {
+            list.add(StringUtils.substringBefore(allServer, ":"));
+        }
+        String join = StringUtils.join(list, " ");
+        return Response.ok(join).build();
     }
 
 }
