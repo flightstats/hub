5a217e8d92a106bd517235609a0fe27417ecb761
Aaron S <snarfmonkey@gmail.com>
2017-06-22 11:49:31 -0700

    can we try this?

--- localDocker.sh
+++ localDocker.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
+set -euo pipefail errfail
+
+echo "Cleaning up any old files."
+rm -rf docker/hub
+
+echo "Running gradle clean compileJava distTar"
 gradle clean compileJava distTar
 
-bash docker/attackHubCodeWithGradle.sh
+echo "Untarring the distTar. Thanks, Gradle."
+tar -xvf build/distributions/hub-*.tgz -C docker/
 
+echo "Putting the hub code where the Dockerfile wants it."
+mv docker/hub-*/ docker/hub
+
+echo "Building the docker image."
 docker build docker
 
-echo 'Now run the following command using the image id above'
-echo 'docker run -p 80:80 <image id>'
+NEWESTIMAGE=$(docker images -q | head -1)
+
+echo "Now run the following command using the image id above"
+echo "E.g. docker run -p 80:80 ${NEWESTIMAGE}"
