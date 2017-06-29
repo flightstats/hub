755713ff4f8557d033233e952cc253f32449c9ec
Aaron S <snarfmonkey@gmail.com>
2017-06-29 16:16:54 -0700

    use environment variables instead of a cmd string.

---
+++ localDocker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail errfail
+
+echo "Cleaning up any old files."
+rm -rf docker/hub
+
+echo "Running gradle clean compileJava distTar"
+gradle clean compileJava distTar
+
+echo "Untarring the distTar. Thanks, Gradle."
+tar -xvf build/distributions/hub-*.tgz -C docker/
+
+echo "Putting the hub code where the Dockerfile wants it."
+mv docker/hub-*/ docker/hub
+
+echo "Building the docker image."
+docker build docker
+
+NEWESTIMAGE=$(docker images -q | head -1)
+
+echo "Now run the following command using the image id above:"
+echo "docker run -p 80:80 ${NEWESTIMAGE}"
