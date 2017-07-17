22bf283d681c82cdc9f61cf8c3eba4b30b0b93a5
Ryan Zander <exide@hotmail.com>
2017-07-17 12:58:15 -0700

    Extended the existing websocket test.

--- src/test/integration/websocket_spec.js
+++ src/test/integration/websocket_spec.js
@@ -12,14 +12,15 @@ var MINUTE = 60 * 1000;
 describe(testName, function () {
     utils.createChannel(channelName);
 
-    var wsUrl = channelResource.replace('http', 'ws') + '/ws'
-    console.log('wsUrl ', wsUrl);
+    var webSocketStartingAtLatest;
+    var itemUrl;
 
-    var webSocket;
+    it('starts websocket at the latest item in ' + channelName, function (done) {
+        var wsUrl = channelResource.replace('http', 'ws') + '/ws';
+        console.log('wsUrl', wsUrl);
 
-    it('starts websocket ' + channelName, function (done) {
-        webSocket = new WebSocket(wsUrl);
-        webSocket.on('open', function (message) {
+        webSocketStartingAtLatest = new WebSocket(wsUrl);
+        webSocketStartingAtLatest.on('open', function () {
             console.log('opened wsUrl', wsUrl);
             setTimeout(function () {
                 done()
@@ -31,23 +32,57 @@ describe(testName, function () {
     utils.addItem(channelResource);
 
     it('waits for data ' + channelName, function (done) {
-        webSocket.onmessage = function (message) {
-            console.log('messagedUrl', message.data);
+        webSocketStartingAtLatest.onmessage = function (message) {
+            itemUrl = message.data;
+            console.log('messagedUrl', itemUrl);
             done();
         };
     }, MINUTE + 1);
 
     it('closes websocket ' + channelName, function (done) {
-        webSocket.onclose = function () {
+        webSocketStartingAtLatest.onclose = function () {
             console.log('closed ws');
             done();
         };
 
-        webSocket.close();
+        webSocketStartingAtLatest.close();
+
+    }, MINUTE + 2);
+
+    var webSocketStartingAtItem;
+
+    it('starts websocket at the latest item in ' + channelName, function (done) {
+        var wsUrl = itemUrl.replace('http', 'ws') + '/ws';
+        console.log('wsUrl', wsUrl);
+
+        webSocketStartingAtItem = new WebSocket(wsUrl);
+        webSocketStartingAtItem.on('open', function () {
+            console.log('opened wsUrl', wsUrl);
+            setTimeout(function () {
+                done()
+            }, 2000);
+        });
+
+    }, 15000);
+
+    utils.addItem(channelResource);
+
+    it('waits for data ' + channelName, function (done) {
+        webSocketStartingAtItem.onmessage = function (message) {
+            itemUrl = message.data;
+            console.log('messagedUrl', itemUrl);
+            done();
+        };
+    }, MINUTE + 1);
+
+    it('closes websocket ' + channelName, function (done) {
+        webSocketStartingAtItem.onclose = function () {
+            console.log('closed ws');
+            done();
+        };
+
+        webSocketStartingAtItem.close();
 
     }, MINUTE + 2);
 
 });
-
-
-
