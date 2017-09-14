7b3a73c82036ca87ef76c719e3d5cbeb96f70054
moulliet <gmoulliet@flightstats.com>
2017-09-14 11:58:53 -0700

    changing s3verifier to use curator lock

--- src/main/java/com/flightstats/hub/cluster/Lockable.java
+++ src/main/java/com/flightstats/hub/cluster/Lockable.java
@@ -2,16 +2,6 @@ package com.flightstats.hub.cluster;
 
 public interface Lockable {
 
-    /**
-     * This will be called in the same thread.
-     */
-    default void runWithLock() throws Exception {
-        //todo - gfm - deprecated?
-    }
-
-    default void takeLeadership(Leadership leadership) throws Exception {
-        //todo - gfm -
-        //do nothing for now ...
-    }
+    void takeLeadership(Leadership leadership) throws Exception;
 
 }
