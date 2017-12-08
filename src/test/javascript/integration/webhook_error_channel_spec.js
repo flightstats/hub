281ff846698b3db4d1d41bcb3910691a58c4f377
Ryan Zander <exide@hotmail.com>
2017-12-07 16:51:46 -0800

    Threw together an integration test.

---
+++ src/test/javascript/integration/webhook_error_channel_spec.js
@@ -0,0 +1,116 @@
+require('../integration_config');
+
+const moment = require('moment');
+
+const dataChannelName = utils.randomChannelName();
+const dataChannelURL = `${hubUrlBase}/channel/${dataChannelName}`;
+const errorChannelName = utils.randomChannelName();
+const errorChannelURL = `${hubUrlBase}/channel/${errorChannelName}`;
+const callbackServerPort = utils.getPort();
+const callbackServerURL = `${callbackDomain}:${callbackServerPort}`;
+const webhookName = utils.randomChannelName();
+const webhookURL = `${hubUrlBase}/webhook/${webhookName}`;
+
+describe(__filename, () => {
+
+    let callbackServer;
+    let callbackItems = [];
+    let postedItems = [];
+    let postedTime;
+
+    it('creates a data channel', (done) => {
+        utils.httpPut(dataChannelURL)
+            .then(response => expect(response.statusCode).toEqual(201))
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('creates an error channel', (done) => {
+        utils.httpPut(errorChannelURL)
+            .then(response => expect(response.statusCode).toEqual(201))
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('creates a callback server', (done) => {
+        callbackServer = utils.startHttpServer(callbackServerPort, function (string) {
+            callbackItems.push(string);
+        }, done);
+    });
+
+    it('creates a webhook', (done) => {
+        let headers = {'Content-Type': 'application/json'};
+        let payload = {
+            channelUrl: dataChannelURL,
+            callbackUrl: callbackServerURL,
+            errorChannelUrl: errorChannelURL
+        };
+        utils.httpPut(webhookURL, headers, payload)
+            .then(response => expect(response.statusCode).toEqual(201))
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('posts an item to the data channel', (done) => {
+        let headers = {'Content-Type': 'text/plain'};
+        let payload = moment.utc().toISOString();
+        utils.httpPost(dataChannelURL, headers, payload)
+            .then(response => {
+                expect(response.statusCode).toEqual(201);
+                postedItems.push(response.body._links.self.href);
+            })
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('waits for data', function (done) {
+        utils.waitForData(callbackItems, postedItems, done);
+    });
+
+    it('verifies no error posted to the error channel', (done) => {
+        utils.httpGet(`${errorChannelURL}/latest`)
+            .then(response => expect(response.statusCode).toEqual(404))
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('kills the callback server', (done) => {
+        expect(callbackServer).toBeDefined();
+        utils.closeServer(callbackServer, done);
+    });
+
+    it('posts an item to the data channel', (done) => {
+        let headers = {'Content-Type': 'text/plain'};
+        let payload = moment.utc().toISOString();
+        utils.httpPost(dataChannelURL, headers, payload)
+            .then(response => {
+                expect(response.statusCode).toEqual(201);
+                postedItems.push(response.body._links.self.href);
+            })
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+    it('waits for the webhook to give up', (done) => {
+        // todo: figure out how to do this :)
+    });
+
+    it('verifies an error was posted to the error channel', (done) => {
+        expect(postedTime).toBeDefined();
+        utils.httpGet(`${errorChannelURL}/latest`)
+            .then(response => {
+                expect(response.statusCode).toEqual(200);
+                expect(response.body.itemURL).toEqual(postedItems[1]);
+                expect(response.body.webhookURL).toEqual(webhookURL);
+                // todo: figure out how many retries are expected
+                expect(response.body.retryCount).toEqual(1);
+                expect(moment(response.body.lastAttemptTime)).toBeGreaterThan(postedTime);
+                expect(moment(response.body.lastAttemptTime)).toBeLessThan(moment.utc());
+                // todo: figure out what error we'll get
+                expect(response.body.lastAttemptError).toEqual('blah');
+            })
+            .catch(error => expect(error).toBeNull())
+            .finally(done);
+    });
+
+});
