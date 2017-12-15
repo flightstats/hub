eab429a35c871462e86465c52b3bee2479c5895a
Ryan Zander <exide@hotmail.com>
2017-12-15 15:59:18 -0800

    Revert changes to this unit test.

--- src/test/java/com/flightstats/hub/webhook/WebhookLeaderTest.java
+++ src/test/java/com/flightstats/hub/webhook/WebhookLeaderTest.java
@@ -6,67 +6,41 @@ import com.flightstats.hub.util.TimeUtil;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class WebhookLeaderTest {
 
     @Test()
     public void testWebhookOK() {
         DateTime twentyMinutesAgo = TimeUtil.now().minusMinutes(20);
         ContentKey contentKey = new ContentKey(twentyMinutesAgo);
-        Webhook webhook = Webhook.builder().ttlMinutes(21).build().withDefaults();
+        Webhook webhook = Webhook.builder().ttlMinutes(21).build();
         ChannelConfig channelConfig = ChannelConfig.builder().ttlDays(10).build();
-        WebhookStatus status = mock(WebhookStatus.class);
-        when(status.getErrors()).thenReturn(new ArrayList<>());
-        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook, status);
+        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook);
     }
 
     @Test(expected = ItemExpiredException.class)
     public void testWebhookExpired() {
         DateTime twentyMinutesAgo = TimeUtil.now().minusMinutes(20);
         ContentKey contentKey = new ContentKey(twentyMinutesAgo);
-        Webhook webhook = Webhook.builder().ttlMinutes(19).build().withDefaults();
-        WebhookStatus status = mock(WebhookStatus.class);
-        when(status.getErrors()).thenReturn(new ArrayList<>());
-        WebhookLeader.checkExpiration(contentKey, null, webhook, status);
+        Webhook webhook = Webhook.builder().ttlMinutes(19).build();
+        WebhookLeader.checkExpiration(contentKey, null, webhook);
     }
 
     @Test
     public void testChannelExpiration() {
         DateTime tenDaysAgo = TimeUtil.now().minusDays(10).plusMinutes(1);
         ContentKey contentKey = new ContentKey(tenDaysAgo);
-        Webhook webhook = Webhook.builder().ttlMinutes(0).build().withDefaults();
+        Webhook webhook = Webhook.builder().ttlMinutes(0).build();
         ChannelConfig channelConfig = ChannelConfig.builder().ttlDays(10).build();
-        WebhookStatus status = mock(WebhookStatus.class);
-        when(status.getErrors()).thenReturn(new ArrayList<>());
-        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook, status);
+        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook);
     }
 
     @Test(expected = ItemExpiredException.class)
     public void testChannelExpired() {
         DateTime tenDaysAgo = TimeUtil.now().minusDays(10).minusMinutes(1);
         ContentKey contentKey = new ContentKey(tenDaysAgo);
-        Webhook webhook = Webhook.builder().ttlMinutes(0).build().withDefaults();
+        Webhook webhook = Webhook.builder().ttlMinutes(0).build();
         ChannelConfig channelConfig = ChannelConfig.builder().ttlDays(10).build();
-        WebhookStatus status = mock(WebhookStatus.class);
-        when(status.getErrors()).thenReturn(new ArrayList<>());
-        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook, status);
-    }
-
-    @Test(expected = ItemExpiredException.class)
-    public void testMaxAttemptsReached() {
-        ContentKey contentKey = new ContentKey(TimeUtil.now());
-        Webhook webhook = Webhook.builder().maxAttempts(1).build().withDefaults();
-        ChannelConfig channelConfig = ChannelConfig.builder().build();
-        WebhookStatus status = mock(WebhookStatus.class);
-        List<String> errors = Collections.singletonList(contentKey.toUrl());
-        when(status.getErrors()).thenReturn(errors);
-        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook, status);
+        WebhookLeader.checkExpiration(contentKey, channelConfig, webhook);
     }
 
 }
