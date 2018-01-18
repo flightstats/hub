f2ebc47aa93de9ca7d2ea2fb5ea8195258fd36a1
moulliet <gmoulliet@flightstats.com>
2018-01-18 13:25:45 -0800

    fixing time

--- src/test/javascript/continuous/verify_read_cache_spec.js
+++ src/test/javascript/continuous/verify_read_cache_spec.js
@@ -32,7 +32,7 @@ describe(__filename, () => {
         utils.httpGet(`${hubUrlBase}/internal/properties`)
             .then(response => {
                 expect(response.statusCode).toEqual(200);
-                spokeTTLMinutes = response.body.properties['spoke.write.ttlMinutes'];
+                spokeTTLMinutes = parseInt(response.body.properties['spoke.write.ttlMinutes'], 10);
                 console.log('spokeTTLMinutes:', spokeTTLMinutes);
             })
             .catch(error => expect(error).toBeNull())
@@ -43,14 +43,9 @@ describe(__filename, () => {
 
     it('has data old enough to use', (done) => {
         expect(spokeTTLMinutes).toBeDefined();
-        const utc = moment().utc();
-        console.log('utc:', utc);
-        let timeInThePast = utc.subtract(spokeTTLMinutes + 1, 'minutes');
-        console.log('timeInThePast:', timeInThePast);
-        console.log('spokeTTLMinutes:', spokeTTLMinutes);
+        let timeInThePast = moment().utc().subtract(spokeTTLMinutes + 1, 'minutes');
         let timePath = timeInThePast.format('YYYY/MM/DD/HH/mm');
         let url = `${channelResource}/${timePath}`;
-        console.log('timePath:', timePath);
         utils.httpGet(url)
             .then(response => {
                 if (response.body._links.uris.length === 0) {
