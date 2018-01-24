a1efd638ff7ba39e0e8ba4807b25512b8648a136
Ryan Zander <exide@hotmail.com>
2018-01-24 15:44:49 -0800

    Added an integration test for error channel usage.

--- src/test/javascript/integration/webhook_error_channel_spec.js
+++ src/test/javascript/integration/webhook_error_channel_spec.js
@@ -16,26 +16,24 @@ describe(__filename, () => {
     let callbackServer;
     let callbackItems = [];
     let postedItems = [];
-    let postedTime;
-    let giveUpTime;
 
     it('creates a data channel', (done) => {
         utils.httpPut(dataChannelURL)
             .then(response => expect(response.statusCode).toEqual(201))
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
     it('creates an error channel', (done) => {
         utils.httpPut(errorChannelURL)
             .then(response => expect(response.statusCode).toEqual(201))
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
     it('creates a callback server', (done) => {
-        callbackServer = utils.startHttpServer(callbackServerPort, function (string) {
-            callbackItems.push(string);
+        callbackServer = utils.startHttpServer(callbackServerPort, (request) => {
+            let json = JSON.parse(request);
+            console.log('incoming:', json);
+            json.uris.forEach(uri => callbackItems.push(uri));
         }, done);
     });
 
@@ -46,11 +44,10 @@ describe(__filename, () => {
             callbackUrl: callbackServerURL,
             errorChannelUrl: errorChannelURL,
             callbackTimeoutSeconds: 10,
-            maxAttempts: 1 // todo: replace with the correct property name once #986 is complete
+            maxAttempts: 1
         };
         utils.httpPut(webhookURL, headers, payload)
             .then(response => expect(response.statusCode).toEqual(201))
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
@@ -60,9 +57,9 @@ describe(__filename, () => {
         utils.httpPost(dataChannelURL, headers, payload)
             .then(response => {
                 expect(response.statusCode).toEqual(201);
-                postedItems.push(response.body._links.self.href);
+                let itemURL = response.body._links.self.href;
+                postedItems.push(itemURL);
             })
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
@@ -72,8 +69,8 @@ describe(__filename, () => {
 
     it('verifies no error posted to the error channel', (done) => {
         utils.httpGet(`${errorChannelURL}/latest`)
+            .then(utils.followRedirectIfPresent)
             .then(response => expect(response.statusCode).toEqual(404))
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
@@ -81,6 +78,8 @@ describe(__filename, () => {
         expect(callbackServer).toBeDefined();
         utils.closeServer(callbackServer, done);
     });
+
+    let postedTime;
 
     it('posts an item to the data channel', (done) => {
         let headers = {'Content-Type': 'text/plain'};
@@ -90,34 +89,41 @@ describe(__filename, () => {
                 expect(response.statusCode).toEqual(201);
                 postedItems.push(response.body._links.self.href);
                 postedTime = moment.utc();
+                console.log('postedTime:', postedTime.toISOString());
             })
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
+    let giveUpTime;
+
     it('waits for the webhook to give up', (done) => {
-        let tenSeconds = 10 * 1000;
-        setTimeout(() => {
-            giveUpTime = moment.utc();
-            done();
-        }, tenSeconds);
+        let timeoutMS = 10 * 1000;
+        let clause = (response) => response.body.errors.filter(e => e.includes('max attempts reached')).length > 0;
+        utils.httpGetUntil(webhookURL, clause, timeoutMS)
+            .then(response => {
+                giveUpTime = moment.utc();
+                console.log('giveUpTime:', giveUpTime.toISOString());
+            })
+            .catch(error => done.fail(error))
+            .finally(done);
     });
 
     it('verifies an error was posted to the error channel', (done) => {
         expect(postedTime).toBeDefined();
         expect(giveUpTime).toBeDefined();
-        utils.httpGet(`${errorChannelURL}/latest`)
+        let timeoutMS = 10 * 1000;
+        let clause = (response) => response.statusCode === 303;
+        utils.httpGetUntil(`${errorChannelURL}/latest`, clause, timeoutMS)
+            .then(utils.followRedirectIfPresent)
             .then(response => {
-                expect(response.statusCode).toEqual(200);
-                expect(response.body.itemURL).toEqual(postedItems[1]);
-                expect(response.body.webhookURL).toEqual(webhookURL);
-                expect(response.body.attempts).toEqual(1);
+                console.log(response.body);
+                expect(response.body.webhook).toEqual(webhookURL);
+                expect(response.body.failedItem).toEqual(postedItems[1]);
+                expect(response.body.numberOfAttempts).toEqual(1);
                 expect(moment(response.body.lastAttemptTime)).toBeGreaterThan(postedTime);
                 expect(moment(response.body.lastAttemptTime)).toBeLessThan(giveUpTime);
-                // todo: figure out what error we'll get
-                expect(response.body.lastAttemptError).toEqual('blah');
+                expect(response.body.lastAttemptError).toEqual("max attempts reached (1)");
             })
-            .catch(error => expect(error).toBeNull())
             .finally(done);
     });
 
