bf55b67dfaf9be62e9a4f2a49965f865e45f475e
Ryan Zander <exide@hotmail.com>
2018-05-29 10:08:02 -0700

    Better log output and only INFO+ going to STDOUT.

--- src/test/load/log.py
+++ src/test/load/log.py
@@ -1,18 +1,20 @@
 import logging
 import sys
-
 from lumberjack.handlers import GzippedTimedRotatingFileHandler
 
 
 def setup_logging(filename='unnamed.log'):
+    log_format = '%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s'
+    formatter = logging.Formatter(log_format)
+
+    # remove any root handlers setup by locust.io
     root_logger = logging.getLogger()
+    root_logger.handlers = []
+
+    # setup our own root handlers
     root_logger.setLevel(logging.NOTSET)
-
-    log_format = '%(asctime)s [%(levelname)s] %(name)s : %(message)s'
-    formatter = logging.Formatter(log_format, '%Y-%m-%d %H:%M:%S')
-
-    root_logger.addHandler(create_file_handler(formatter, filename))
     root_logger.addHandler(create_stream_handler(formatter))
+    root_logger.addHandler(create_file_handler(formatter, filename))
 
     # squelch some 3rd party logging
     urllib_logger = logging.getLogger('urllib3.connectionpool')
@@ -27,7 +29,7 @@ def create_file_handler(formatter, filename):
 
 
 def create_stream_handler(formatter):
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.StreamHandler(sys.__stdout__)
     handler.setFormatter(formatter)
     handler.setLevel(logging.INFO)
     return handler
