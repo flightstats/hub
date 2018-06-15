113cc25fc30397fae5b5e4ee795d9e816babf15b
Ryan Zander <exide@hotmail.com>
2018-06-15 10:29:14 -0700

    I was wrong apparently, you do need to use the statsd module.

--- src/test/load/hubTasks.py
+++ src/test/load/hubTasks.py
@@ -9,7 +9,7 @@ import time
 from urlparse import urlparse
 
 import requests
-from datadog import dogstatsd
+from datadog import statsd
 from websocket import WebSocketApp
 from datetime import datetime, timedelta
 from flask import request, jsonify
@@ -254,7 +254,7 @@ class HubTasks:
             logger.info('item before start time: ' + short_href)
         else:
             HubTasks.verify_ordered(self.channel, short_href, websockets, "websocket")
-        dogstatsd.timing('websocket.on_message', datetime.now() - start, tags=['channel:'+self.channel])
+        statsd.timing('hub.locust.websocket.on_message.time', datetime.now() - start, tags=['channel:'+self.channel])
 
     def on_error(self, ws, error):
         logger.error('websocket | ' + self.channel + ' | error: ' + str(error))
