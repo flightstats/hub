f4f67b0b5d3da13fac3a80b5de0a5832c264d3dd
Paul-Hess <pauljacksonhess@gmail.com>
2018-07-12 16:40:46 -0700

    channel put channel protected specs

--- src/test/javascript/integration/channel_put_owner_spec.js
+++ src/test/javascript/integration/channel_put_owner_spec.js
@@ -1,4 +1,5 @@
 require('../integration_config');
+const { getProp } = require('../lib/helpers');
 
 var request = require('request');
 var channelName = utils.randomChannelName();
@@ -6,33 +7,33 @@ var channelResource = channelUrl + "/" + channelName;
 var testName = __filename;
 
 function verifyOptionals(parse) {
-    expect(parse.owner).toBe('the man');
+    expect(getProp('owner', parse)).toBe('the man');
 }
 
 describe(testName, function () {
     console.log('channel url', channelResource);
     it("creates channel " + channelName + " at " + channelUrl, function (done) {
         request.put({
-                url: channelResource,
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    owner: 'the man'
-                })
-            },
-            function (err, response, body) {
-                expect(err).toBeNull();
-                expect(response.statusCode).toBe(201);
-                var parse = utils.parseJson(response, testName);
-                verifyOptionals(parse);
-                done();
-            });
+            url: channelResource,
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                owner: 'the man'
+            })
+        },
+        function (err, response, body) {
+            expect(err).toBeNull();
+            expect(getProp('statusCode', response)).toBe(201);
+            var parse = utils.parseJson(response, testName);
+            verifyOptionals(parse);
+            done();
+        });
     });
 
     it("verifies channel exists " + channelResource, function (done) {
         request.get({url: channelResource + '?cached=false'},
             function (err, response, body) {
                 expect(err).toBeNull();
-                expect(response.statusCode).toBe(200);
+                expect(getProp('statusCode', response)).toBe(200);
                 var parse = utils.parseJson(response, testName);
                 verifyOptionals(parse);
                 done();
@@ -41,29 +42,28 @@ describe(testName, function () {
 
     it("puts channel with null owner " + channelResource, function (done) {
         request.put({
-                url: channelResource,
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({owner: 'stuff'})
-            },
-            function (err, response, body) {
-                expect(err).toBeNull();
-                expect(response.statusCode).toBe(201);
-                var parse = utils.parseJson(response, testName);
-                expect(parse.owner).toBe("stuff");
-                done();
-            });
+            url: channelResource,
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({owner: 'stuff'})
+        },
+        function (err, response, body) {
+            expect(err).toBeNull();
+            expect(getProp('statusCode', response)).toBe(201);
+            var parse = utils.parseJson(response, testName);
+            expect(getProp('owner', parse)).toBe("stuff");
+            done();
+        });
     });
 
     it("verifies channel exists with correct owner " + channelResource, function (done) {
         request.get({url: channelResource + '?cached=false'},
             function (err, response, body) {
                 expect(err).toBeNull();
-                expect(response.statusCode).toBe(200);
+                expect(getProp('statusCode', response)).toBe(200);
                 var parse = utils.parseJson(response, testName);
-                expect(parse.owner).toBe("stuff");
+                expect(getProp('owner', parse)).toBe("stuff");
                 done();
             });
     });
 
 });
-
