1faf143aaeff12e37c22ab7437d8d01bffb57a04
Paul-Hess <pauljacksonhess@gmail.com>
2018-07-25 16:55:21 -0700

    begin post method wrapper

--- src/test/javascript/integration/channel_creation_invalid_tag_spec.js
+++ src/test/javascript/integration/channel_creation_invalid_tag_spec.js
@@ -1,21 +1,13 @@
 require('../integration_config');
-const { getProp } = require('../lib/helpers');
+const { getProp, hubClientPost } = require('../lib/helpers');
 
-
-var channelName = utils.randomChannelName();
+const channelName = utils.randomChannelName();
 
 describe(__filename, function () {
-
-    it('creates a channel with an invalid tag', function (done) {
-        var url = channelUrl;
-        var headers = {'Content-Type': 'application/json'};
-        var body = {'name': channelName, 'tags': ['foo bar', 'bar@home', 'tagz']};
-
-        utils.httpPost(url, headers, body)
-            .then(function (response) {
-                expect(getProp('statusCode', response)).toEqual(400);
-            })
-            .finally(done);
+    it('creates a channel with an invalid tag', async () => {
+        const headers = { 'Content-Type': 'application/json' };
+        const body = { 'name': channelName, 'tags': ['foo bar', 'bar@home', 'tagz'] };
+        const response = await hubClientPost(channelUrl, headers, body);
+        expect(getProp('statusCode', response)).toEqual(400);
     });
-
 });
