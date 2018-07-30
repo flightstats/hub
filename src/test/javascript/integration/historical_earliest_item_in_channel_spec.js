a2731ee45e20b2d67b88930b5123f82ed65a22b7
Paul-Hess <pauljacksonhess@gmail.com>
2018-07-30 16:36:34 -0700

    hist epoch tests

--- src/test/javascript/integration/historical_earliest_item_in_channel_spec.js
+++ src/test/javascript/integration/historical_earliest_item_in_channel_spec.js
@@ -1,11 +1,16 @@
 require('../integration_config');
-const { fromObjectPath } = require('../lib/helpers');
+const { getProp, fromObjectPath, hubClientPut } = require('../lib/helpers');
 
-var channel = utils.randomChannelName();
-var channelResource = channelUrl + "/" + channel;
-var testName = __filename;
-var moment = require('moment');
-
+const channel = utils.randomChannelName();
+const channelResource = `${channelUrl}/${channel}`;
+const headers = { 'Content-Type': 'application/json' };
+const moment = require('moment');
+const mutableTime = moment.utc().subtract(1, 'minute');
+const channelBody = {
+    mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
+    tags: ["test"],
+};
+const items = [];
 /**
  * create a channel
  * post 2 historical items
@@ -13,22 +18,15 @@ var moment = require('moment');
  * post a current item
  * get items back out with earliest/10
  */
-describe(testName, function () {
-
-    var mutableTime = moment.utc().subtract(1, 'minute');
-
-    var channelBody = {
-        mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
-        tags: ["test"]
-    };
-
-    utils.putChannel(channel, false, channelBody, testName);
-
-    var items = [];
+describe(__filename, function () {
+    beforeAll(async () => {
+        const response = await hubClientPut(channelResource, headers, channelBody);
+        expect(getProp('statusCode', response)).toEqual(201);
+    });
 
     it('posts two historical items', function (done) {
-        var historicalItem1 = channelResource + '/' + '2013/11/20/12/00/00/000';
-        var historicalItem2 = channelResource + '/' + '2013/11/20/12/01/00/000';
+        const historicalItem1 = `${channelResource}/2013/11/20/12/00/00/000`;
+        const historicalItem2 = `${channelResource}/2013/11/20/12/01/00/000`;
         utils.postItemQ(historicalItem1)
             .then(function (value) {
                 const location = fromObjectPath(['response', 'headers', 'location'], value);
@@ -43,15 +41,15 @@ describe(testName, function () {
     });
 
     it("gets earliest in default Epoch in channel ", function (done) {
-        utils.getLocation(channelResource + '/earliest?trace=true', 404, false, done);
+        utils.getLocation(`${channelResource}/earliest?trace=true`, 404, false, done);
     });
 
     it("gets earliest Immutable in channel missing ", function (done) {
-        utils.getLocation(channelResource + '/earliest?epoch=IMMUTABLE', 404, false, done);
+        utils.getLocation(`${channelResource}/earliest?epoch=IMMUTABLE`, 404, false, done);
     });
 
     it("gets earliest Mutable in channel", function (done) {
-        utils.getLocation(channelResource + '/earliest?epoch=MUTABLE', 303, items[0], done);
+        utils.getLocation(`${channelResource}/earliest?epoch=MUTABLE`, 303, items[0], done);
     });
 
     it('posts item now', function (done) {
@@ -64,18 +62,18 @@ describe(testName, function () {
     });
 
     it("gets earliest Immutable in channel - after now item", function (done) {
-        utils.getLocation(channelResource + '/earliest?stable=false&trace=true', 303, items[2], done);
+        utils.getLocation(`${channelResource}/earliest?stable=false&trace=true`, 303, items[2], done);
     });
 
     it("gets earliest Mutable in channel - after now item", function (done) {
-        utils.getLocation(channelResource + '/earliest?epoch=MUTABLE', 303, items[0], done);
+        utils.getLocation(`${channelResource}/earliest?epoch=MUTABLE`, 303, items[0], done);
     });
 
     it("gets earliest N Mutable in channel ", function (done) {
-        utils.getQuery(channelResource + '/earliest/10?epoch=MUTABLE', 200, items.slice(0, 2), done);
+        utils.getQuery(`${channelResource}/earliest/10?epoch=MUTABLE`, 200, items.slice(0, 2), done);
     });
 
     it("gets earliest N ALL in channel ", function (done) {
-        utils.getQuery(channelResource + '/earliest/10?stable=false&epoch=ALL', 200, items, done);
+        utils.getQuery(`${channelResource}/earliest/10?stable=false&epoch=ALL`, 200, items, done);
     });
 });
--- src/test/javascript/integration/historical_epoch_time_queries_change_spec.js
+++ src/test/javascript/integration/historical_epoch_time_queries_change_spec.js
@@ -1,16 +1,26 @@
 require('../integration_config');
+const request = require('request');
+const moment = require('moment');
 const {
     fromObjectPath,
     getProp,
+    hubClientPut,
 } = require('../lib/helpers');
 
-var request = require('request');
-var channel = utils.randomChannelName();
-var moment = require('moment');
-
-var tag = Math.random().toString().replace(".", "");
-var testName = __filename;
-
+const channel = utils.randomChannelName();
+const tag = Math.random().toString().replace(".", "");
+const mutableTime = moment.utc().subtract(1, 'minute');
+const channelResource = `${channelUrl}/${channel}`;
+const headers = { 'Content-Type': 'application/json' };
+const pointInThePastURL = channelResource + '/' + mutableTime.format('YYYY/MM/DD/HH/mm/ss/SSS');
+const channelBody = {
+    ttlDays: 0,
+    mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
+    tags: [tag, "test"],
+};
+let historicalLocation = null;
+let liveLocation = null;
+let liveTime = null;
 /**
  * This should:
  * Create a channel
@@ -19,30 +29,55 @@ var testName = __filename;
  * insert an item into now, verify item with get
  * Query items by time, verify exclusion
  */
-describe(testName, function () {
-
-    utils.putChannel(channel, false, {ttlDays: 20}, testName, 201);
-
-    var mutableTime = moment.utc().subtract(1, 'minute');
-
-    var channelBody = {
-        ttlDays: 0,
-        mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
-        tags: [tag, "test"]
+describe(__filename, function () {
+    const timeQuery = (query, expected, done) => {
+        const url = `${channelResource}${query}`;
+        request.get({url: url, json: true},
+            function (err, response, body) {
+                expect(err).toBeNull();
+                expect(getProp('statusCode', response)).toBe(200);
+                const uris = fromObjectPath(['_links', 'uris'], body) || [];
+                expect(uris.length).toBe(expected.length);
+                for (let i = 0; i < uris.length; i++) {
+                    expect(expected[i]).toBeDefined();
+                    expect(uris[i]).toBe(expected[i]);
+                }
+                done();
+            });
     };
 
+    const queryTimes = (format, done) => {
+        const liveQuery = liveTime.format(format);
+        const mutableQuery = mutableTime.format(format);
+
+        let queryAll = done;
+        if (liveQuery === mutableQuery) {
+            queryAll = function () {
+                timeQuery(mutableQuery + '?epoch=ALL&trace=true&stable=false', [historicalLocation, liveLocation], done);
+            };
+        }
+
+        const queryMutable = function () {
+            timeQuery(mutableQuery + '?epoch=MUTABLE&trace=true&stable=false', [historicalLocation], queryAll);
+        };
+        timeQuery(liveQuery + '?epoch=IMMUTABLE&trace=true&stable=false', [liveLocation], queryMutable);
+    };
+
+    beforeAll(async () => {
+        const response = await hubClientPut(channelResource, headers, { ttlDays: 20 });
+        expect(getProp('statusCode', response)).toEqual(201);
+    });
+
     utils.itRefreshesChannels();
 
-    utils.putChannel(channel, false, channelBody, testName);
-
-    var channelURL = hubUrlBase + '/channel/' + channel;
-    var pointInThePastURL = channelURL + '/' + mutableTime.format('YYYY/MM/DD/HH/mm/ss/SSS');
-
-    var historicalLocation;
+    it('updates the channel to mutableTime', async () => {
+        const response = await hubClientPut(channelResource, headers, channelBody);
+        expect(getProp('statusCode', response)).toEqual(201);
+    });
 
     utils.itRefreshesChannels();
 
-    it('posts historical item to ' + channel, function (done) {
+    it(`posts historical item to ${channel}`, function (done) {
         utils.postItemQ(pointInThePastURL)
             .then(function (value) {
                 historicalLocation = fromObjectPath(['response', 'headers', 'location'], value);
@@ -50,7 +85,7 @@ describe(testName, function () {
             });
     });
 
-    it('gets historical item from ' + historicalLocation, function (done) {
+    it(`gets historical item from ${historicalLocation}`, function (done) {
         request.get({url: historicalLocation},
             function (err, response, body) {
                 expect(err).toBeNull();
@@ -59,19 +94,16 @@ describe(testName, function () {
             });
     });
 
-    var liveLocation;
-    var liveTime;
-
-    it('posts live item to ' + channel, function (done) {
-        utils.postItemQ(channelURL)
+    it(`posts live item to ${channel}`, function (done) {
+        utils.postItemQ(channelResource)
             .then(function (value) {
                 liveLocation = fromObjectPath(['response', 'headers', 'location'], value);
-                liveTime = moment((liveLocation || '').substring(channelURL.length), '/YYYY/MM/DD/HH/mm/ss/SSS');
+                liveTime = moment((liveLocation || '').substring(channelResource.length), '/YYYY/MM/DD/HH/mm/ss/SSS');
                 done();
             });
     });
 
-    it('gets live item from ' + liveLocation, function (done) {
+    it(`gets live item from ${liveLocation}`, function (done) {
         request.get({url: liveLocation},
             function (err, response, body) {
                 expect(err).toBeNull();
@@ -79,39 +111,6 @@ describe(testName, function () {
                 done();
             });
     });
-
-    function timeQuery(query, expected, done) {
-        var url = channelURL + query;
-        request.get({url: url, json: true},
-            function (err, response, body) {
-                expect(err).toBeNull();
-                expect(getProp('statusCode', response)).toBe(200);
-                const uris = fromObjectPath(['_links', 'uris'], body) || [];
-                expect(uris.length).toBe(expected.length);
-                for (var i = 0; i < uris.length; i++) {
-                    expect(expected[i]).toBeDefined();
-                    expect(uris[i]).toBe(expected[i]);
-                }
-                done();
-            });
-    }
-
-    function queryTimes(format, done) {
-        var liveQuery = liveTime.format(format);
-        var mutableQuery = mutableTime.format(format);
-
-        var queryAll = done;
-        if (liveQuery === mutableQuery) {
-            queryAll = function () {
-                timeQuery(mutableQuery + '?epoch=ALL&trace=true&stable=false', [historicalLocation, liveLocation], done);
-            };
-        }
-
-        var queryMutable = function () {
-            timeQuery(mutableQuery + '?epoch=MUTABLE&trace=true&stable=false', [historicalLocation], queryAll);
-        };
-        timeQuery(liveQuery + '?epoch=IMMUTABLE&trace=true&stable=false', [liveLocation], queryMutable);
-    };
 
     it('mutable item by day', function (done) {
         queryTimes('/YYYY/MM/DD', done);
@@ -132,5 +131,4 @@ describe(testName, function () {
     it('mutable item by millis', function (done) {
         queryTimes('/YYYY/MM/DD/HH/mm/ss/SSS', done);
     });
-
 });
