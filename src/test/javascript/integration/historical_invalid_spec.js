0fae3b422eb98121a0a426527dfece6084ab9025
Paul-Hess <pauljacksonhess@gmail.com>
2018-07-30 17:14:59 -0700

    more hist specs

--- src/test/javascript/integration/historical_invalid_spec.js
+++ src/test/javascript/integration/historical_invalid_spec.js
@@ -1,23 +1,30 @@
 require('../integration_config');
+const moment = require('moment');
+const { getProp, hubClientPut } = require('../lib/helpers');
 
-var channel = utils.randomChannelName();
-var moment = require('moment');
-
-var testName = __filename;
-var channelBody = {};
+const channel = utils.randomChannelName();
+const channelResource = `${channelUrl}/${channel}`;
+const headers = { 'Content-Type': 'application/json' };
+const mutableTime = moment.utc().subtract(1, 'minute');
 /**
  * This should:
  * Create a normal channel
  * Verify no items can be inserted through the historical interface
  */
-describe(testName, function () {
+describe(__filename, function () {
+    beforeAll(async () => {
+        const response = await hubClientPut(channelResource, headers, {});
+        expect(getProp('statusCode', response)).toEqual(201);
+    });
 
-    utils.putChannel(channel, false, channelBody, testName);
-    var channelUrl = hubUrlBase + '/channel/' + channel + '/';
-    utils.addItem(channelUrl + '2016/06/01/12/00/00/000', 403);
-    utils.addItem(channelUrl + moment.utc().format('YYYY/MM/DD/HH/mm/ss/SSS'), 403);
+    utils.addItem(`${channelResource}/2016/06/01/12/00/00/000`, 403);
 
-    var mutableTime = moment.utc().subtract(1, 'minute');
-    utils.putChannel(channel, false, {mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS')}, testName, 400);
+    utils.addItem(`${channelResource}/${moment.utc().format('YYYY/MM/DD/HH/mm/ss/SSS')}`, 403);
 
+    // ({ "error":"Only one of ttlDays, maxItems and mutableTime can be defined " })
+    it('returns a 400 on mutableTime PUT with bad configuration', async () => {
+        const body = { mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS') };
+        const response = await hubClientPut(channelResource, headers, body);
+        expect(getProp('statusCode', response)).toEqual(400);
+    });
 });
--- src/test/javascript/integration/historical_latest_item_in_channel_spec.js
+++ src/test/javascript/integration/historical_latest_item_in_channel_spec.js
@@ -1,11 +1,18 @@
 require('../integration_config');
-const { fromObjectPath } = require('../lib/helpers');
+const { getProp, fromObjectPath, hubClientPut } = require('../lib/helpers');
 
-var channel = utils.randomChannelName();
-var channelResource = channelUrl + "/" + channel;
-var testName = __filename;
-var moment = require('moment');
-
+const channel = utils.randomChannelName();
+const channelResource = `${channelUrl}/${channel}`;
+const headers = { 'Content-Type': 'application/json' };
+const testName = __filename;
+const moment = require('moment');
+const mutableTime = moment.utc().subtract(1, 'minute');
+const items = [];
+const channelBody = {
+    mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
+    tags: ["test"],
+};
+let latest = null;
 /**
  * create a channel
  * post 2 items
@@ -13,21 +20,14 @@ var moment = require('moment');
  * get both items back out with latest/10
  */
 describe(testName, function () {
-
-    var mutableTime = moment.utc().subtract(1, 'minute');
-
-    var channelBody = {
-        mutableTime: mutableTime.format('YYYY-MM-DDTHH:mm:ss.SSS'),
-        tags: ["test"]
-    };
-
-    utils.putChannel(channel, false, channelBody, testName);
-
-    var items = [];
+    beforeAll(async () => {
+        const response = await hubClientPut(channelResource, headers, channelBody);
+        expect(getProp('statusCode', response)).toEqual(201);
+    });
 
     it('posts two historical items', function (done) {
-        var historicalItem1 = channelResource + moment(mutableTime).subtract(2, 'minute').format('/YYYY/MM/DD/HH/mm/ss/SSS');
-        var historicalItem2 = channelResource + mutableTime.format('/YYYY/MM/DD/HH/mm/ss/SSS');
+        const historicalItem1 = `${channelResource}${moment(mutableTime).subtract(2, 'minute').format('/YYYY/MM/DD/HH/mm/ss/SSS')}`;
+        const historicalItem2 = `${channelResource}${mutableTime.format('/YYYY/MM/DD/HH/mm/ss/SSS')}`;
         utils.postItemQ(historicalItem1)
             .then(function (value) {
                 items.push(fromObjectPath(['response', 'headers', 'location'], value));
@@ -50,8 +50,6 @@ describe(testName, function () {
     it("gets latest Mutable in channel ", function (done) {
         utils.getLocation(channelResource + '/latest?epoch=MUTABLE', 303, items[1], done);
     });
-
-    var latest;
 
     it('posts item now', function (done) {
         utils.postItemQ(channelResource)
