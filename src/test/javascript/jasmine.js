fbf789cbd0d1728568ac9fab4a640bc2f6046c9c
Paul-Hess <pauljacksonhess@gmail.com>
2018-08-23 13:45:19 -0700

    update jasmine, so it does not throw uncaught errors under the hood

--- src/test/javascript/jasmine.js
+++ src/test/javascript/jasmine.js
@@ -1,22 +1,22 @@
 const Jasmine = require('jasmine');
 const reporter = require('./lib/ddt_reporter');
 
-const specs = process.argv.slice(2);
+// const specs = process.argv.slice(2);
 const jasmine = new Jasmine();
 
 global.hubDomain = process.env.hubDomain;
 
 jasmine.loadConfig({
     spec_dir: '.',
-    spec_files: ['**/*_spec.js'],
+    spec_files: [process.env.FILES],
     stopSpecOnExpectationFailure: true,
 });
 
 jasmine.clearReporters();
 jasmine.addReporter(reporter);
-
-if (specs.length > 0) {
-    jasmine.execute(specs);
-} else {
-    jasmine.execute();
-}
+jasmine.execute();
+// if (specs.length > 0) {
+//
+// } else {
+//     jasmine.execute();
+// }
