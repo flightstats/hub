0a7d009662c3f8f6d64bcee84c00ba19d23764a1
Paul-Hess <pauljacksonhess@gmail.com>
2018-08-24 11:51:31 -0700

    clean up the jasmine script changes

--- src/test/javascript/jasmine.js
+++ src/test/javascript/jasmine.js
@@ -1,23 +1,15 @@
 const Jasmine = require('jasmine');
 const reporter = require('./lib/ddt_reporter');
-
-// const specs = process.argv.slice(2);
 const jasmine = new Jasmine();
 
 global.hubDomain = process.env.hubDomain;
-const files = process.env.FILES || 'integration/*.js';
-
+const specs = process.argv[2] || 'integration/*_spec.js';
 jasmine.loadConfig({
     spec_dir: '.',
-    spec_files: [files],
+    spec_files: [specs],
     stopSpecOnExpectationFailure: true,
 });
 
 jasmine.clearReporters();
 jasmine.addReporter(reporter);
 jasmine.execute();
-// if (specs.length > 0) {
-//
-// } else {
-//     jasmine.execute();
-// }
