92a29c4bcc94e9ba84b29e5036b9b41a92107091
Ryan Zander <exide@hotmail.com>
2018-09-10 18:03:39 -0700

    Need to call the shell for piped commands.

--- src/main/java/com/flightstats/hub/spoke/SpokeContentDao.java
+++ src/main/java/com/flightstats/hub/spoke/SpokeContentDao.java
@@ -66,7 +66,7 @@ public class SpokeContentDao {
         String storePath = "/spoke/" + store.name().toLowerCase();
         logger.trace("getting oldest item from " + storePath);
         // expected result format: YYYY-MM-DD+HH:MM:SS.SSSSSSSSSS /spoke/store/channel/yyyy/mm/dd/hh/mm/ssSSShash
-        String result = Commander.run(new String[]{"find", storePath, "-type f", "-printf '%T+ %p'", "|", "sort", "|", "head", "-n", "1"}, 3);
+        String result = Commander.run(new String[]{"/bin/bash", "-c", "find " + storePath + " -type f -printf '%T+ %p' | sort | head -n 1"}, 3);
         String spokePath = StringUtils.substring(result, 31);
         return ChannelContentKey.fromSpokePath(spokePath);
     }
