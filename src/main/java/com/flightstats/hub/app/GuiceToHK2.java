5c2bc19ac85fbab02af51196ca05f82306e5776a
Ryan Zander <exide@hotmail.com>
2018-11-09 13:24:55 -0800

    Handle @Named bindings appropriately.

--- src/main/java/com/flightstats/hub/app/GuiceToHK2.java
+++ src/main/java/com/flightstats/hub/app/GuiceToHK2.java
@@ -1,9 +1,14 @@
 package com.flightstats.hub.app;
 
 import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.name.Named;
 import lombok.extern.slf4j.Slf4j;
 import org.glassfish.hk2.api.Factory;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @Slf4j
 class GuiceToHK2 extends AbstractBinder {
@@ -17,15 +22,40 @@ class GuiceToHK2 extends AbstractBinder {
     @Override
     protected void configure() {
         injector.getBindings().forEach((key, value) -> {
-            String typeName = key.getTypeLiteral().getType().getTypeName();
-            log.debug("mapping guice to hk2: {}", typeName);
-            try {
-                Class boundClass = Class.forName(typeName);
-                bindFactory(new ServiceFactory<>(boundClass)).to(boundClass);
-            } catch (ClassNotFoundException e) {
-                log.warn("unable to find class: {}", typeName);
+            if (isNamedBinding(key)) {
+                bindNamedClass(key);
+            } else {
+                bindClass(key);
             }
         });
+    }
+
+    private boolean isNamedBinding(Key<?> key) {
+        return key.getAnnotationType() != null && key.getAnnotationType().getSimpleName().equals("Named");
+    }
+
+    private void bindClass(Key<?> key) {
+        try {
+            String typeName = key.getTypeLiteral().getType().getTypeName();
+            log.info("mapping guice to hk2: {}", typeName);
+            Class boundClass = Class.forName(typeName);
+            bindFactory(new ServiceFactory<>(boundClass)).to(boundClass);
+        } catch (ClassNotFoundException e) {
+            log.warn("unable to bind {}", key);
+        }
+    }
+
+    private void bindNamedClass(Key<?> key) {
+        try {
+            String typeName = key.getTypeLiteral().getType().getTypeName();
+            Method value = key.getAnnotationType().getDeclaredMethod("value");
+            String name = (String) value.invoke(key.getAnnotation());
+            log.info("mapping guice to hk2: {} (named: {})", typeName, name);
+            Class boundClass = Class.forName(typeName);
+            bindFactory(new ServiceFactory<>(boundClass)).to(boundClass).named(name);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            log.warn("unable to bind {}", key);
+        }
     }
 
     private class ServiceFactory<T> implements Factory<T> {
