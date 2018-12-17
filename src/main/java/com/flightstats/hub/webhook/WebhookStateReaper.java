85e006a019bf5b2d1085f6aafd1bc6dbe8044827
Laurie Kemmerer <lkemmerer@flightstats.com>
2018-12-17 14:58:55 -0800

    Add more tests

--- src/main/java/com/flightstats/hub/webhook/WebhookStateReaper.java
+++ src/main/java/com/flightstats/hub/webhook/WebhookStateReaper.java
@@ -1,13 +1,19 @@
 package com.flightstats.hub.webhook;
 
+import com.flightstats.hub.app.HubServices;
 import com.flightstats.hub.cluster.LastContentPath;
+import com.flightstats.hub.dao.Dao;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.AbstractScheduledService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import static com.flightstats.hub.webhook.WebhookLeader.WEBHOOK_LAST_COMPLETED;
 
+@Singleton
 public class WebhookStateReaper {
     private final static Logger logger = LoggerFactory.getLogger(WebhookStateReaper.class);
 
@@ -16,17 +22,19 @@ public class WebhookStateReaper {
     private final WebhookError webhookError;
 
     @Inject
-    WebhookStateReaper(LastContentPath lastContentPath, WebhookContentPathSet webhookInProcess, WebhookError webhookError) {
+    WebhookStateReaper(LastContentPath lastContentPath,
+                       WebhookContentPathSet webhookInProcess,
+                       WebhookError webhookError) {
         this.lastContentPath = lastContentPath;
         this.webhookInProcess = webhookInProcess;
         this.webhookError = webhookError;
     }
 
-    void delete(String webhookName) {
-        logger.info("deleting " + webhookName);
-        webhookInProcess.delete(webhookName);
-        lastContentPath.delete(webhookName, WEBHOOK_LAST_COMPLETED);
-        webhookError.delete(webhookName);
-        logger.info("deleted " + webhookName);
+    void delete(String webhook) {
+        logger.info("deleting " + webhook);
+        webhookInProcess.delete(webhook);
+        lastContentPath.delete(webhook, WEBHOOK_LAST_COMPLETED);
+        webhookError.delete(webhook);
+        logger.info("deleted " + webhook);
     }
 }
--- src/test/java/com/flightstats/hub/webhook/WebhookStateReaperTest.java
+++ src/test/java/com/flightstats/hub/webhook/WebhookStateReaperTest.java
@@ -2,12 +2,14 @@ package com.flightstats.hub.webhook;
 
 import com.flightstats.hub.cluster.LastContentPath;
 import com.flightstats.hub.dao.ChannelService;
+import com.flightstats.hub.dao.Dao;
 import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.test.Integration;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.KeeperException;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,39 +21,131 @@ import static org.mockito.Mockito.mock;
 
 public class WebhookStateReaperTest {
     private static CuratorFramework curator;
-    private ChannelService channelService = mock(ChannelService.class);
+    private LastContentPath lastContentPath;
+    private WebhookContentPathSet webhookInProcess;
+    private WebhookError webhookError;
+
+    private static final String webhookName = "onTheHook";
+    private static final DateTime start = new DateTime(2014, 12, 3, 20, 45, DateTimeZone.UTC);
+    private static final ContentKey key = new ContentKey(start, "B");
 
     @BeforeClass
     public static void setupCurator() throws Exception {
         curator = Integration.startZooKeeper();
     }
 
+    @Before
+    @SuppressWarnings("unchecked")
+    public void setup() {
+        ChannelService channelService = mock(ChannelService.class);
+
+        lastContentPath = new LastContentPath(curator);
+        webhookError = new WebhookError(curator, channelService);
+        webhookInProcess = new WebhookContentPathSet(curator);
+    }
+
     @Test
     public void testCleansUpZookeeperNodesRelatedToState() throws Exception {
-        LastContentPath lastContentPath = new LastContentPath(curator);
-        WebhookContentPathSet webhookInProcess = new WebhookContentPathSet(curator);
-        WebhookError webhookError = new WebhookError(curator, channelService);
+        // GIVEN
+        addLastCompleted(webhookName);
+        addWebhookInProcess(webhookName);
+        addError(webhookName);
 
-        String webhookName = "onTheHook";
-        DateTime start = new DateTime(2014, 12, 3, 20, 45, DateTimeZone.UTC);
-        ContentKey key1 = new ContentKey(start, "B");
-        lastContentPath.initialize(webhookName, key1, WebhookLeader.WEBHOOK_LAST_COMPLETED);
-
-        webhookInProcess.add(webhookName, key1);
-
-        webhookError.add(webhookName, "oops");
-
-        // make sure the givens did what we asked...
-        assertTrue(curator.getData().forPath(WebhookLeader.WEBHOOK_LAST_COMPLETED + webhookName).length > 0);
-        assertEquals(1, webhookError.get(webhookName).size());
-        assertEquals(1, webhookInProcess.getSet(webhookName, key1).size());
-
+        // WHEN
         WebhookStateReaper reaper = new WebhookStateReaper(lastContentPath, webhookInProcess, webhookError);
         reaper.delete(webhookName);
 
+        // THEN
+        assertLastCompletedDeleted(webhookName);
+        assertErrorDeleted(webhookName);
+        assertWebhookInProcessDeleted(webhookName);
+    }
+
+    @Test
+    public void testCleansUpZookeeperNodesRelatedToState_whenNoWebhookErrors() throws Exception {
+        // GIVEN
+        addLastCompleted(webhookName);
+        addWebhookInProcess(webhookName);
+
+        // WHEN
+        WebhookStateReaper reaper = new WebhookStateReaper(lastContentPath, webhookInProcess, webhookError);
+        reaper.delete(webhookName);
+
+        // THEN
+        assertLastCompletedDeleted(webhookName);
+        assertErrorDeleted(webhookName);
+        assertWebhookInProcessDeleted(webhookName);
+    }
+
+    @Test
+    public void testCleansUpZookeeperNodesRelatedToState_whenNoWebhookInProcess() throws Exception {
+        // GIVEN
+        addLastCompleted(webhookName);
+        addError(webhookName);
+
+        // WHEN
+        WebhookStateReaper reaper = new WebhookStateReaper(lastContentPath, webhookInProcess, webhookError);
+        reaper.delete(webhookName);
+
+        // THEN
+        assertLastCompletedDeleted(webhookName);
+        assertErrorDeleted(webhookName);
+        assertWebhookInProcessDeleted(webhookName);
+    }
+
+    @Test
+    public void testCleansUpZookeeperNodesRelatedToState_whenNoContentWasAdded() throws Exception {
+        // GIVEN
+        addWebhookInProcess(webhookName);
+        addError(webhookName);
+
+        // WHEN
+        WebhookStateReaper reaper = new WebhookStateReaper(lastContentPath, webhookInProcess, webhookError);
+        reaper.delete(webhookName);
+
+        // THEN
+        assertLastCompletedDeleted(webhookName);
+        assertErrorDeleted(webhookName);
+        assertWebhookInProcessDeleted(webhookName);
+    }
+
+    private void addLastCompleted(String webhook) throws Exception {
+        lastContentPath.initialize(webhook, key, WebhookLeader.WEBHOOK_LAST_COMPLETED);
+        assertLastCompletedExists(webhook);
+    }
+
+    private void addWebhookInProcess(String webhook) {
+        webhookInProcess.add(webhook, key);
+        assertWebhookInProcessExists(webhook);
+    }
+
+    private void addError(String webhook) {
+        webhookError.add(webhook, "oops");
+        assertErrorExists(webhook);
+    }
+
+    private void assertLastCompletedExists(String webhook) throws Exception {
+        assertTrue(curator.getData().forPath(WebhookLeader.WEBHOOK_LAST_COMPLETED + webhook).length > 0);
+    }
+
+    private void assertWebhookInProcessExists(String webhook) {
+        assertEquals(1, webhookInProcess.getSet(webhook, key).size());
+    }
+
+    private void assertErrorExists(String webhook) {
+        assertEquals(1, webhookError.get(webhook).size());
+    }
+
+    private void assertLastCompletedDeleted(String webhook) {
         assertThrows(KeeperException.NoNodeException.class,
-                () -> curator.getData().forPath(WebhookLeader.WEBHOOK_LAST_COMPLETED + webhookName));
-        assertTrue(webhookError.get(webhookName).isEmpty());
-        assertTrue(webhookInProcess.getSet(webhookName, key1).isEmpty());
+                () -> curator.getData().forPath(WebhookLeader.WEBHOOK_LAST_COMPLETED + webhook));
+    }
+
+    private void assertErrorDeleted(String webhook) {
+        assertTrue(webhookError.get(webhook).isEmpty());
+    }
+
+    private void assertWebhookInProcessDeleted(String webhook) {
+        assertTrue(webhookInProcess.getSet(webhook, key).isEmpty());
     }
 }
