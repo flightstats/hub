3d109bc1ab7dd79dc4554c56a75c9e0fe61aab66
Laurie Kemmerer <lkemmerer@flightstats.com>
2018-12-18 13:48:28 -0800

    Add some metrics logging around how many webhook leaders we delete

--- src/test/java/com/flightstats/hub/webhook/ActiveWebhookSweeperTest.java
+++ src/test/java/com/flightstats/hub/webhook/ActiveWebhookSweeperTest.java
@@ -1,5 +1,6 @@
 package com.flightstats.hub.webhook;
 
+import com.flightstats.hub.metrics.MetricsService;
 import org.junit.Test;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -17,6 +18,7 @@ public class ActiveWebhookSweeperTest {
     private static final String EMPTY_WEBHOOK = "gotNothing";
 
     private final WebhookLeaderLocks webhookLeaderLocks = mock(WebhookLeaderLocks.class);
+    private final MetricsService metricsService = mock(MetricsService.class);
 
     @Test
     public void testCleanupEmpty_keepsWebhooksWithLeasesAndLocksAndDiscardsOthers() throws Exception {
@@ -33,12 +35,14 @@ public class ActiveWebhookSweeperTest {
         when(webhookLeaderLocks.getLockPaths(WEBHOOK_WITH_LOCK)).thenReturn(newArrayList("lock1b"));
         when(webhookLeaderLocks.getLockPaths(EMPTY_WEBHOOK)).thenReturn(emptyList());
 
-        ActiveWebhookSweeper sweeper = new ActiveWebhookSweeper(webhookLeaderLocks);
+        ActiveWebhookSweeper sweeper = new ActiveWebhookSweeper(webhookLeaderLocks, metricsService);
         sweeper.cleanupEmpty();
 
         verify(webhookLeaderLocks, never()).deleteWebhookLeader(WEBHOOK_WITH_A_FEW_LEASES);
         verify(webhookLeaderLocks, never()).deleteWebhookLeader(WEBHOOK_WITH_LEASE);
         verify(webhookLeaderLocks, never()).deleteWebhookLeader(WEBHOOK_WITH_LOCK);
         verify(webhookLeaderLocks).deleteWebhookLeader(EMPTY_WEBHOOK);
+
+        verify(metricsService).count("webhook.leaders.cleanup", 1);
     }
 }
