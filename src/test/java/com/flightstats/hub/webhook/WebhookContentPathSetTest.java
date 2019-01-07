b7b0a76555b051255248e106f20df96e4eda1c17
Laurie Kemmerer <lkemmerer@flightstats.com>
2019-01-07 15:52:39 -0800

    Use new ZK utilities for WebhookContentPath

--- src/test/java/com/flightstats/hub/webhook/WebhookContentPathSetTest.java
+++ src/test/java/com/flightstats/hub/webhook/WebhookContentPathSetTest.java
@@ -3,6 +3,7 @@ package com.flightstats.hub.webhook;
 import com.flightstats.hub.model.ContentKey;
 import com.flightstats.hub.model.ContentPath;
 import com.flightstats.hub.test.Integration;
+import com.flightstats.hub.util.SafeZooKeeperUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,21 +11,22 @@ import org.junit.Test;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class WebhookContentPathSetTest {
-
-    private CuratorFramework curator;
+    private SafeZooKeeperUtils zooKeeperUtils;
     private WebhookContentPathSet groupSet;
     private String groupName;
 
     @Before
     public void setUp() throws Exception {
-        curator = Integration.startZooKeeper();
+        CuratorFramework curator = Integration.startZooKeeper();
+        zooKeeperUtils = new SafeZooKeeperUtils(curator);
     }
 
     @Test
     public void testLifecycle() throws Exception {
-        groupSet = new WebhookContentPathSet(curator);
+        groupSet = new WebhookContentPathSet(zooKeeperUtils);
         ContentKey first = new ContentKey();
         ContentKey second = new ContentKey();
         ContentKey third = new ContentKey();
@@ -53,7 +55,7 @@ public class WebhookContentPathSetTest {
 
     @Test
     public void testDelete() throws Exception {
-        groupSet = new WebhookContentPathSet(curator);
+        groupSet = new WebhookContentPathSet(zooKeeperUtils);
         groupName = "testDelete";
         ContentKey contentKey = new ContentKey();
         addAndCompare(contentKey, 1);
