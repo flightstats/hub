07ab4a956d98efcdfbe88f69d8ae5718c8fc092b
Paul <pauljacksonhess@gmail.com>
2019-01-23 14:20:23 -0800

    do a little cleanup

--- src/test/javascript/integration/item_length_large_spec.js
+++ src/test/javascript/integration/item_length_large_spec.js
@@ -14,28 +14,27 @@ const {
 } = require('../lib/config');
 
 const channelUrl = getChannelUrl();
-
+const channelName = randomChannelName();
+const channelEndpoint = `${channelUrl}/${channelName}`;
+const itemHeaders = {'Content-Type': 'text/plain'};
+const headers = { 'Content-Type': 'application/json' };
 const testContext = {
     itemSize: 0,
     itemContent: '',
+    itemURL: null,
+    createdChannel: false,
 };
+
 /**
  * POST a large item, GET it, and verify the "X-Item-Length"
  * header is present with the correct value
  */
 
 describe(__filename, function () {
-    const channelName = randomChannelName();
-    const channelEndpoint = channelUrl + '/' + channelName;
-    const itemHeaders = {'Content-Type': 'text/plain'};
-    const headers = { 'Content-Type': 'application/json' };
-    let itemURL;
-    let createdChannel = false;
-
     beforeAll(async () => {
         const channel = await createChannel(channelName, null, 'large inserts');
         if (getProp('statusCode', channel) === 201) {
-            createdChannel = true;
+            testContext.createdChannel = true;
             console.log(`created channel for ${__filename}`);
         }
         const propertiesUrl = `${getHubUrlBase()}/internal/properties`;
@@ -47,22 +46,24 @@ describe(__filename, function () {
     });
 
     it('posts a large item', async () => {
+        const { createdChannel } = testContext;
         if (!createdChannel) return fail('channel not created in before block');
         const { itemContent } = testContext;
         const response = await hubClientPost(channelEndpoint, itemHeaders, itemContent);
         const body = getProp('body', response);
-        itemURL = fromObjectPath(['_links', 'self', 'href'], body);
+        const itemURL = fromObjectPath(['_links', 'self', 'href'], body);
         expect(itemURL).toBeDefined();
+        testContext.itemURL = itemURL;
     });
 
     it('verifies item has correct length info', async () => {
+        const { itemSize, itemURL } = testContext;
         if (!itemURL) {
             expect(itemURL).toBeDefined();
             return false;
         }
         const result = await getHubItem(itemURL);
         console.log('headers', getProp('headers', result));
-        const { itemSize } = testContext;
         const xItemLength = fromObjectPath(['headers', 'x-item-length'], result);
         const bytes = itemSize - 1; // not sure why the -1 is needed. stole this from insert_and_fetch_large_spec.js
         expect(xItemLength).toBe(bytes.toString());
