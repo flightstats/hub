a49172a26da480f352e7b379195bb0e48f614f68
Paul <pauljacksonhess@gmail.com>
2019-01-23 14:11:09 -0800

    patch item_length_large_spec with intelligence around max payload size

--- src/test/javascript/integration/item_length_large_spec.js
+++ src/test/javascript/integration/item_length_large_spec.js
@@ -4,14 +4,21 @@ const {
     getHubItem,
     getProp,
     hubClientDelete,
+    hubClientGet,
     hubClientPost,
     randomChannelName,
 } = require('../lib/helpers');
 const {
+    getHubUrlBase,
     getChannelUrl,
 } = require('../lib/config');
 
 const channelUrl = getChannelUrl();
+
+const testContext = {
+    itemSize: 0,
+    itemContent: '',
+};
 /**
  * POST a large item, GET it, and verify the "X-Item-Length"
  * header is present with the correct value
@@ -21,8 +28,7 @@ describe(__filename, function () {
     const channelName = randomChannelName();
     const channelEndpoint = channelUrl + '/' + channelName;
     const itemHeaders = {'Content-Type': 'text/plain'};
-    const itemSize = 41 * 1024 * 1024;
-    const itemContent = Array(itemSize).join('a');
+    const headers = { 'Content-Type': 'application/json' };
     let itemURL;
     let createdChannel = false;
 
@@ -32,10 +38,17 @@ describe(__filename, function () {
             createdChannel = true;
             console.log(`created channel for ${__filename}`);
         }
+        const propertiesUrl = `${getHubUrlBase()}/internal/properties`;
+        const internals = await hubClientGet(propertiesUrl, headers);
+        const properties = fromObjectPath(['body', 'properties'], internals) || {};
+        const maxPayloadSizeMB = parseInt(properties['app.maxPayloadSizeMB'] || '43');
+        testContext.itemSize = maxPayloadSizeMB - 1;
+        testContext.itemContent = Array(testContext.itemSize).join('a');
     });
 
     it('posts a large item', async () => {
         if (!createdChannel) return fail('channel not created in before block');
+        const { itemContent } = testContext;
         const response = await hubClientPost(channelEndpoint, itemHeaders, itemContent);
         const body = getProp('body', response);
         itemURL = fromObjectPath(['_links', 'self', 'href'], body);
@@ -49,6 +62,7 @@ describe(__filename, function () {
         }
         const result = await getHubItem(itemURL);
         console.log('headers', getProp('headers', result));
+        const { itemSize } = testContext;
         const xItemLength = fromObjectPath(['headers', 'x-item-length'], result);
         const bytes = itemSize - 1; // not sure why the -1 is needed. stole this from insert_and_fetch_large_spec.js
         expect(xItemLength).toBe(bytes.toString());
