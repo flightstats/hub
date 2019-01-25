0916a694a96729bdc6120392cfc7a1d703de4849
Paul <pauljacksonhess@gmail.com>
2019-01-25 14:28:09 -0800

    make the test work with whatever random hub properties is loaded

--- src/test/java/com/flightstats/hub/metrics/MetricsConfigTest.java
+++ src/test/java/com/flightstats/hub/metrics/MetricsConfigTest.java
@@ -1,5 +1,8 @@
 package com.flightstats.hub.metrics;
 
+import com.flightstats.hub.app.HubBindings;
+import com.flightstats.hub.app.HubProperties;
+import com.flightstats.hub.app.HubPropertiesTest;
 import org.junit.Test;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertFalse;
@@ -14,18 +17,23 @@ public class MetricsConfigTest {
                 .builder()
                 .buildWithDefaults()
                 .build();
-        assertEquals("hub", metricsConfig.getRole());
-        assertEquals("development", metricsConfig.getTeam());
-        assertEquals("dev", metricsConfig.getEnv());
+
+        String clusterTag = HubProperties.getProperty("cluster.location", "local") +
+                "-" +
+                HubProperties.getProperty("app.environment", "dev");
+
+        assertEquals(HubProperties.getProperty("metrics.tag.role", "hub"), metricsConfig.getRole());
+        assertEquals(HubProperties.getProperty("metrics.tag.team", "development"), metricsConfig.getTeam());
+        assertEquals(HubProperties.getProperty("app.environment", "dev"), metricsConfig.getEnv());
         assertEquals("local", metricsConfig.getAppVersion());
-        assertEquals("http", metricsConfig.getInfluxdbProtocol());
-        assertEquals("localhost", metricsConfig.getInfluxdbHost());
-        assertEquals(8086, metricsConfig.getInfluxdbPort());
-        assertEquals("", metricsConfig.getInfluxdbPass());
-        assertEquals("", metricsConfig.getInfluxdbUser());
-        assertEquals("hub_tick", metricsConfig.getInfluxdbDatabaseName());
-        assertEquals("local-dev", metricsConfig.getClusterTag());
-        assertEquals(15, metricsConfig.getReportingIntervalSeconds());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.protocol", "http"), metricsConfig.getInfluxdbProtocol());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.host", "localhost"), metricsConfig.getInfluxdbHost());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.port", 8086), metricsConfig.getInfluxdbPort());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.user", ""), metricsConfig.getInfluxdbPass());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.password", ""), metricsConfig.getInfluxdbUser());
+        assertEquals(HubProperties.getProperty("metrics.influxdb.database.name", "hub_tick"), metricsConfig.getInfluxdbDatabaseName());
+        assertEquals(clusterTag, metricsConfig.getClusterTag());
+        assertEquals(HubProperties.getProperty("metrics.seconds", 15), metricsConfig.getReportingIntervalSeconds());
         assertFalse(metricsConfig.isEnabled());
     }
 
