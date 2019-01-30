b7ebcdffca8165e488551b877770e01ac55d74e4
Laurie Kemmerer <lkemmerer@flightstats.com>
2019-01-30 13:11:59 -0800

    Order WebhookErrors by creation time for gets and deletion, because that's what used to happen before the refactor.

--- src/test/java/com/flightstats/hub/webhook/WebhookErrorServiceUnitTest.java
+++ src/test/java/com/flightstats/hub/webhook/WebhookErrorServiceUnitTest.java
@@ -1,9 +1,11 @@
 package com.flightstats.hub.webhook;
 
 import com.flightstats.hub.dao.ChannelService;
+import com.flightstats.hub.util.TimeUtil;
 import com.flightstats.hub.webhook.error.WebhookError;
 import com.flightstats.hub.webhook.error.WebhookErrorPruner;
 import com.flightstats.hub.webhook.error.WebhookErrorRepository;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.util.List;
@@ -20,6 +22,8 @@ public class WebhookErrorServiceUnitTest {
     private final ChannelService channelService = mock(ChannelService.class);
     private final WebhookErrorPruner errorPruner = mock(WebhookErrorPruner.class);
     private final WebhookErrorRepository errorRepo = mock(WebhookErrorRepository.class);
+
+    private final DateTime startingTime = TimeUtil.now();
 
     @Test
     public void testAdd() {
@@ -52,14 +56,55 @@ public class WebhookErrorServiceUnitTest {
         assertEquals(newArrayList("0 message", "2 message", "4 message"), errors);
     }
 
-    private List<WebhookError> setupErrorMocks(String webhookName, int numberOfErrors) {
-        List<WebhookError> webhookErrors = IntStream.range(0, numberOfErrors)
-                .mapToObj(number -> WebhookError.builder()
-                        .name("error" + number)
-                        .data(number + " message")
-                        .build())
-                .collect(toList());
+    @Test
+    public void testGet_withStrangelyOrderedErrors() {
+        String webhookName = "webhookName";
+
+        List<WebhookError> webhookErrors = newArrayList(
+                getWebhook(5),
+                getWebhook(1),
+                getWebhook(4),
+                getWebhook(2),
+                getWebhook(3),
+                getWebhook(0)
+
+        );
+        setupErrorMocks(webhookName, webhookErrors);
+
+        List<WebhookError> orderedWebhookErrors = newArrayList(
+                getWebhook(0),
+                getWebhook(1),
+                getWebhook(2),
+                getWebhook(3),
+                getWebhook(4),
+                getWebhook(5));
+        List<WebhookError> errorsToDelete = newArrayList(orderedWebhookErrors.get(1), orderedWebhookErrors.get(3), orderedWebhookErrors.get(5));
+        when(errorPruner.pruneErrors(webhookName, orderedWebhookErrors)).thenReturn(errorsToDelete);
+
+        WebhookErrorService webhookErrorService = new WebhookErrorService(errorRepo, errorPruner, channelService);
+
+        List<String> errors = webhookErrorService.lookup(webhookName);
+
+        assertEquals(newArrayList("0 message", "2 message", "4 message"), errors);
+    }
+
+    private List<WebhookError> setupErrorMocks(String webhookName, List<WebhookError> webhookErrors) {
         when(errorRepo.getErrors(webhookName)).thenReturn(webhookErrors);
         return webhookErrors;
+    }
+
+    private List<WebhookError> setupErrorMocks(String webhookName, int numberOfErrors) {
+        List<WebhookError> webhookErrors = IntStream.range(0, numberOfErrors)
+                .mapToObj(this::getWebhook)
+                .collect(toList());
+        return setupErrorMocks(webhookName, webhookErrors);
+    }
+
+    private WebhookError getWebhook(int number) {
+        return WebhookError.builder()
+                .name("error" + number)
+                .data(number + " message")
+                .creationTime(startingTime.plusMinutes(number))
+                .build();
     }
 }
--- src/test/java/com/flightstats/hub/webhook/error/WebhookErrorPrunerTest.java
+++ src/test/java/com/flightstats/hub/webhook/error/WebhookErrorPrunerTest.java
@@ -1,6 +1,7 @@
 package com.flightstats.hub.webhook.error;
 
 import com.flightstats.hub.util.TimeUtil;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -19,29 +20,31 @@ import static org.mockito.Mockito.when;
 
 public class WebhookErrorPrunerTest {
     private final WebhookErrorRepository errorRepo = mock(WebhookErrorRepository.class);
+    private final DateTime nowish = TimeUtil.now();
 
     @Test
     public void testAddCleansUpOldestExcessiveErrors() {
         String webhookName = "webhookName";
 
-        List<WebhookError> webhookErrors = setupErrorMocks(webhookName, 12, errorIndex -> Duration.ofMinutes(12 - errorIndex));
+        // insert errors in the list in reverse order newest-to-oldest
+        List<WebhookError> webhookErrors = setupErrorMocks(webhookName, 12, nowish::minusMinutes);
 
         WebhookErrorPruner webhookErrorPruner = new WebhookErrorPruner(errorRepo);
 
         List<WebhookError> prunedErrors = webhookErrorPruner.pruneErrors(webhookName, webhookErrors);
 
-        assertEquals(newArrayList("error0", "error1"), prunedErrors.stream().map(WebhookError::getName).collect(toList()));
+        assertEquals(newArrayList("error11", "error10"), prunedErrors.stream().map(WebhookError::getName).collect(toList()));
 
         verify(errorRepo, times(2)).delete(anyString(), anyString());
-        verify(errorRepo).delete(webhookName, "error0");
-        verify(errorRepo).delete(webhookName, "error1");
+        verify(errorRepo).delete(webhookName, "error10");
+        verify(errorRepo).delete(webhookName, "error11");
     }
 
     @Test
     public void testAddCleansUpErrorsOlderThanADay() {
         String webhookName = "webhookName";
 
-        List<WebhookError> webhookErrors = setupErrorMocks(webhookName, 2, errorIndex -> Duration.ofDays(1 - errorIndex).plusMinutes(1));
+        List<WebhookError> webhookErrors = setupErrorMocks(webhookName, 2, errorIndex -> nowish.minus(Duration.ofDays(1 - errorIndex).plusMinutes(1).toMillis()));
 
         WebhookErrorPruner webhookErrorPruner = new WebhookErrorPruner(errorRepo);
 
@@ -53,12 +56,12 @@ public class WebhookErrorPrunerTest {
         verify(errorRepo).delete(webhookName, "error0");
     }
 
-    private List<WebhookError> setupErrorMocks(String webhookName, int numberOfErrors, Function<Integer, Duration> createdAtOffsetCalculator) {
+    private List<WebhookError> setupErrorMocks(String webhookName, int numberOfErrors, Function<Integer, DateTime> createdAtOffsetCalculator) {
         List<WebhookError> webhookErrors = IntStream.range(0, numberOfErrors)
                 .mapToObj(number -> WebhookError.builder()
                         .name("error" + number)
                         .data(number + " message")
-                        .creationTime(TimeUtil.now().minus(createdAtOffsetCalculator.apply(number).toMillis()))
+                        .creationTime(createdAtOffsetCalculator.apply(number))
                         .build())
                 .collect(toList());
         when(errorRepo.getErrors(webhookName)).thenReturn(webhookErrors);
