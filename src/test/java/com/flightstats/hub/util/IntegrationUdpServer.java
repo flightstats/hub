48b625624741862bf5b6b561558626970bd738bb
Paul <pauljacksonhess@gmail.com>
2019-02-08 13:24:15 -0800

    improve statsd integration server

--- src/test/java/com/flightstats/hub/util/IntegrationUdpServer.java
+++ src/test/java/com/flightstats/hub/util/IntegrationUdpServer.java
@@ -1,57 +1,74 @@
 package com.flightstats.hub.util;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.concurrent.CompletableFuture;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
+@Builder
 public class IntegrationUdpServer {
+    private long timeoutMillis;
     private int port;
-    private boolean listen = false;
-    private String results = "";
-    private Runnable runnable  = () -> {
+
+    @Builder.Default
+    private boolean listening = false;
+
+    private final CompletableFuture<Void> timeoutFuture = CompletableFuture.runAsync(new TimeoutRunnable());
+
+    private final CompletableFuture<String> serverFuture = CompletableFuture.supplyAsync(() -> {
+        String result = "";
         try {
-            start();
-        } catch (Exception ex) {
-            System.out.println("caught exception" + ex);
-        }
-    };
-
-    public void startServer() {
-        new Thread(runnable).start();
-    }
-
-    public IntegrationUdpServer(int port) {
-        this.port = port;
-    }
-
-            public void start() throws IOException {
-                DatagramSocket serverSocket = new DatagramSocket(port);
-                byte[] data = new byte[1024];
-                this.listen = true;
-                while(this.listen) {
-                    DatagramPacket receivePacket = new DatagramPacket(data, data.length);
-                    serverSocket.receive(receivePacket);
-                    String sentence = new String( receivePacket.getData());
-                    this.results = sentence;
-                    System.out.println("RECEIVED: " + sentence);
-                    InetAddress IPAddress = receivePacket.getAddress();
-                    int port = receivePacket.getPort();
-                    String capitalizedSentence = sentence.toUpperCase();
-                    data = capitalizedSentence.getBytes();
-                    DatagramPacket sendPacket =
-                            new DatagramPacket(data, data.length, IPAddress, port);
-                    serverSocket.send(sendPacket);
+            DatagramSocket serverSocket = new DatagramSocket(port);
+            byte[] data = new byte[1024];
+            listening = true;
+            while(listening) {
+                result = openSocket(data, serverSocket);
+                if (!result.equals("")) {
+                    listening = false;
                 }
             }
+        } catch (Exception ex) {
+            listening = false;
+            throw new IllegalStateException(ex);
+        }
+        return result;
+    });
 
-            public String getResults() {
-                return results;
+    private String openSocket(byte[] data, DatagramSocket serverSocket) {
+        DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+        try {
+            serverSocket.receive(receivePacket);
+            return new String(receivePacket.getData()).trim();
+        } catch (IOException ex) {
+            serverFuture.completeExceptionally(ex);
+            return "";
+        }
+    }
+
+    public String getAsyncResult() {
+        try {
+            return serverFuture.get();
+        } catch (Exception ex) {
+            serverFuture.completeExceptionally(ex);
+            return "";
+        }
+    }
+
+    private class TimeoutRunnable implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(timeoutMillis);
+                if (!serverFuture.isDone()) {
+                    serverFuture.completeExceptionally(new TimeoutException("timed out waiting for test task completion"));
+                }
+            } catch (Exception ex) {
+                serverFuture.completeExceptionally(ex);
             }
-
-            public void stop() {
-                this.listen = false;
-            }
-
-
+        }
+    }
 }
