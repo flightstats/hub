8563f61c1099a6d02533c52016a0016bd3603dfa
Paul <pauljacksonhess@gmail.com>
2019-02-20 16:37:39 -0800

    spread imports and add a test

--- src/test/java/com/flightstats/hub/model/ChannelConfigTest.java
+++ src/test/java/com/flightstats/hub/model/ChannelConfigTest.java
@@ -8,7 +8,11 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ChannelConfigTest {
 
