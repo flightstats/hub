3333404b71dc89b12172716995c87219cd736277
Chikkanagappa <manjula.chikkanagappa@flightglobal.com>
2019-03-01 14:50:21 -0800

    PDXDDT-679 Java system test for webhooks

---
+++ src/systemTest/java/com/flightstats/hub/callback/CallbackResource.java
@@ -0,0 +1,43 @@
+package com.flightstats.hub.callback;
+
+import com.flightstats.hub.callback.model.RequestObject;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Slf4j
+@Path("/callback")
+public class CallbackResource {
+
+    private final CacheObject cacheObject = new CacheObject();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @SneakyThrows
+    public Response create(RequestObject requestObject) {
+        this.cacheObject.put(requestObject);
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/{webhookname}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @SneakyThrows
+    public Response get(@PathParam("webhookname") String webhookName) {
+        List<String> value = this.cacheObject.get(webhookName);
+        if (value != null && !value.isEmpty()) {
+            return Response.ok(value.toString()).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
