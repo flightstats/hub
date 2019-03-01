25e44735c20720b78d3f502e746a9844f5aa48ae
Chikkanagappa <manjula.chikkanagappa@flightglobal.com>
2019-03-01 14:50:21 -0800

    PDXDDT-679 Java system test for webhooks

---
+++ src/integrationTest/java/com/flightstats/hub/server/CallbackServlet.java
@@ -0,0 +1,17 @@
+package com.flightstats.hub.server;
+
+import lombok.SneakyThrows;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CallbackServlet extends HttpServlet {
+
+    @SneakyThrows
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{ \"status\": \"ok\"}");
+    }
+}
