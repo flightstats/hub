600e4ce6692496f406aa680324a3fb6a237e09e5
Chikkanagappa <manjula.chikkanagappa@flightglobal.com>
2019-03-01 14:50:21 -0800

    PDXDDT-679 Java system test for webhooks

---
+++ src/systemTest/java/com/flightstats/hub/client/ChannelItemResourceClient.java
@@ -0,0 +1,26 @@
+package com.flightstats.hub.client;
+
+import com.flightstats.hub.model.ChannelItem;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ChannelItemResourceClient {
+
+    @POST("/channel/{channelName}")
+    Call<ChannelItem> add(@Path("channelName") String channelName,
+                          @Body Object item);
+
+    @GET("/channel/{channelName}/{Y}/{M}/{D}/{h}/{m}/{s}/{ms}/{hash}")
+    Call<Object> get(@Path("channelName") String channelName,
+                     @Path("Y") int year,
+                     @Path("M") int month,
+                     @Path("D") int day,
+                     @Path("h") int hour,
+                     @Path("m") int minute,
+                     @Path("s") int second,
+                     @Path("ms") int millis,
+                     @Path("hash") String hash);
+}
