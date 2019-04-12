6495141939b834accf9047a1c008facbe9feb6a5
Paul <pauljacksonhess@gmail.com>
2019-04-12 11:04:49 -0700

    add monitor to metric emitter

--- src/main/java/com/flightstats/hub/metrics/PeriodicMetricEmitter.java
+++ src/main/java/com/flightstats/hub/metrics/PeriodicMetricEmitter.java
@@ -1,16 +1,19 @@
 package com.flightstats.hub.metrics;
 
 import com.flightstats.hub.app.HubProperties;
+import com.flightstats.hub.dao.aws.S3AccessMonitor;
 import com.flightstats.hub.spoke.SpokeStore;
 
 import javax.inject.Inject;
 
 public class PeriodicMetricEmitter {
     private StatsdReporter statsdReporter;
+    private S3AccessMonitor s3AccessMonitor;
 
     @Inject
-    PeriodicMetricEmitter(StatsdReporter statsdReporter) {
+    PeriodicMetricEmitter(StatsdReporter statsdReporter, S3AccessMonitor s3AccessMonitor) {
         this.statsdReporter = statsdReporter;
+        this.s3AccessMonitor = s3AccessMonitor;
     }
 
     void emit() {
@@ -18,5 +21,8 @@ public class PeriodicMetricEmitter {
         statsdReporter.gauge("s3.writeQueue.threads", HubProperties.getS3WriteQueueThreads());
         statsdReporter.gauge("spoke.write.ttl", HubProperties.getSpokeTtlMinutes(SpokeStore.WRITE));
         statsdReporter.gauge("spoke.read.ttl", HubProperties.getSpokeTtlMinutes(SpokeStore.READ));
+        if (!s3AccessMonitor.verifyReadWriteAccess()) {
+            statsdReporter.incrementCounter("s3.access.failed");
+        }
     }
 }
