823bcf5e4dfc81a073d3106ef91e9e4168682a94
Chikkanagappa <manjula.chikkanagappa@flightglobal.com>
2019-05-02 10:39:43 -0700

    cleaning up tests

--- src/test/java/com/flightstats/hub/spoke/SpokeWriteContentDaoTest.java
+++ src/test/java/com/flightstats/hub/spoke/SpokeWriteContentDaoTest.java
@@ -1,18 +1,10 @@
 package com.flightstats.hub.spoke;
 
-import com.flightstats.hub.cluster.Cluster;
-import com.flightstats.hub.cluster.SpokeDecommissionCluster;
-import com.flightstats.hub.config.AppProperties;
 import com.flightstats.hub.config.PropertiesLoader;
-import com.flightstats.hub.config.SpokeProperties;
-import com.flightstats.hub.config.binding.HubBindings;
 import com.flightstats.hub.dao.ContentDaoUtil;
 import com.flightstats.hub.model.Content;
 import com.flightstats.hub.test.Integration;
-import com.flightstats.hub.util.Sleeper;
-import com.google.inject.Injector;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.curator.framework.CuratorFramework;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,26 +15,7 @@ public class SpokeWriteContentDaoTest {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-
-        final Injector injector = Integration.startAwsHub();
-        util = new ContentDaoUtil(injector.getInstance(SpokeWriteContentDao.class));
-        final CuratorFramework curator = injector.getInstance(CuratorFramework.class);
-
-        final SpokeProperties spokeProperties = new SpokeProperties(PropertiesLoader.getInstance());
-        Cluster cluster = HubBindings.buildSpokeCluster(curator,
-                new SpokeDecommissionCluster(curator, spokeProperties),
-                new AppProperties(PropertiesLoader.getInstance()), spokeProperties);
-
-        for (int i = 0; i < 10; i++) {
-            if (cluster.getAllServers().size() == 0) {
-                log.info("no servers yet...");
-                Sleeper.sleep(500);
-            } else {
-                log.info("servers {}", cluster.getAllServers());
-                return;
-            }
-        }
-        log.info("no servers found");
+        util = new ContentDaoUtil(Integration.startAwsHub().getInstance(SpokeWriteContentDao.class));
     }
 
     @Test
