94dcf89793c71ae1430bb48d55d7e466512725ff
klecklerd <klecklerd@cirium.com>
2019-05-15 13:41:17 -0700

    Remove code related to creating and updating dynamo tables.  We will still check that both dynamo tables exist on startup.

--- src/main/java/com/flightstats/hub/dao/aws/DynamoChannelConfigDaoLifecycle.java
+++ src/main/java/com/flightstats/hub/dao/aws/DynamoChannelConfigDaoLifecycle.java
@@ -1,30 +1,18 @@
 package com.flightstats.hub.dao.aws;
 
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
-import com.amazonaws.services.dynamodbv2.model.KeyType;
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
-import com.flightstats.hub.config.AppProperties;
 import com.flightstats.hub.config.DynamoProperties;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 public class DynamoChannelConfigDaoLifecycle extends AbstractIdleService {
     private final DynamoUtils dynamoUtils;
-    private final AppProperties appProperties;
     private final DynamoProperties dynamoProperties;
 
     @Inject
-    public DynamoChannelConfigDaoLifecycle(DynamoUtils dynamoUtils, AppProperties appProperties, DynamoProperties dynamoProperties) {
+    public DynamoChannelConfigDaoLifecycle(DynamoUtils dynamoUtils, DynamoProperties dynamoProperties) {
         this.dynamoUtils = dynamoUtils;
-        this.appProperties = appProperties;
         this.dynamoProperties = dynamoProperties;
     }
 
@@ -39,34 +27,12 @@ public class DynamoChannelConfigDaoLifecycle extends AbstractIdleService {
 
     void initialize() {
         String tableName = dynamoProperties.getChannelConfigTableName();
-        ProvisionedThroughput throughput = dynamoUtils.getProvisionedThroughput("channel");
 
-        if (appProperties.isReadOnly()) {
-            if (!dynamoUtils.doesTableExist(tableName)) {
-                String msg = String.format("Probably fatal error. Dynamo channel config table doesn't exist for r/o node.  %s", tableName);
-                log.error(msg);
-                throw new IllegalArgumentException(msg);
-            }
-        } else {
-            if (!dynamoUtils.doesTableExist(tableName)) {
-                createTable(tableName, throughput);
-            } else {
-                dynamoUtils.updateTable(tableName, throughput);
-            }
+        if (!dynamoUtils.doesTableExist(tableName)) {
+            String msg = String.format("Dynamo channel config table doesn't exist.  %s", tableName);
+            log.error(msg);
+            throw new IllegalArgumentException(msg);
         }
-    }
-
-    private void createTable(String tableName, ProvisionedThroughput throughput) {
-        log.info("creating table {} ", tableName);
-        List<AttributeDefinition> attributes = new ArrayList<>();
-        attributes.add(new AttributeDefinition("key", ScalarAttributeType.S));
-        CreateTableRequest request = new CreateTableRequest()
-                .withTableName(tableName)
-                .withAttributeDefinitions(attributes)
-                .withKeySchema(new KeySchemaElement("key", KeyType.HASH))
-                .withProvisionedThroughput(throughput);
-
-        dynamoUtils.createTable(request);
     }
 
 }
--- src/main/java/com/flightstats/hub/dao/aws/DynamoWebhookDaoLifecycle.java
+++ src/main/java/com/flightstats/hub/dao/aws/DynamoWebhookDaoLifecycle.java
@@ -1,6 +1,5 @@
 package com.flightstats.hub.dao.aws;
 
-import com.flightstats.hub.config.AppProperties;
 import com.flightstats.hub.config.DynamoProperties;
 import com.google.common.util.concurrent.AbstractIdleService;
 import lombok.extern.slf4j.Slf4j;
@@ -10,13 +9,11 @@ import javax.inject.Inject;
 @Slf4j
 public class DynamoWebhookDaoLifecycle extends AbstractIdleService {
     private final DynamoUtils dynamoUtils;
-    private final AppProperties appProperties;
     private final DynamoProperties dynamoProperties;
 
     @Inject
-    public DynamoWebhookDaoLifecycle(DynamoUtils dynamoUtils, AppProperties appProperties, DynamoProperties dynamoProperties) {
+    public DynamoWebhookDaoLifecycle(DynamoUtils dynamoUtils, DynamoProperties dynamoProperties) {
         this.dynamoUtils = dynamoUtils;
-        this.appProperties = appProperties;
         this.dynamoProperties = dynamoProperties;
     }
 
@@ -30,16 +27,12 @@ public class DynamoWebhookDaoLifecycle extends AbstractIdleService {
     }
 
     private void initialize() {
-        if (appProperties.isReadOnly()) {
-            if (!dynamoUtils.doesTableExist(dynamoProperties.getWebhookConfigTableName())) {
-                String msg = String.format(
-                        "Probably fatal error. Dynamo webhook config table doesn't exist for r/o node.  %s",
-                        dynamoProperties.getWebhookConfigTableName());
-                log.error(msg);
-                throw new IllegalArgumentException(msg);
-            }
-        } else {
-            dynamoUtils.createAndUpdate(dynamoProperties.getWebhookConfigTableName(), "webhook", "name");
+        if (!dynamoUtils.doesTableExist(dynamoProperties.getWebhookConfigTableName())) {
+            String msg = String.format(
+                    "Dynamo webhook config table doesn't exist.  %s",
+                    dynamoProperties.getWebhookConfigTableName());
+            log.error(msg);
+            throw new IllegalArgumentException(msg);
         }
     }
 
