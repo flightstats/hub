197b1e7be3ac5442c042843bd35eae890d3a77e0
Paul <pauljacksonhess@gmail.com>
2019-05-15 15:45:17 -0700

    refactor ttl enforcer to ignore input case from either data source

--- src/main/java/com/flightstats/hub/dao/TtlEnforcer.java
+++ src/main/java/com/flightstats/hub/dao/TtlEnforcer.java
@@ -7,9 +7,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class TtlEnforcer {
     private final static Logger logger = LoggerFactory.getLogger(TtlEnforcer.class);
@@ -18,22 +21,24 @@ public class TtlEnforcer {
                                Consumer<ChannelConfig> channelConsumer) {
         try {
             File spokeRoot = new File(path);
-            Set<String> dirSet = new HashSet<>(Arrays.asList(spokeRoot.list()));
-            Iterable<ChannelConfig> channels = channelService.getChannels();
-            Set<String> channelSet = new HashSet<>();
-            for (ChannelConfig channel : channels) {
-                channelSet.add(channel.getLowerCaseName());
-                channelConsumer.accept(channel);
-            }
-            dirSet.removeAll(channelSet);
-            dirSet.remove("lost+found");
-            for (String dir : dirSet) {
-                String dirPath = path + "/" + dir;
-                logger.info("removing dir without channel {}", dirPath);
-                Commander.run(new String[]{"rm", "-rf", dirPath}, 1);
-            }
+            String[] pathArray = spokeRoot.list();
+            Set<String> dirSet = pathArray != null ? new HashSet<>(Arrays.asList(pathArray)) : Collections.emptySet();
+            Collection<ChannelConfig> channels = channelService.getChannels();
+            channels.forEach(channelConsumer);
+            Set<String> channelSet = channels.stream()
+                    .map(ChannelConfig::getLowerCaseName)
+                    .collect(Collectors.toSet());
+            dirSet
+                .stream()
+                .map(String::toLowerCase)
+                .filter(channel -> !channelSet.contains(channel.toLowerCase()) && !channel.equals("lost+found"))
+                .forEach(dir -> {
+                    String dirPath = path + "/" + dir;
+                    logger.info("removing dir without channel {}", dirPath);
+                    Commander.run(new String[]{"rm", "-rf", dirPath}, 1);
+                });
         } catch (Exception e) {
-            logger.warn("unble to run " + path, e);
+            logger.warn("unable to run " + path, e);
         }
     }
 }
