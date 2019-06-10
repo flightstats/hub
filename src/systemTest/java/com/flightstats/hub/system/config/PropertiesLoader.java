fb50ee67fdc49e31c043f0bfae34d5dd99f56d9c
Laurie Kemmerer <lkemmerer@flightstats.com>
2019-06-10 13:25:30 -0700

    Pull project settings from gradle; extract them when figuring out properties in the properties loader

--- src/systemTest/java/com/flightstats/hub/system/config/PropertiesLoader.java
+++ src/systemTest/java/com/flightstats/hub/system/config/PropertiesLoader.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 import java.util.Properties;
 
 import static com.flightstats.hub.util.StringUtils.randomAlphaNumeric;
@@ -12,16 +13,32 @@ import static com.flightstats.hub.util.StringUtils.randomAlphaNumeric;
 class PropertiesLoader {
     Properties loadProperties(String propertiesFileName) {
         Properties properties = new Properties();
+        properties.putAll(loadFileProperties(propertiesFileName));
+        properties.putAll(getSystemTestSystemProperties());
+
+        properties.setProperty(PropertiesName.HELM_RELEASE_NAME, getHelmReleaseName(properties));
+        properties.setProperty(PropertiesName.HELM_RELEASE_DELETE, isHelmReleaseDeletable(properties));
+        properties.setProperty(PropertiesName.HELM_CLUSTERED_HUB, isHelmHubClustered(properties));
+
+        return properties;
+    }
+
+    private Properties loadFileProperties(String propertiesFileName) {
+        Properties properties = new Properties();
         try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(propertiesFileName)) {
             properties.load(inputStream);
-
-            properties.setProperty(PropertiesName.HELM_RELEASE_NAME, getHelmReleaseName(properties));
-            properties.setProperty(PropertiesName.HELM_RELEASE_DELETE, isHelmReleaseDeletable(properties));
-            properties.setProperty(PropertiesName.HELM_CLUSTERED_HUB, isHelmHubClustered(properties));
-
         } catch (IOException e) {
             log.error("Property file {} not found", propertiesFileName, e);
         }
+        return properties;
+    }
+
+    private Properties getSystemTestSystemProperties() {
+        Properties properties = new Properties();
+        System.getProperties().forEach((key, value) ->
+                properties.put(
+                        key.toString().replace("systemTest.", ""),
+                        value));
         return properties;
     }
 
