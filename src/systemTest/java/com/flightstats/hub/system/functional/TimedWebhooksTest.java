1a36563b31f45a4d5bd3fd49dab876b1acc75f65
Paul <pauljacksonhess@gmail.com>
2019-06-13 10:10:55 -0700

    merge remote

--- src/systemTest/java/com/flightstats/hub/system/functional/TimedWebhooksTest.java
+++ src/systemTest/java/com/flightstats/hub/system/functional/TimedWebhooksTest.java
@@ -6,7 +6,6 @@ import com.flightstats.hub.system.config.DependencyInjector;
 import com.flightstats.hub.system.service.CallbackService;
 import com.flightstats.hub.system.service.ChannelService;
 import com.flightstats.hub.system.service.WebhookService;
-import com.flightstats.hub.utility.StringHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
 import org.awaitility.Duration;
@@ -15,7 +14,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,19 +21,17 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import static com.flightstats.hub.util.StringUtils.randomAlphaNumeric;
+
 @Slf4j
 class TimedWebhooksTest extends DependencyInjector {
-    @Inject
-    @Named("test.data")
-    private String testData;
+    private static final String TEST_DATA = "TEST_DATA";
     private static final int CHANNEL_COUNT = 5;
     private static final int TIMEOUT = 5 * 60;
 
     private final List<String> channels = new ArrayList<>();
     private final List<Webhook> webhooks = new ArrayList<>();
     private final ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> channelItemsPosted = new ConcurrentHashMap<>();
-    @Inject
-    private StringHelper stringHelper;
     @Inject
     private ChannelService channelService;
     @Inject
@@ -55,9 +51,9 @@ class TimedWebhooksTest extends DependencyInjector {
     private boolean channelAndWebhookFactory(WebhookType type) {
         try {
             for (int i = 0; i < CHANNEL_COUNT; i++) {
-                String channelName = stringHelper.randomAlphaNumeric(10);
-                String webhookName = stringHelper.randomAlphaNumeric(10);
-                channelService.create(channelName);
+                String channelName = randomAlphaNumeric(10);
+                String webhookName = randomAlphaNumeric(10);
+                channelService.createWithDefaults(channelName);
                 Webhook webhook = Webhook.builder()
                         .channelUrl(channelService.getChannelUrl(channelName))
                         .name(webhookName)
@@ -83,7 +79,7 @@ class TimedWebhooksTest extends DependencyInjector {
         try {
             for (int i = 0; i <= 3; i++) {
                 channels.parallelStream().forEach(channelName -> {
-                    List<String> nextItems = channelService.addItems(channelName, testData, CHANNEL_COUNT / 4);
+                    List<String> nextItems = channelService.addItems(channelName, TEST_DATA, CHANNEL_COUNT / 4);
                     ConcurrentLinkedQueue<String> items = new ConcurrentLinkedQueue<>();
                     channelItemsPosted.putIfAbsent(channelName, items);
                     nextItems.forEach(item -> channelItemsPosted.get(channelName).add(item));
@@ -106,9 +102,9 @@ class TimedWebhooksTest extends DependencyInjector {
         }
     }
 
-    private int getItemsPostedCount(String channelName) {
+    private List<String> getItemsPostedForChannel(String channelName) {
         try {
-            return channelItemsPosted.get(channelName).size();
+            return new ArrayList<>(channelItemsPosted.get(channelName));
         } catch (Exception e) {
             log.error("failed to get count of items posted for channel: {}", channelName);
             throw e;
@@ -124,8 +120,8 @@ class TimedWebhooksTest extends DependencyInjector {
         webhooks.parallelStream().forEach(webhook -> {
             String webhookName = webhook.getName();
             String channelName = getChannelName(webhook);
-            int itemsPostedCount = getItemsPostedCount(channelName);
-            callbackService.awaitItemCountSentToWebhook(webhookName, itemsPostedCount);
+            List<String> itemsPosted = getItemsPostedForChannel(channelName);
+            callbackService.areItemsEventuallySentToWebhook(webhookName, itemsPosted);
         });
     }
 }
