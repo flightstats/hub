851edd1d3dfce69a1aeefbbfa976ef9950f98fa2
Laurie Kemmerer <lkemmerer@flightstats.com>
2019-06-20 16:09:08 -0700

    Refactor FormattedStringHelper a little to separate concerns

--- src/systemTest/java/com/flightstats/hub/system/service/ChannelItemRetriever.java
+++ src/systemTest/java/com/flightstats/hub/system/service/ChannelItemRetriever.java
@@ -3,6 +3,7 @@ package com.flightstats.hub.system.service;
 import com.flightstats.hub.clients.hub.HubClientFactory;
 import com.flightstats.hub.clients.hub.channel.ChannelItemResourceClient;
 import com.flightstats.hub.model.ChannelItem;
+import com.flightstats.hub.model.ChannelItemPathParts;
 import com.flightstats.hub.model.Links;
 import com.flightstats.hub.model.Location;
 import com.flightstats.hub.model.TimeQueryResult;
@@ -16,29 +17,19 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-
-import static javax.ws.rs.core.Response.Status.CREATED;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 public class ChannelItemRetriever {
     private final ChannelItemResourceClient channelItemResourceClient;
+    private final ChannelItemPathPartsBuilder pathPartsBuilder;
     private final HttpUrl hubBaseUrl;
 
     @Inject
     public ChannelItemRetriever(HubClientFactory hubClientFactory) {
         this.channelItemResourceClient = hubClientFactory.getHubClient(ChannelItemResourceClient.class);
         this.hubBaseUrl = hubClientFactory.getHubBaseUrl();
+        this.pathPartsBuilder = new ChannelItemPathPartsBuilder(this.hubBaseUrl.toString());
     }
 
     public String getChannelUrl(String channelName) {
@@ -48,43 +39,37 @@ public class ChannelItemRetriever {
     private HttpUrl getHubBaseUrl() {
         return hubBaseUrl;
     }
+
+    @SneakyThrows
+    public Optional<Object> getItem(String path) {
+        ChannelItemPathParts pathParts = pathPartsBuilder.buildFromItemUrl(path);
+        Object response = channelItemResourceClient.get(
+                pathParts.getChannelName(),
+                pathParts.getYear(),
+                pathParts.getMonth(),
+                pathParts.getDay(),
+                pathParts.getHour(),
+                pathParts.getMinute(),
+                pathParts.getSecond(),
+                pathParts.getMillis(),
+                pathParts.getHashKey());
+        return Optional.ofNullable(((Call) response).execute().body());
+    }
+
     @SneakyThrows
     private Optional<TimeQueryResult> getItemByTimeFromLocation(String path, Location location) {
-        FormattedStringHelper helper = new FormattedStringHelper().withExtractedUrlPaths(path, hubBaseUrl.toString());
+        ChannelItemPathParts pathParts = pathPartsBuilder.buildFromItemUrl(path);
         Call<TimeQueryResult> response = channelItemResourceClient.getItemsSecondsPath(
-                helper.getChannelName(),
-                helper.getYear(),
-                helper.getMonth(),
-                helper.getDay(),
-                helper.getHour(),
-                helper.getMinute(),
-                helper.getSecond(),
+                pathParts.getChannelName(),
+                pathParts.getYear(),
+                pathParts.getMonth(),
+                pathParts.getDay(),
+                pathParts.getHour(),
+                pathParts.getMinute(),
+                pathParts.getSecond(),
                 location);
         Optional<TimeQueryResult> op = Optional.ofNullable(response.execute().body());
         op.filter(o -> o.get_links() != null);
         return op;
-    }
-
-    @SneakyThrows
-    public Optional<Object> getItem(String path) {
-        FormattedStringHelper helper = new FormattedStringHelper().withExtractedUrlPaths(path, hubBaseUrl.toString());
-        Object response = channelItemResourceClient.get(
-                helper.getChannelName(),
-                helper.getYear(),
-                helper.getMonth(),
-                helper.getDay(),
-                helper.getHour(),
-                helper.getMinute(),
-                helper.getSecond(),
-                helper.getMillis(),
-                helper.getHashKey());
-        return Optional.ofNullable(((Call) response).execute().body());
-    }
-
-    public boolean confirmItemInCache(String itemUri, Location location) {
-        TimeQueryResult result = getItemByTimeFromLocation(itemUri, location)
-                .orElse(TimeQueryResult.builder()._links(Links.builder().uris(new String[]{}).build()).build());
-        List<String> uris = Arrays.asList(result.get_links().getUris());
-        return uris.stream().anyMatch(str -> str.equals(itemUri));
     }
 }
