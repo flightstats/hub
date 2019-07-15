548431c21feb6c2e92e9ea3c15b5738875a8d34a
Laurie Kemmerer <lkemmerer@flightstats.com>
2019-07-15 12:43:03 -0700

    Write a test for items outside the spoke ttl. Still need to modify to use a hub version with a very short spoke ttl.

--- src/systemTest/java/com/flightstats/hub/system/functional/ChannelLatestUpdatedStateTest.java
+++ src/systemTest/java/com/flightstats/hub/system/functional/ChannelLatestUpdatedStateTest.java
@@ -1,24 +1,34 @@
 package com.flightstats.hub.system.functional;
 
-import com.flightstats.hub.system.ModelBuilder;
+import com.flightstats.hub.model.ChannelConfig;
+import com.flightstats.hub.model.ChannelItemPathParts;
+import com.flightstats.hub.model.ChannelItemWithBody;
 import com.flightstats.hub.system.extension.TestClassWrapper;
 import com.flightstats.hub.system.service.ChannelConfigService;
 import com.flightstats.hub.system.service.ChannelItemCreator;
 import com.flightstats.hub.system.service.ChannelItemRetriever;
 import com.flightstats.hub.system.service.InternalZookeeperService;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.SortedSet;
+import java.util.concurrent.TimeUnit;
 
 import static com.flightstats.hub.util.StringUtils.randomAlphaNumeric;
+import static java.util.stream.Collectors.toList;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.fail;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 class ChannelLatestUpdatedStateTest extends TestClassWrapper {
     private String channelName;
@@ -31,11 +41,9 @@ class ChannelLatestUpdatedStateTest extends TestClassWrapper {
     private ChannelItemCreator channelItemCreator;
     @Inject
     private InternalZookeeperService zookeeperService;
-    @Inject
-    private ModelBuilder modelBuilder;
 
     @BeforeEach
-    private void setup() {
+    void setup() {
         channelName = randomAlphaNumeric(10);
         channelConfigService.createWithDefaults(channelName);
     }
@@ -57,34 +65,46 @@ class ChannelLatestUpdatedStateTest extends TestClassWrapper {
 
     @Test
     void channelWithItemsInSpoke_hasZkLatestItemsEqualToLastInsertedItemAfterQueryingLatestEndpoint() {
-        List<String> data = channelItemCreator.addItems(channelName, "data", 3);
-        String expectedLastItemUrl = data.stream().min(Comparator.reverseOrder())
+        List<ChannelItemWithBody> channelItems = channelItemCreator.addItems(channelName, "data", 3);
+        ChannelItemWithBody expectedLastItem = channelItems.stream().min((other, item) -> item.getDateTime().compareTo(other.getDateTime()))
                 .orElseThrow(AssertionError::new);
 
         String latestItemUrl = channelItemRetriever.getLatestUnstableItemUrl(channelName)
                 .orElseThrow(AssertionError::new);
-        assertEquals(expectedLastItemUrl, latestItemUrl);
+        assertEquals(expectedLastItem.getItemUrl(), latestItemUrl);
 
-        String channelLatestUpdated = zookeeperService.getChannelLatestUpdated(channelName)
-                .orElseThrow(AssertionError::new);
-        assertEquals(expectedLastItemUrl, channelLatestUpdated);
+        await().atMost(90, TimeUnit.SECONDS)
+                .until(() -> zookeeperService.getChannelLatestUpdated(channelName)
+                                .orElseThrow(AssertionError::new),
+                        is(equalTo(expectedLastItem.getZookeeperItemPath())));
     }
 
     @Test
     void channelWithItemsOutsideSpokeWindow_hasStateEqualToChannelLatest() {
-        // create channel with some items outside spoke ttl
-        // verify channel/latest == last item key
-        // verify ZK ChannelLatestUpdated == last item
+        DateTime now = DateTime.now();
+        String historicalChannelName = channelName + "-historical";
+        ChannelConfig config = ChannelConfig.builder()
+                .name(historicalChannelName)
+                .mutableTime(now)
+                .build();
+        channelConfigService.create(config);
+
+        SortedSet<ChannelItemWithBody> channelItems = channelItemCreator.addHistoricalItems(historicalChannelName,
+                Arrays.asList(now.minusMinutes(5), now.minusMinutes(4)));
+        ChannelItemWithBody mostRecentItem = channelItems.last();
+
+        ChannelConfig updatedConfig = config.toBuilder()
+                .mutableTime(now.minusDays(1))
+                .build();
+        channelConfigService.update(updatedConfig);
+
+        String latestItemUrl = channelItemRetriever.getLatestUnstableItemUrl(historicalChannelName)
+                .orElseThrow(AssertionError::new);
+        assertEquals(mostRecentItem.getItemUrl(), latestItemUrl);
+
+        await().atMost(90, TimeUnit.SECONDS)
+                .until(() -> zookeeperService.getChannelLatestUpdated(historicalChannelName)
+                                .orElseThrow(AssertionError::new),
+                        is(equalTo(mostRecentItem.getZookeeperItemPath())));
     }
-
-/*
-set ttl to 2 minutes if necessary
-
-make N channels
-
-add items to N1 channels
-
-do not add items to N2
-delete N3 items from N4 of N channels
-*/
 }
