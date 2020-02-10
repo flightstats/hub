5be6592ebfb1824b5cf81abc6c757bdf0d33a3c0
Laurie Kemmerer <lkemmerer@flightstats.com>
2020-02-10 15:03:46 -0800

    add a couple more tests...

--- src/test/java/com/flightstats/hub/webhook/strategy/TimedWebhookStrategyTest.java
+++ src/test/java/com/flightstats/hub/webhook/strategy/TimedWebhookStrategyTest.java
@@ -1,14 +1,28 @@
 package com.flightstats.hub.webhook.strategy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flightstats.hub.cluster.ClusterCacheDao;
+import com.flightstats.hub.dao.aws.ContentRetriever;
 import com.flightstats.hub.model.ContentKey;
+import com.flightstats.hub.model.ContentPath;
+import com.flightstats.hub.model.MinutePath;
 import com.flightstats.hub.model.SecondPath;
+import com.flightstats.hub.webhook.Webhook;
+import org.hamcrest.Matchers;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.BiConsumer;
 
+import java.util.Optional;
+
+import static com.flightstats.hub.constant.ZookeeperNodes.WEBHOOK_LAST_COMPLETED;
+import static com.flightstats.hub.model.WebhookType.MINUTE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TimedWebhookStrategyTest {
     @Test
@@ -39,7 +53,6 @@ class TimedWebhookStrategyTest {
         compareContentKey.accept(new DateTime(2016, 4, 21, 17, 22, 59, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
     }
 
-
     @Test
     void testRoundingMinutePath() {
         BiConsumer<DateTime, String> compareContentPath = (start, expected) -> {
@@ -52,5 +65,51 @@ class TimedWebhookStrategyTest {
         compareContentPath.accept(new DateTime(2016, 4, 21, 17, 1, 58, 0, DateTimeZone.UTC), "2016-04-21T17:00:00.000Z");
         compareContentPath.accept(new DateTime(2016, 4, 21, 17, 58, 1, 0, DateTimeZone.UTC), "2016-04-21T17:57:00.000Z");
         compareContentPath.accept(new DateTime(2016, 4, 21, 17, 59, 59, 0, DateTimeZone.UTC), "2016-04-21T17:58:00.000Z");
+    }
+
+    @Test
+    void testStartingPath_minuteBasedWebhookWithAStartingContentKey_returnsChannelLatestOrWhateverZkTellsUs() {
+        ContentRetriever contentRetriever = mock(ContentRetriever.class);
+        ClusterCacheDao clusterCacheDao = mock(ClusterCacheDao.class);
+        ObjectMapper objectMapper = mock(ObjectMapper.class);
+        ContentPath startingPath = ContentPath.fromUrl("2019/02/04/13/23/00/930/abcdef").get();
+        Webhook webhook = Webhook.builder()
+                .name("webhook")
+                .channelUrl("http://hi.com")
+                .callbackUrl("http://hi-webhook.com")
+                .batch(MINUTE.name())
+                .parallelCalls(2)
+                .startingKey(startingPath)
+                .build();
+
+        ContentPath mostRecentKey = ContentKey.fromUrl("2019/02/04/13/25/00/999/adbdsa").get();
+        when(clusterCacheDao.get("webhook", startingPath, WEBHOOK_LAST_COMPLETED)).thenReturn(mostRecentKey);
+
+        TimedWebhookStrategy timedWebhookStrategy = new TimedWebhookStrategy(contentRetriever, clusterCacheDao, objectMapper, webhook);
+
+        assertEquals(mostRecentKey, timedWebhookStrategy.getStartingPath());
+
+    }
+
+    @Test
+    void testStartingPath_withMinuteBasedWebhookAndNoStartingPath_returnsChannelLatestOrWhateverZkTellsUs() {
+        ContentRetriever contentRetriever = mock(ContentRetriever.class);
+        ClusterCacheDao clusterCacheDao = mock(ClusterCacheDao.class);
+        ObjectMapper objectMapper = mock(ObjectMapper.class);
+        Webhook webhook = Webhook.builder()
+                .name("webhook")
+                .channelUrl("http://hi.com")
+                .callbackUrl("http://hi-webhook.com")
+                .batch(MINUTE.name())
+                .parallelCalls(2)
+                .startingKey(null)
+                .build();
+
+        ContentPath channelLatest = ContentPath.fromUrl("2019/03/02/21/22/21/000/abcdef").get();
+        when(clusterCacheDao.get("webhook", new MinutePath(), WEBHOOK_LAST_COMPLETED)).thenReturn(channelLatest);
+
+        TimedWebhookStrategy timedWebhookStrategy = new TimedWebhookStrategy(contentRetriever, clusterCacheDao, objectMapper, webhook);
+
+        assertEquals(channelLatest, timedWebhookStrategy.getStartingPath());
     }
 }
