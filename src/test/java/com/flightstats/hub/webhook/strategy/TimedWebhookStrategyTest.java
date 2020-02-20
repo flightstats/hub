966cda0b6f93dd06a60d86c54acc37ce966023e5
Laurie Kemmerer <lkemmerer@flightstats.com>
2020-02-20 13:53:44 -0800

    Remove unnecessary test setup. Move assertion logic into the test that's using it.

--- src/test/java/com/flightstats/hub/webhook/strategy/TimedWebhookStrategyTest.java
+++ src/test/java/com/flightstats/hub/webhook/strategy/TimedWebhookStrategyTest.java
@@ -6,53 +6,51 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiConsumer;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TimedWebhookStrategyTest {
-//    @BeforeAll
-//    static void setupIntegration() {
-//        IntegrationTestSetup.run();
-//    }
-
     @Test
     void testRoundingSecondPath() {
-        compare(new DateTime(2016, 4, 21, 17, 22, 0, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compare(new DateTime(2016, 4, 21, 17, 22, 1, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compare(new DateTime(2016, 4, 21, 17, 22, 58, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compare(new DateTime(2016, 4, 21, 17, 22, 59, 0, DateTimeZone.UTC), "2016-04-21T17:22:00.000Z");
-    }
+        BiConsumer<DateTime, String> compare = (start, expected) -> {
+            SecondPath secondPath = new SecondPath(start);
+            DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
+            assertEquals(expected, stable.toString(), start.toString());
+        };
 
-    private void compare(DateTime start, String expected) {
-        SecondPath secondPath = new SecondPath(start);
-        DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
-        assertEquals(expected, stable.toString(), start.toString());
+        compare.accept(new DateTime(2016, 4, 21, 17, 22, 0, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compare.accept(new DateTime(2016, 4, 21, 17, 22, 1, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compare.accept(new DateTime(2016, 4, 21, 17, 22, 58, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compare.accept(new DateTime(2016, 4, 21, 17, 22, 59, 0, DateTimeZone.UTC), "2016-04-21T17:22:00.000Z");
     }
 
     @Test
     void testRoundingContentKey() {
-        compareContentKey(new DateTime(2016, 4, 21, 17, 22, 0, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compareContentKey(new DateTime(2016, 4, 21, 17, 22, 1, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compareContentKey(new DateTime(2016, 4, 21, 17, 22, 58, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
-        compareContentKey(new DateTime(2016, 4, 21, 17, 22, 59, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        BiConsumer<DateTime, String> compareContentKey = (DateTime start, String expected) -> {
+            ContentKey secondPath = new ContentKey(start);
+            DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
+            assertEquals(expected, stable.toString(), start.toString());
+        };
+
+        compareContentKey.accept(new DateTime(2016, 4, 21, 17, 22, 0, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compareContentKey.accept(new DateTime(2016, 4, 21, 17, 22, 1, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compareContentKey.accept(new DateTime(2016, 4, 21, 17, 22, 58, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
+        compareContentKey.accept(new DateTime(2016, 4, 21, 17, 22, 59, 0, DateTimeZone.UTC), "2016-04-21T17:21:00.000Z");
     }
 
-    private void compareContentKey(DateTime start, String expected) {
-        ContentKey secondPath = new ContentKey(start);
-        DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
-        assertEquals(expected, stable.toString(), start.toString());
-    }
 
     @Test
     void testRoundingMinutePath() {
-        compareContentPath(new DateTime(2016, 4, 21, 17, 0, 59, 0, DateTimeZone.UTC), "2016-04-21T16:59:00.000Z");
-        compareContentPath(new DateTime(2016, 4, 21, 17, 1, 58, 0, DateTimeZone.UTC), "2016-04-21T17:00:00.000Z");
-        compareContentPath(new DateTime(2016, 4, 21, 17, 58, 1, 0, DateTimeZone.UTC), "2016-04-21T17:57:00.000Z");
-        compareContentPath(new DateTime(2016, 4, 21, 17, 59, 59, 0, DateTimeZone.UTC), "2016-04-21T17:58:00.000Z");
-    }
+        BiConsumer<DateTime, String> compareContentPath = (start, expected) -> {
+            ContentKey secondPath = new ContentKey(start);
+            DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
+            assertEquals(expected, stable.toString(), start.toString());
+        };
 
-    private void compareContentPath(DateTime start, String expected) {
-        ContentKey secondPath = new ContentKey(start);
-        DateTime stable = TimedWebhookStrategy.replicatingStable_minute(secondPath);
-        assertEquals(expected, stable.toString(), start.toString());
+        compareContentPath.accept(new DateTime(2016, 4, 21, 17, 0, 59, 0, DateTimeZone.UTC), "2016-04-21T16:59:00.000Z");
+        compareContentPath.accept(new DateTime(2016, 4, 21, 17, 1, 58, 0, DateTimeZone.UTC), "2016-04-21T17:00:00.000Z");
+        compareContentPath.accept(new DateTime(2016, 4, 21, 17, 58, 1, 0, DateTimeZone.UTC), "2016-04-21T17:57:00.000Z");
+        compareContentPath.accept(new DateTime(2016, 4, 21, 17, 59, 59, 0, DateTimeZone.UTC), "2016-04-21T17:58:00.000Z");
     }
 }
