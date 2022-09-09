f431a8977a47200af07b5b39acb803ba5a517038
Laurie Kemmerer <laurie.kemmerer@cirium.com>
2022-09-09 16:02:05 -0700

    simplify system tests since the k8s/helm stuff is far out of date

--- src/systemTest/java/com/flightstats/hub/system/extension/TestSuiteClassWrapper.java
+++ src/systemTest/java/com/flightstats/hub/system/extension/TestSuiteClassWrapper.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(GuiceProviderExtension.class)
-@ExtendWith(HubLifecycleSuiteExtension.class)
 @ExtendWith(DependencyInjectionExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestSuiteClassWrapper {
