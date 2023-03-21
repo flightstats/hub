0f4e45fd0cb627f3cf386de20af79247dc4fe4f4
Laurie Kemmerer <laurie.kemmerer@cirium.com>
2023-03-21 09:40:28 -0700

    Delete Channel Documentation Resources

--- src/main/java/com/flightstats/hub/config/binding/SingleHubBindings.java
+++ src/main/java/com/flightstats/hub/config/binding/SingleHubBindings.java
@@ -7,9 +7,7 @@ import com.flightstats.hub.dao.CachedDao;
 import com.flightstats.hub.dao.CachedLowerCaseDao;
 import com.flightstats.hub.dao.ContentService;
 import com.flightstats.hub.dao.Dao;
-import com.flightstats.hub.dao.DocumentationDao;
 import com.flightstats.hub.dao.file.FileChannelConfigurationDao;
-import com.flightstats.hub.dao.file.FileDocumentationDao;
 import com.flightstats.hub.dao.file.FileWebhookDao;
 import com.flightstats.hub.dao.file.SingleContentService;
 import com.flightstats.hub.model.ChannelConfig;
@@ -25,7 +23,6 @@ public class SingleHubBindings extends AbstractModule {
     @Override
     protected void configure() {
         bind(ContentService.class).to(SingleContentService.class).asEagerSingleton();
-        bind(DocumentationDao.class).to(FileDocumentationDao.class).asEagerSingleton();
         bind(ChannelTtlEnforcer.class).asEagerSingleton();
         bind(ServiceRegistration.class).to(SingleServicesRegistration.class);
     }
