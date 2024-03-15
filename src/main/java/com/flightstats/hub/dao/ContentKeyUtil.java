21eca41dc0a59ec21732f12903bc596c247aa131
Abhay A Mishra <abhayanand.mishra@lexisnexisrisk.com>
2024-03-15 15:57:42 +0530

    Feature/hub 1333 hub 1347 (#1358)
    * added condition to ignore ttl days and maxItems if KeepForever is true , in case of false throw Exception
    
    * Added test cases
    
    * Refactor the test cases
    
    * Refactor the test cases
    
    * Add protect=true to failing ChannelValidation test cases; keepForever to ChannelConfig#toString
    
    * Add debug stuff; this can be reverted
    
    * Removed the extra test case
    
    * HUB-676 Removed /channel/{channel}/batch endpoint
    
    * Cleanup log statements
    
    * Removed the extra test case
    
    * HUB-1339: Initial Fix to check for InternalSpokeResource (#1351)
    
    * Initial Fix to check for InternalSpokeResource
    
    * Initial Fix to check for InternalSpokeResource
    
    ---------
    
    Co-authored-by: Abhay Anand Mishra <abhayanand.mishra@cirium.com>
    
    * Feature/hub 1333 (#1355)
    
    * HUB-1333 Changes to support testcases execution for windows specific file path
    
    * HUB-1333 reverted info to trace
    
    * HUB-1333 modified url string to use Paths.get()
    
    * HUB-1333 Refactoring paths
    
    * HUB-1333 Normalize paths
    
    ---------
    
    Co-authored-by: Gunasekaran <gunasekp@b2b.regn.net>
    
    * HUB-1347: High CVE - FileSpokeStore.java (#1354)
    
    * HUB-1347: High CVE - FileSpokeStore.java
    
    * HUB-1347: High CVE - FileSpokeStore.java
    
    * HUB-1347: Merging change from HUB-1333
    
    * HUB-1347: Merging change from HUB-1333
    
    * HUB-1347: Merging change from HUB-1333
    
    * HUB-1347: Merging change from HUB-1333
    
    ---------
    
    Co-authored-by: Abhay Anand Mishra <abhayanand.mishra@cirium.com>
    
    ---------
    
    Co-authored-by: dasarih0 <dasarih0@b2b.regn.net>
    Co-authored-by: Laurie Kemmerer <laurie.kemmerer@cirium.com>
    Co-authored-by: PrabhavathyG <prabhavathy.gunasekaran@cirium.com>
    Co-authored-by: harinadh-dasari <152592880+harinadh-dasari@users.noreply.github.com>
    Co-authored-by: PrabhavathyG <153164813+PrabhavathyG@users.noreply.github.com>
    Co-authored-by: Abhay Anand Mishra <abhayanand.mishra@cirium.com>
    Co-authored-by: Gunasekaran <gunasekp@b2b.regn.net>

--- src/main/java/com/flightstats/hub/dao/ContentKeyUtil.java
+++ src/main/java/com/flightstats/hub/dao/ContentKeyUtil.java
@@ -1,10 +1,12 @@
 package com.flightstats.hub.dao;
 
 import com.flightstats.hub.model.*;
+import com.flightstats.hub.util.HubUtils;
 import com.flightstats.hub.util.TimeUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 
+import java.nio.file.FileSystems;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -67,7 +69,7 @@ public class ContentKeyUtil {
 
     public static Optional<ContentKey> convertKey(String key) {
         if (StringUtils.isNotEmpty(key)) {
-            return ContentKey.fromUrl(StringUtils.substringAfter(key, "/"));
+            return ContentKey.fromUrl(StringUtils.substringAfter(key, HubUtils.FILE_SYSTEM_SEPARATOR));
         }
         return Optional.empty();
     }
