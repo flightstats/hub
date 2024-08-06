6bd02d6337117b90969681469037e573bcfdaf55
dasarih0 <dasarih0@b2b.regn.net>
2024-08-06 15:36:53 +0530

    Cleanup the Code

--- src/main/java/com/flightstats/hub/config/properties/PropertiesLoader.java
+++ src/main/java/com/flightstats/hub/config/properties/PropertiesLoader.java
@@ -51,7 +51,11 @@ public class PropertiesLoader {
 
         URL resource = null;
         try {
-            resource = new File(file).toURI().toURL();
+            if (Objects.nonNull(file) && sanitizePathTraversal(file)) {
+                log.error("Path traversal detected in input file ", file);
+            } else {
+                resource = new File(file).toURI().toURL();
+            }
         } catch (MalformedURLException e) {
             log.warn("Problem loading file {}", file, e);
         }
@@ -77,6 +81,10 @@ public class PropertiesLoader {
         if (getProperty("hub.read.only", false)) {
             ensureReadOnlyPropertiesAreSet();
         }
+    }
+
+    private boolean sanitizePathTraversal(String file) {
+        return file.contains("../") || file.contains("%");
     }
 
     private void ensureReadOnlyPropertiesAreSet() {
